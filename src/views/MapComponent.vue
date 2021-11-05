9e675e05f0d5d8e66c2061364877a75b5bf74d75
Werner Kramer <werner.kramer@vortech.nl>
2021-11-05 23:19:43 +0100

    Add hard coded wms layer

--- src/views/MapComponent.vue
+++ src/views/MapComponent.vue
@@ -2,12 +2,12 @@
   <div class="map-container">
     <v-mapbox
       :access-token="accessToken"
-      map-style='https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json'
-      :center="[5, 51]"
-      :zoom="6"
+      map-style='https://basemaps.cartocdn.com/gl/positron-gl-style/style.json'
+      :center="[54, 25]"
+      :zoom="5"
       :pitch="0"
       :bearing="0"
-      :min-zoom="5"
+      :min-zoom="2"
       :interactive="true"
       :drag-pan="true"
       :scroll-zoom="true"
@@ -19,12 +19,31 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { Map } from 'mapbox-gl'
 
 @Component
 export default class MapComponent extends Vue {
   accessToken = process.env.VUE_APP_MAPBOX_TOKEN
   mounted (): void {
-    const map = (this.$refs.map as any).map
+    const map: Map = (this.$refs.map as any).map
+    map.on('load', () => {
+      map.addSource('adoos', {
+        type: 'raster',
+        // use the tiles option to specify a WMS tile source URL
+        // https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/
+        tiles: [
+          'https://rwsos-dataservices-ont.avi.deltares.nl/adoos/FewsWebServices/wms?service=WMS&request=GetMap&version=1.3&layers=AGM_Currents_FM&styles=Class.Wind.Speed&format=image%2Fpng&transparent=true&crs=EPSG:3857&showContours=false&time=2021-11-05T22%3A00%3A00.000Z&uppercase=false&bbox={bbox-epsg-3857}'
+        ]
+      })
+      map.addLayer(
+        {
+          id: 'wms-test-layer',
+          type: 'raster',
+          source: 'adoos',
+          paint: {}
+        }
+      )
+    })
   }
 }
 </script>
