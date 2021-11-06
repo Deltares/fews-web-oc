30d90070df8a39dfaffdff5242c135d3adeafb6e
Werner Kramer <werner.kramer@vortech.nl>
2021-11-06 12:43:54 +0100

    Fix wms layer tile size

--- src/views/MapComponent.vue
+++ src/views/MapComponent.vue
@@ -32,8 +32,9 @@ export default class MapComponent extends Vue {
         // use the tiles option to specify a WMS tile source URL
         // https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/
         tiles: [
-          'https://rwsos-dataservices-ont.avi.deltares.nl/adoos/FewsWebServices/wms?service=WMS&request=GetMap&version=1.3&layers=AGM_Currents_FM&styles=Class.Wind.Speed&format=image%2Fpng&transparent=true&crs=EPSG:3857&showContours=false&time=2021-11-05T22%3A00%3A00.000Z&uppercase=false&bbox={bbox-epsg-3857}'
-        ]
+          'https://rwsos-dataservices-ont.avi.deltares.nl/adoos/FewsWebServices/wms?service=WMS&request=GetMap&version=1.3&layers=AGM_Currents_FM&styles=Class.Wind.Speed&format=image%2Fpng&transparent=true&crs=EPSG:3857&showContours=false&time=2021-11-05T22%3A00%3A00.000Z&uppercase=false&bbox={bbox-epsg-3857}&height=512&width=512'
+        ],
+        tileSize: 512
       })
       map.addLayer(
         {
