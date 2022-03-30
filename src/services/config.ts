9051c643093bff754869fb872d8438848961b863
ekkelenk <ekkelenkamp@gmail.com>
2022-03-30 11:14:49 +0200

    DWO-81: Deploy Web-OC as Azure Web App using Azure Devops. Fix base url.

--- src/services/config.ts
+++ src/services/config.ts
@@ -2,18 +2,18 @@ import { WebStorageStateStore } from 'oidc-client-ts'
 
 const publicPath = process.env.NODE_ENV === 'production'
   ? process.env.VUE_APP_PUBLIC_PATH
-  : ''
+  : '/'
 
 const baseUrl = window.location.origin + publicPath
 const oidcSettings = {
   authority: `${process.env.VUE_APP_AUTH_AUTHORITY}`,
   client_id: `${process.env.VUE_APP_AUTH_ID}`,
-  redirect_uri: `${baseUrl}/auth/callback`,
+  redirect_uri: `${baseUrl}auth/callback`,
   registration_uri: `${process.env.VUE_APP_AUTH_AUTHORITY}/protocol/openid-connect/registrations`,
-  silent_redirect_uri: `${baseUrl}/auth/silent`,
+  silent_redirect_uri: `${baseUrl}auth/silent`,
   response_type: `${process.env.VUE_APP_AUTH_TYPE}`,
   scope: `${process.env.VUE_APP_AUTH_SCOPE}`,
-  post_logout_redirect_uri: `${baseUrl}/auth/logout`,
+  post_logout_redirect_uri: `${baseUrl}auth/logout`,
   userStore: new WebStorageStateStore({ store: window.localStorage }),
   monitorSession: false,
   sessionChecksEnabled: true
