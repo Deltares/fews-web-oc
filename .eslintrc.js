3e018de4f863e5a8cdedbd068d0a2cca8a36bdc5
Werner Kramer <werner.kramer@vortech.nl>
2022-06-17 11:20:13 +0200

    Fix .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/essential',
-    '@vue/standard',
+    'eslint:recommended',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
