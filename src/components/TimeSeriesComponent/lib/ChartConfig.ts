8fcd9e8dd83cced2af07fcea02941150d70b7741
grijzea <andre.grijze@deltares.nl>
2022-09-27 10:06:28 +0200

    code cleanup (added list when multiple thumbnails are available)

--- src/components/TimeSeriesComponent/lib/ChartConfig.ts
+++ src/components/TimeSeriesComponent/lib/ChartConfig.ts
@@ -1,35 +1,5 @@
-import * as CSS from 'csstype'
-
-export interface ChartSeriesOptions {
-  x: {
-    key: string;
-    axisIndex: number;
-  };
-  y: {
-    key: string;
-    axisIndex: number;
-  };
-}
-
-export interface ChartSeries {
-  id: string;
-  dataResources: string[];
-  name: string;
-  label?: string;
-  type: string;
-  options: ChartSeriesOptions;
-  unit: string;
-  style: CSS.SvgPropertiesHyphen;
-}
-
-export interface Axis {
-  type: string;
-  location: string;
-  label?: string;
-  unit?: string;
-  includeZero?: boolean
-  domain?: number[] | Date[]
-}
+import {ChartSeries} from "./ChartSeries";
+import {Axis} from "./Axis";
 
 export interface ChartConfig {
   title: string;
