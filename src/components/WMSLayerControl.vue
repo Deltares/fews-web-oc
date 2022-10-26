175923f63fed19a9d01639f79f437b858070e4dc
Werner Kramer <werner.kramer@vortech.nl>
2022-10-26 14:54:33 +0200

    WIP: Add layer control

---
+++ src/components/WMSLayerControl.vue
@@ -0,0 +1,118 @@
+<template>
+  <v-menu
+    v-model="currentLayerMenu"
+    bottom
+    right
+    offset-y
+    :max-height="600"
+    max-width="350"
+    transition="slide-y-transition"
+    origin="top left"
+    :close-on-content-click="false"
+  >
+  <template v-slot:activator="{ on }">
+    <v-chip pill v-on.self="on" :color="$vuetify.theme.dark ? 'rgba(0,0,0,.5)' : 'rgba(255,255,255,.5)'" style="backdrop-filter: blur(4px);">
+      <v-switch v-model="active" @click.stop @change="onChange(layerName)"></v-switch>
+      {{ layerLabel }}
+      <span style="margin-left: 10px; padding: 0 10px; border-radius: 10px; background-color: rgba(0, 0, 0, 0.7); color: white;">{{formatedTime}}</span>
+      <v-icon> mdi-chevron-down </v-icon>
+    </v-chip>
+  </template>
+    <v-card>
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-icon>mdi-layers</v-icon>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ layerLabel }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Analysis time: {{ time }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item-group v-model="layerName">
+          <v-list-item
+            v-for="item in items"
+            :key="item.name"
+            :value="item.name"
+            @click="onChange(item.name)"
+          >
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+            <v-list-item-icon>
+              <v-icon v-show="item.name == layerName">mdi-check</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+import { DateTime } from 'luxon'
+
+export interface WMSLayerControlValue {
+  name: string;
+  active: boolean;
+}
+
+@Component
+export default class WMSLayerControl extends Vue {
+  @Prop({ default: () => [] }) items!: any[];
+  @Prop({ default: () => { return {
+    name: '',
+    active: true
+  }}}) value!: WMSLayerControlValue;
+  @Prop({ default: '' }) time!: string;
+  @Prop({ default: () => { return new Date() } }) timeIndex!: Date;
+
+  active = true
+  layerName: string = ''
+  currentLayerMenu = false
+
+  // TODO: make value a object with layer, active, time
+  mounted() {
+    this.valueChange();
+  }
+
+  onChange(id: string) {
+    this.$emit("change", { active: this.active, name: id} );
+  }
+
+  closeMenu() {
+    this.currentLayerMenu = false
+  }
+
+  get formatedTime(): string{
+    if ( isNaN(this.timeIndex.getTime()) ) {
+      return '--:--'
+    } else {
+      const format = 'HH:mm ZZZZ'
+      const timeZone = 'Europe/Amsterdam'
+      const dateTime = DateTime.fromJSDate(this.timeIndex).setZone( timeZone ).setLocale('nl-NL')
+      return dateTime.toFormat(format)
+    }
+  }
+
+  get layerLabel(): string {
+    const selectedLayer = this.items?.find((layer: any) => { return layer.name === this.layerName})
+    return selectedLayer ? selectedLayer.title : ''
+  }
+
+  @Watch("value", { deep: true})
+  valueChange() {
+    console.log('value', this.value)
+    this.layerName = this.value.name;
+    this.active = this.value.active
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/DataView.vue
+++ src/views/DataView.vue
@@ -5,7 +5,8 @@
         <v-divider></v-divider>
         <v-subheader>filters</v-subheader>
         <v-list expand nav>
-          <v-list-item v-for="filter in filters" :key="filter.id" link :to="{ name: 'DataViewer', params: { filterId: filter.id } }">
+          <v-list-item v-for="filter in filters" :key="filter.id" link
+            :to="{ name: 'DataViewer', params: { filterId: filter.id } }">
             <v-list-item-icon>
               <v-icon>{{ filter.icon }}</v-icon>
             </v-list-item-icon>
@@ -17,7 +18,8 @@
         <v-divider></v-divider>
         <v-subheader>categories</v-subheader>
         <v-list expand nav>
-          <v-list-item v-for="(c, i) of categories" :key="i" :to="{ name: 'DataViewer', params: { filterId, categoryId: c } }">
+          <v-list-item v-for="(c, i) of categories" :key="i"
+            :to="{ name: 'DataViewer', params: { filterId, categoryId: c } }">
             <v-list-item-content>
               <v-list-item-title>{{ c }}</v-list-item-title>
             </v-list-item-content>
@@ -29,48 +31,59 @@
       <div class="grid-map" v-show="showMap">
         <div class="map-container" style="height: calc(100% - 48px); position: relative;">
           <MapComponent :layer="layerOptions">
-            <v-mapbox-layer :options="layerA" clickable @click="onLocationClick"></v-mapbox-layer>
+            <!-- <v-mapbox-layer :options="layerA" clickable @click="onLocationClick"></v-mapbox-layer> -->
           </MapComponent>
+        </div>
+        <div style="position: absolute; z-index: 1200; padding-left: 5px;">
+          <v-chip-group>
+            <WMSLayerControl
+              v-model="layerOptions"
+              :time="externalForecastTime"
+              :items="currentLayers"
+              :timeIndex="currentTime"
+              @change="updateActiveLayer">
+            </WMSLayerControl>
+          </v-chip-group>
         </div>
         <DateTimeSlider class="date-time-slider" v-model="currentTime" :dates="times" @update:now="setCurrentTime"
           @input="debouncedSetLayerOptions" @timeupdate="updateTime">
         </DateTimeSlider>
+      </div>
+      <div class="grid-charts" ref="grid-charts" v-if="hasSelectedLocation && !$vuetify.breakpoint.mobile">
+        <v-toolbar class="toolbar-charts" dense flat>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn icon plain @click="onDockModeChange('left')">
+              <v-icon>mdi-dock-left</v-icon>
+            </v-btn>
+            <v-btn icon plain @click="onDockModeChange('bottom')">
+              <v-icon>mdi-dock-bottom</v-icon>
+            </v-btn>
+            <v-btn icon plain @click="onDockModeChange('right')">
+              <v-icon>mdi-dock-right</v-icon>
+            </v-btn>
+            <v-btn icon plain @click="closeCharts">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <router-view @toggleFullscreen="toggleFullscreen"></router-view>
+      </div>
+      <div class="grid-charts fullscreen" ref="grid-charts" v-if="hasSelectedLocation && $vuetify.breakpoint.mobile">
+        <v-toolbar class="toolbar-charts" dense flat>
+          <v-toolbar-title>
+          </v-toolbar-title>
+          <v-spacer />
+          <v-toolbar-items>
+            <v-btn icon plain @click="closeCharts">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <router-view :message="currentParameters">
+        </router-view>
+      </div>
     </div>
-    <div class="grid-charts" ref="grid-charts" v-if="hasSelectedLocation && !$vuetify.breakpoint.mobile">
-      <v-toolbar class="toolbar-charts" dense flat>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn icon plain @click="onDockModeChange('left')">
-            <v-icon>mdi-dock-left</v-icon>
-          </v-btn>
-          <v-btn icon plain @click="onDockModeChange('bottom')">
-            <v-icon>mdi-dock-bottom</v-icon>
-          </v-btn>
-          <v-btn icon plain @click="onDockModeChange('right')">
-            <v-icon>mdi-dock-right</v-icon>
-          </v-btn>
-          <v-btn icon plain @click="closeCharts">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-      <router-view @toggleFullscreen="toggleFullscreen"></router-view>
-    </div>
-    <div class="grid-charts fullscreen" ref="grid-charts" v-if="hasSelectedLocation && $vuetify.breakpoint.mobile">
-      <v-toolbar class="toolbar-charts" dense flat>
-        <v-toolbar-title>
-        </v-toolbar-title>
-        <v-spacer />
-        <v-toolbar-items>
-          <v-btn icon plain @click="closeCharts">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-      <router-view :message="currentParameters">
-      </router-view>
-    </div>
-  </div>
   </div>
 </template>
 
@@ -85,35 +98,45 @@ import { Layer } from '@/lib/wms';
 import { DateController } from '@/lib/TimeControl/DateController';
 import DateTimeSlider from '@/components/DateTimeSlider.vue'
 import { ColourMap } from 'wb-charts';
+import WMSLayerControl, { WMSLayerControlValue } from '@/components/WMSLayerControl.vue'
 
 interface Filter {
   id: string;
   name: string;
-  icon?: string;
+  icon ? : string;
 }
 
 interface Parameter {
   id: string;
   name: string;
-  icon?: string;
+  icon ? : string;
   parameterGroup: string;
 }
 
 @Component({
   components: {
     MapComponent,
-    DateTimeSlider
+    DateTimeSlider,
+    WMSLayerControl
   }
-}
-)
+})
 export default class DataView extends Mixins(WMSMixin) {
-  @Prop({ default: '', type: String })
+  @Prop({
+    default: '',
+    type: String
+  })
   filterId!: string
 
-  @Prop({ default: '', type: String })
+  @Prop({
+    default: '',
+    type: String
+  })
   categoryId!: string
 
-  @Prop({ default: '', type: String })
+  @Prop({
+    default: '',
+    type: String
+  })
   locationId!: string
 
   stateDockMode = 'right'
@@ -141,7 +164,10 @@ export default class DataView extends Mixins(WMSMixin) {
   unit: string = ""
 
   categories: string[] = []
-  locations: Location[]=  []
+  locations: Location[] = []
+
+  externalForecastTime = new Date()
+
   layerA = {
     'id': 'a',
     'type': 'circle',
@@ -172,18 +198,24 @@ export default class DataView extends Mixins(WMSMixin) {
     },
   }
 
-  created (): void {
+  created(): void {
     this.dateController = new DateController([])
-    this.debouncedSetLayerOptions = debounce(this.setLayerOptions, 500, { leading: true, trailing: true })
+    this.debouncedSetLayerOptions = debounce(this.setLayerOptions, 500, {
+      leading: true,
+      trailing: true
+    })
   }
 
-  async mounted () {
+  async mounted() {
     const baseUrl = this.$config.get('VUE_APP_FEWS_WEBSERVICES_URL')
     this.webServiceProvider = new PiWebserviceProvider(baseUrl)
     const response = await fetch(`${baseUrl}/rest/fewspiservice/v1/filters?documentFormat=PI_JSON`)
-    const filters = (await response.json()).filters.map( (f: Filter) => {
+    const filters = (await response.json()).filters.map((f: Filter) => {
       return {
-        ...f, ...{ icon: `mdi-alpha-${f.name[0].toLowerCase()}-circle-outline`}
+        ...f,
+        ...{
+          icon: `mdi-alpha-${f.name[0].toLowerCase()}-circle-outline`
+        }
       }
     })
     this.filters = filters
@@ -205,10 +237,11 @@ export default class DataView extends Mixins(WMSMixin) {
     }
     // const response = await this.webServiceProvider.getParameters(filter as any)
     const baseUrl = this.$config.get('VUE_APP_FEWS_WEBSERVICES_URL')
-    const response = await fetch(`${baseUrl}/rest/fewspiservice/v1/parameters?documentFormat=PI_JSON&filterId=${this.currentFilterId}`)
+    const response = await fetch(
+      `${baseUrl}/rest/fewspiservice/v1/parameters?documentFormat=PI_JSON&filterId=${this.currentFilterId}`)
     const parameters: Parameter[] = (await response.json()).timeSeriesParameters
     this.parameters = parameters
-    this.categories = uniq(parameters.map( p => p.parameterGroup))
+    this.categories = uniq(parameters.map(p => p.parameterGroup))
     this.currentCategoryId = this.categories[0]
     this.getLocations()
   }
@@ -220,33 +253,31 @@ export default class DataView extends Mixins(WMSMixin) {
     }
     // const response = await this.webServiceProvider.getParameters(filter as any)
     const baseUrl = this.$config.get('VUE_APP_FEWS_WEBSERVICES_URL')
-    const response = await fetch(`${baseUrl}/rest/fewspiservice/v1/locations?documentFormat=GEO_JSON&filterId=${this.currentFilterId}`)
+    const response = await fetch(
+      `${baseUrl}/rest/fewspiservice/v1/locations?documentFormat=GEO_JSON&filterId=${this.currentFilterId}`)
     const locations: any = await response.json()
     this.layerA.source.data = locations
   }
 
   @Watch('categoryId')
   onCategoryChange(): void {
+    this.layerOptions = {}
     this.currentCategoryId = this.categoryId
-    this.currentParameters = this.parameters.filter( (p) => p.parameterGroup === this.currentCategoryId )
+    this.currentParameters = this.parameters.filter((p) => p.parameterGroup === this.currentCategoryId)
     this.getLocations()
     this.updateLayers()
   }
 
   async updateLayers() {
-    const currentParameterIds = this.currentParameters.map( p => p.id )
-    console.log('currentParameterIds', currentParameterIds, this.layers.length)
-    const currentLayers = this.layers.filter( l => {
-      const layerParameterIds = l.keywordList.filter( k => k.parameterId ).map( k => k.parameterId )
+    const currentParameterIds = this.currentParameters.map(p => p.id)
+    const currentLayers = this.layers.filter(l => {
+      const layerParameterIds = l.keywordList.filter(k => k.parameterId).map(k => k.parameterId)
       const intersect = intersection(currentParameterIds, layerParameterIds)
-      console.log(this.categoryId, intersect.length > 0, intersect)
       return intersect.length > 0
     })
-    console.log('currentLayers', currentLayers)
     this.currentLayers = currentLayers
     if (currentLayers.length > 0) {
       this.layerName = currentLayers[0].name
-      console.log('setCurrent layer', this.layerName)
       this.onLayerChange()
     }
   }
@@ -265,7 +296,13 @@ export default class DataView extends Mixins(WMSMixin) {
   closeCharts() {
     if (this.hasSelectedLocation) {
       const params = this.$route.params
-      this.$router.push({ name: 'DataViewer', params: { filterId: params.filterId, categoryId: params.categoryId } })
+      this.$router.push({
+        name: 'DataViewer',
+        params: {
+          filterId: params.filterId,
+          categoryId: params.categoryId
+        }
+      })
     }
   }
 
@@ -279,12 +316,17 @@ export default class DataView extends Mixins(WMSMixin) {
 
   onLocationClick(e: any) {
     const locationId = e.features[0].properties.locationId
-    console.log(locationId)
-    this.$router.push({name: 'DataViewerWithLocation', params: { filterId: this.filterId, categoryId: this.categoryId, locationId }})
+    this.$router.push({
+      name: 'DataViewerWithLocation',
+      params: {
+        filterId: this.filterId,
+        categoryId: this.categoryId,
+        locationId
+      }
+    })
   }
 
   get hasSelectedLocation() {
-    console.log('hasLocation', this.locationId)
     return this.locationId !== ''
   }
 
@@ -309,7 +351,7 @@ export default class DataView extends Mixins(WMSMixin) {
     this.stateDockMode = dockMode
   }
 
-  setCurrentTime (enabled: boolean): void {
+  setCurrentTime(enabled: boolean): void {
     if (enabled) {
       this.dateController.selectDate(new Date())
       this.currentTime = this.dateController.currentTime
@@ -317,173 +359,190 @@ export default class DataView extends Mixins(WMSMixin) {
     }
   }
 
-  updateTime (date: Date): void {
+  updateTime(date: Date): void {
     this.dateController.selectDate(date)
     this.currentTime = this.dateController.currentTime
   }
 
-  @Watch('layerName')
-  async onLayerChange (): Promise<void> {
+  async onLayerChange(): Promise <void> {
     console.log('onLayerChange', this.layerName)
     try {
       this.times = await this.getTimes(this.layerName)
     } catch {
-      console.log('no times')
       this.times = []
     }
     this.dateController.dates = this.times
     this.dateController.selectDate(this.currentTime)
     this.currentTime = this.dateController.currentTime
-    console.log('currentLayer', this.layerName, this.currentTime)
     try {
       const response = await this.getLegendGraphic(this.layerName)
       this.legend = response.legend
       this.unit = response.unit
     } catch {
-      console.log('no legend')
       this.legend = []
       this.unit = ""
     }
     this.setLayerOptions()
   }
 
-  setLayerOptions (): void {
-    if (this.layerName) { this.layerOptions = { name: this.layerName, time: this.currentTime } }
-    console.log(this.layerOptions)
+  async updateActiveLayer(value: WMSLayerControlValue): Promise<void> {
+    // window.dispatchEvent(new Event('resize'))
+    console.log('updateActiveLayer', value)
+    if (value.name !== this.layerName || (value.active)) {
+      this.layerName = value.name
+      this.onLayerChange()
+      this.setLayerOptions()
+    }
+    if (value.active === false) {
+      this.layerOptions = {}
+    }
+    // this.setColourBar()
   }
 
+  setLayerOptions(): void {
+    if (this.layerName) {
+      this.layerOptions = {
+        name: this.layerName,
+        time: this.currentTime,
+        active: true
+      }
+    }
+    console.log(this.layerOptions)
+  }
 }
+
 </script>
 
 <style scoped>
-.grid-root {
-  display: flex;
-  padding: 0px;
-  width: 100%;
-  height: 100%;
-}
+  .grid-root {
+    display: flex;
+    padding: 0px;
+    width: 100%;
+    height: 100%;
+  }
 
-.grid-root.right {
-  flex-direction: row;
-}
+  .grid-root.right {
+    flex-direction: row;
+  }
 
-.grid-root.left {
-  flex-direction: row-reverse;
-}
+  .grid-root.left {
+    flex-direction: row-reverse;
+  }
 
-.grid-root.bottom {
-  flex-direction: column;
-}
+  .grid-root.bottom {
+    flex-direction: column;
+  }
 
-.grid-map {
-  display: flex;
-  flex-basis: 400px;
-  flex: 1 1 auto;
-  flex-direction: column;
-}
+  .grid-map {
+    display: flex;
+    flex-basis: 400px;
+    flex: 1 1 auto;
+    flex-direction: column;
+  }
 
-.grid-charts {
-  display: none;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  width: 50%;
-  z-index: 1100;
-}
+  .grid-charts {
+    display: none;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    z-index: 1100;
+  }
 
-.right > .grid-charts,
-.left > .grid-charts {
-  height: 100%;
-  width: 50%;
-}
+  .right>.grid-charts,
+  .left>.grid-charts {
+    height: 100%;
+    width: 50%;
+  }
 
-.bottom > .grid-charts {
-  height: 50%;
-  width: 100%;
-}
+  .bottom>.grid-charts {
+    height: 50%;
+    width: 100%;
+  }
 
-.grid-charts.fullscreen {
-  width: 100%;
-  flex-basis: 100%;
-  height: 100%;
-}
+  .grid-charts.fullscreen {
+    width: 100%;
+    flex-basis: 100%;
+    height: 100%;
+  }
 
-.bottom>.grid-map {
-  height: 50%;
-  width: 100%;
-}
+  .bottom>.grid-map {
+    height: 50%;
+    width: 100%;
+  }
 
-.toolbar-charts {
-  flex: 0 0 auto;
-}
+  .toolbar-charts {
+    flex: 0 0 auto;
+  }
 
-.colourbar {
-  font-size: 0.825em;
-  z-index: 1000;
-  background-color: none;
-  margin-top: -50px;
-  width: 500px;
-  height: 100px;
-}
+  .colourbar {
+    font-size: 0.825em;
+    z-index: 1000;
+    background-color: none;
+    margin-top: -50px;
+    width: 500px;
+    height: 100px;
+  }
 
-.wms-panel {
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-}
+  .wms-panel {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+  }
 
-.wms-layer-control-container {
-  display: flex;
-  z-index: 1200;
-  padding: 15px 50px 15px 50px;
-}
+  .wms-layer-control-container {
+    display: flex;
+    z-index: 1200;
+    padding: 15px 50px 15px 50px;
+  }
 
-.map-only>.wms-panel>.wms-layer-control-container,
-.right>.wms-panel>.wms-layer-control-container {
-  justify-content: flex-start;
-}
+  .map-only>.wms-panel>.wms-layer-control-container,
+  .right>.wms-panel>.wms-layer-control-container {
+    justify-content: flex-start;
+  }
 
-.bottom>.wms-panel>.wms-layer-control-container,
-.left>.wms-panel>.wms-layer-control-container {
-  justify-content: flex-end;
-}
+  .bottom>.wms-panel>.wms-layer-control-container,
+  .left>.wms-panel>.wms-layer-control-container {
+    justify-content: flex-end;
+  }
 
-.mobile>.wms-panel>.wms-layer-control-container {
-  padding: 10px 0px;
-  width: 100%;
-  justify-content: center;
-}
+  .mobile>.wms-panel>.wms-layer-control-container {
+    padding: 10px 0px;
+    width: 100%;
+    justify-content: center;
+  }
 
-.map-only>.wms-panel,
-.right>.wms-panel {
-  left: 0px;
-  bottom: 80px;
-}
+  .map-only>.wms-panel,
+  .right>.wms-panel {
+    left: 0px;
+    bottom: 80px;
+  }
 
-.left>.wms-panel {
-  right: 0px;
-  bottom: 80px;
-}
+  .left>.wms-panel {
+    right: 0px;
+    bottom: 80px;
+  }
 
-.bottom>.wms-panel {
-  top: 10px;
-  right: 0px;
-}
+  .bottom>.wms-panel {
+    top: 10px;
+    right: 0px;
+  }
 
-.mobile>.wms-panel {
-  justify-content: center;
-  bottom: 80px;
-  width: 100%;
-}
+  .mobile>.wms-panel {
+    justify-content: center;
+    bottom: 80px;
+    width: 100%;
+  }
 
-.map-container {
-  display: flex;
-  flex: 1 1;
-  height: 100%;
-}
+  .map-container {
+    display: flex;
+    flex: 1 1;
+    height: 100%;
+  }
 
-.datetime-control-container {
-  flex-grow: 0;
-  z-index: 1000;
-}
+  .datetime-control-container {
+    flex-grow: 0;
+    z-index: 1000;
+  }
+
 </style>
