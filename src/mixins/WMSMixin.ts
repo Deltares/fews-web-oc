d51e7e2eb72911eebf7f83ac0a8715b86921f88a
ekkelenk <ekkelenkamp@gmail.com>
2022-12-14 13:06:57 +0100

    Check for correct layer name returned from get capabilities to determine times.

--- src/mixins/WMSMixin.ts
+++ src/mixins/WMSMixin.ts
@@ -31,23 +31,40 @@ export default class WMSMixin extends Vue {
   async getTimes (layers: string): Promise<Date[]> {
     const capabilities = await this.wmsProvider.getCapabilities({ layers, importFromExternalDataSource: false, onlyHeaders: false, forecastCount: 1 })
     let valueDates: Date[]
-    if (capabilities.layers.length > 0 && capabilities.layers[0].times) {
-      const dates = capabilities.layers[0].times.map((time) => { return new Date(time) })
-      let firstValueDate = dates[0]
-      let lastValueDate = dates[dates.length-1]
-      if (capabilities.layers[0].firstValueTime) { firstValueDate = new Date(capabilities.layers[0].firstValueTime) }
-      if (capabilities.layers[0].lastValueTime) { lastValueDate = new Date(capabilities.layers[0].lastValueTime) }
-      valueDates = dates.filter(d => d >= firstValueDate && d <= lastValueDate)
-      if (capabilities.layers[0].keywordList && capabilities.layers[0].keywordList[0].forecastTime) {
-          this.externalForecast = new Date(capabilities.layers[0].keywordList[0].forecastTime)
+    let selectedLayer: Layer
+    if (capabilities.layers.length > 0) {
+      selectedLayer = capabilities.layers[0]
+      capabilities.layers.forEach( l => {
+        if (l.name === layers) {
+          selectedLayer = l
+        }
+      })
+      if (selectedLayer.times) {
+        const dates = selectedLayer.times.map((time) => {
+          return new Date(time)
+        })
+        let firstValueDate = dates[0]
+        let lastValueDate = dates[dates.length - 1]
+        if (selectedLayer.firstValueTime) {
+          firstValueDate = new Date(selectedLayer.firstValueTime)
+        }
+        if (selectedLayer.lastValueTime) {
+          lastValueDate = new Date(selectedLayer.lastValueTime)
+        }
+        valueDates = dates.filter(d => d >= firstValueDate && d <= lastValueDate)
+        if (selectedLayer.keywordList && selectedLayer.keywordList[0].forecastTime) {
+          this.externalForecast = new Date(selectedLayer.keywordList[0].forecastTime)
+        }
+      } else {
+        valueDates = []
       }
     } else {
-      valueDates = []
       this.externalForecast = new Date('invalid')
       return Promise.reject(new Error('No forecast for selected layer'))
     }
     return valueDates
   }
+
 
   async getLegendGraphic (layers: string): Promise<any> {
     return await this.wmsProvider.getLegendGraphic({layers})
