47afd72f73a667f826b3492f4460cf9e395c5d70
Anne Markensteijn <anne.markensteijn@vortech.nl>
2023-03-10 16:26:29 +0100

    Add files required to build Docker container

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? process.env.VUE_APP_PUBLIC_PATH
+    ? '__BASE_URL__'
     : '/',
   transpileDependencies: [
     'vuetify'
