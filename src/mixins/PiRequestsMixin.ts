02dc7484b85abfaa014f0b0f4a74c4d8f053871e
Anne Markensteijn <anne.markensteijn@vortech.nl>
2023-04-12 15:13:19 +0200

    Only send auth headers if auth is set

--- src/mixins/PiRequestsMixin.ts
+++ src/mixins/PiRequestsMixin.ts
@@ -5,13 +5,8 @@ import { Vue, Component } from 'vue-property-decorator'
 export default class PiRequestsMixin extends Vue {
 
   async transformRequest(request: Request): Promise<Request> {
-    const token = await this.$auth.getAccessToken()
-    const requestInit = {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': "application/json"
-      },
-    }
+    const requestAuthHeaders = await this.$auth.getAuthorizationHeaders()
+    const requestInit = { headers: requestAuthHeaders}
     const newRequest = new Request(request, requestInit)
     return newRequest
   }
--- src/services/authentication/AuthenticationManager.ts
+++ src/services/authentication/AuthenticationManager.ts
@@ -1,5 +1,6 @@
 import { configManager } from '../application-config/'
 import { UserManager } from "oidc-client-ts";
+import { RequestHeaderAuthorization } from '../application-config/ApplicationConfig';
 
 export class AuthenticationManager extends UserManager{
   private readonly _userManager: UserManager
@@ -16,5 +17,18 @@ export class AuthenticationManager extends UserManager{
         return user.access_token;
     }
     return ''
+  }
+
+  public async getAuthorizationHeaders(): Promise<HeadersInit> {
+    if (!configManager.authenticationIsEnabled) return {}
+    switch (configManager.get('VUE_APP_REQUEST_HEADER_AUTHORIZATION')){
+      case (RequestHeaderAuthorization.BEARER): {
+        const token = await this.getAccessToken()
+        const requestAuthHeaders = {'Authorization': `Bearer ${token}`}
+        return requestAuthHeaders
+      }
+      default:
+        return {}
+    }
   }
 }
