81ada720d8f70565832a210126235031e927a443
Werner Kramer <werner.kramer@vortech.nl>
2023-07-03 22:56:49 +0200

    Optimize createTableData

--- src/components/TimeSeriesComponent/lib/createTableData.ts
+++ src/components/TimeSeriesComponent/lib/createTableData.ts
@@ -5,32 +5,34 @@ export function createTableData(chartSeriesArray: ChartSeries[] | undefined, ser
   if (chartSeriesArray === undefined) return []
   const dateTimes = createDateTimes(chartSeriesArray, seriesRecord)
 
-  const seriesDef = chartSeriesArray
-  return dateTimes.map((date: Date) => {
-    const event: any = {}
-    event.date = date.toLocaleString(undefined, {
-      weekday: 'short',
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-      hour12: false
-    })
-    for (const seriesId of seriesIds) {
-      const chartSeries = seriesDef.find((s) => s.id === seriesId)
-      if (chartSeries === undefined) continue
-      const series = seriesRecord[chartSeries.dataResources[0]]
-      if (series === undefined) {
-        event[chartSeries.id] = null
-      } else {
-        const data = series.data ?? []
-        const selected = data.find((dataPoint: { x: Date, y: number }) => date.getTime() === dataPoint.x.getTime())
-        event[chartSeries.id] = selected !== undefined ? selected.y : null
+  const series = chartSeriesArray.filter((s) => seriesIds.includes(s.id))
+  const p = Array(seriesIds.length).fill(0)
+  const dateFormatter = new Intl.DateTimeFormat(undefined, {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false
+  })
+
+  return dateTimes.map((date: Date, i: number) => {
+    const result: any = {}
+    result.date = dateFormatter.format(date)
+    for ( const j in series ) {
+      const s = series[j]
+      const data = seriesRecord[s.dataResources[0]]
+      const event = data.data[p[j]]
+      let value = null
+      if (date.getTime() === event.x.getTime()) {
+        value = event.y
+        p[j]++
       }
-    }
-    return event
+      result[s.id] = value
+    } 
+    return result
   })
 }
 
@@ -42,18 +44,28 @@ function createDateTimes(chartSeriesArray: ChartSeries[] | undefined, seriesReco
   for (const chartSeries of chartSeriesArray) {
     const series = seriesRecord[chartSeries.dataResources[0]]
     if (series !== undefined && series.data !== undefined) {
-      for (const event of series.data) {
-        if (dates.findIndex(date => {
-          return date.getTime() === event.x.getTime()
-        }) === -1) {
-          dates.push(event.x)
-        }
-      }
+      dates.push(...series.data.map((d: any) => d.x) )
     }
   }
+  return sortUniqueDates(dates)
+}
+
+/**
+* 
+* Sorts an array of dates in ascending order and removes any duplicate dates.
+* @param {Date[]} dates - The array of dates to be sorted and made unique.
+* @returns {Date[]} A new array of dates sorted in ascending order without any duplicates.
+*/
+function sortUniqueDates(dates: Date[]): Date[] {
+  if (dates.length === 0) return dates;
   dates.sort((a, b) => {
     return a.getTime() - b.getTime()
   })
-  return dates
+  const results = [dates[0]];
+  for (let i = 1; i < dates.length; i++) {
+    if (dates[i-1].getTime() !== dates[i].getTime()) {
+      results.push(dates[i]);
+    }
+  }
+  return results;
 }
-
