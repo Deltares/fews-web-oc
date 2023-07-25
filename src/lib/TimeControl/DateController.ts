f17f3ec4704aad6d71c912cf0aa852abb73f581d
Cees Voesenek <cees.voesenek@vortech.nl>
2023-07-25 15:30:50 +0200

    Handle undefined dates in date controller

--- src/lib/TimeControl/DateController.ts
+++ src/lib/TimeControl/DateController.ts
@@ -6,8 +6,10 @@ export class DateController {
     this.dates = dates
   }
 
-  selectDate (date: Date): void {
+  selectDate(dateIn?: Date): void {
     if (this.dates === undefined || this.dates.length === 0) return
+    const date = dateIn ?? new Date()
+    // For an unspecified date, use the first available date.
     const index = this.dates.findIndex(cur => cur.getTime() + 10 * 60 * 1000 > date.getTime())
     if (index === -1) {
       if (this.dates[0].getTime() > date.getTime()) {
