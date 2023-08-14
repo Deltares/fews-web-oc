6994530d773e439ba433301eebcf470c5e73fae5
Rudie Ekkelenkamp <ekkelenkamp@gmail.com>
2023-08-14 14:34:34 +0200

    cypress config added.

---
+++ cypress/e2e/about/about.cy.ts
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('about', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://localhost:8080/about')
+  })
+  it('displays two todo items by default', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get('*[class^="v-list-item v-list-item--link theme--light"]').should('have.length', 5)
+  })
+})
