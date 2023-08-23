f4a938fec99019da3b203c7c561ca4792b110e1e
Werner Kramer <werner.kramer@vortech.nl>
2023-08-23 19:51:09 +0200

    Move vuetify to plugins

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import vuetify from './plugins/vuetify.js'
 
-// Vuetify
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
 
 const app = createApp(App)
 app.use(router)
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,13 @@
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+export default vuetify
