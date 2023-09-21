2c54a33eafff76c6afbd20c78cd42bb60bfe1c43
Dirk van Meeuwen <dirk.vanmeeuwen@vortech.nl>
2023-09-21 14:44:03 +0200

    improve double click naming and checking

--- src/components/AnimatedMapboxLayer.vue
+++ src/components/AnimatedMapboxLayer.vue
@@ -181,10 +181,12 @@ export default class AnimatedMapboxLayer extends  Vue {
     }
   }
 
-  setDoubleClickLayer() {
-    if (this.mapObject && this.mapObject.doubleClickZoom.isEnabled()) {
-      // deactivate double click zoom
-      this.mapObject.doubleClickZoom.disable()
+  enableDoubleClickLayer() {
+    if (this.mapObject) {
+      if(this.mapObject.doubleClickZoom.isEnabled()){
+        // deactivate double click zoom
+        this.mapObject.doubleClickZoom.disable()
+      }
       this.mapObject.on("dblclick", (e) => {
         this.$emit("doubleclick", e)
       })
@@ -213,7 +215,7 @@ export default class AnimatedMapboxLayer extends  Vue {
     this.newLayerId = getFrameId(this.layer.name, this.counter)
     const source = this.mapObject.getSource(this.newLayerId)
     const baseUrl = this.$config.get("VUE_APP_FEWS_WEBSERVICES_URL")
-    this.setDoubleClickLayer()
+    this.enableDoubleClickLayer()
     if (this.currentLayer !== originalLayerName) {
       // set default zoom only if layer is changed
       this.setDefaultZoom()
