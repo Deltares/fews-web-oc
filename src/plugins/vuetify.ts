4b94f977cbf80c3aca659cebd0370515e8652ebf
Anne Markensteijn <anne.markensteijn@vortech.nl>
2023-09-25 15:26:12 +0200

    Import v-data-table

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,11 +2,15 @@
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import * as components from 'vuetify/components'
+import { VDataTable } from 'vuetify/labs/VDataTable'
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VDataTable,
+  },
   directives,
 })
 
