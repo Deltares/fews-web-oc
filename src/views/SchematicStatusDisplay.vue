cf213430db7585d9ac96abe18018f1f8292472f6
Cees Voesenek <cees.voesenek@vortech.nl>
2023-09-27 12:24:26 +0200

    Add date/time slider to SchematicStatusDisplay

--- src/views/SchematicStatusDisplay.vue
+++ src/views/SchematicStatusDisplay.vue
@@ -1,21 +1,28 @@
 <template>
   <Teleport to="#web-oc-sidebar-target">
-    <ColumnMenu :active.sync="active" :items="items" :open.sync="open"/>
+    <ColumnMenu :active.sync="active" :items="items" :open.sync="open" />
   </Teleport>
   <div class="container">
-    <div class="ssd-container">
-      <SsdComponent :src="src"/>
-    </div>
+    <SsdComponent :src="src" />
+    <DateTimeSlider
+      v-model:selectedDate="selectedDate"
+      v-model:doFollowNow="doFollowNow"
+      :dates="dates"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import ColumnMenu from '../components/general/ColumnMenu.vue'
 import { ref, onMounted, computed } from 'vue'
-import { ColumnItem } from '../components/general/ColumnItem'
-import SsdComponent from '../components/ssd/SsdComponent.vue'
+
 import { configManager } from '../services/application-config/index.ts'
+import type { ColumnItem } from '../components/general/ColumnItem'
+
 import { useSsd } from '../services/useSsd/index.ts'
+
+import ColumnMenu from '../components/general/ColumnMenu.vue'
+import DateTimeSlider from '../components/general/DateTimeSlider.vue'
+import SsdComponent from '../components/ssd/SsdComponent.vue'
 
 interface Props {
   groupId?: string
@@ -31,12 +38,20 @@ const baseUrl = configManager.get('VITE_FEWS_WEBSERVICES_URL')
 
 const active = ref<string[]>([])
 const open = ref<string[]>([])
-const time = ref('')
+
+const selectedDate = ref(new Date())
+const doFollowNow = ref(false)
 
 onMounted(() => {
   active.value = ['root']
 })
 
+const selectedDateString = computed(
+  () => {
+    const dateString = selectedDate.value.toISOString()
+    return dateString.substring(0, 19) + 'Z'
+  }
+)
 const items = computed(() => {
   const result: ColumnItem[] = []
   const c = capabilities.value
@@ -63,11 +78,11 @@ const items = computed(() => {
   return [{ id: 'root', name: 'Groups', children: result }]
 })
 
-const { capabilities, src } = useSsd(
+const { capabilities, src, dates } = useSsd(
   baseUrl,
   () => props.groupId,
   () => props.panelId,
-  time,
+  selectedDateString,
 )
 </script>
 
@@ -76,11 +91,6 @@ const { capabilities, src } = useSsd(
   display: flex;
   flex-direction: column;
   height: 100%;
-  width: 100%;
-}
-
-.ssd-container {
-  flex: 100% 1 1;
   width: 100%;
 }
 </style>
