78913a4c6ee6e673b6aff3b3bafa14c418c946a2
Anne Markensteijn <anne.markensteijn@vortech.nl>
2023-09-29 14:30:43 +0200

    Assume there is only one display for SSD PI requests

--- src/components/ssd/SsdTimeSeriesDisplay.vue
+++ src/components/ssd/SsdTimeSeriesDisplay.vue
@@ -3,22 +3,9 @@
     <div style="flex: 1 1 100%; height: 100%">
       <WindowComponent>
         <template v-slot:toolbar>
-          <v-menu offset-y z-index="10000">
-            <template v-slot:activator="{ props }">
-              <v-btn class="text-capitalize" variant="text" v-bind="props"
-                >{{ plotIds[selectedPlot] }}<v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list v-model="selectedPlot" density="compact">
-              <v-list-item
-                v-for="(plot, i) in plotIds"
-                v-bind:key="i"
-                @click="selectedPlot = i"
-              >
-                <v-list-item-title>{{ plot }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
+          <v-spacer />
+          {{ displayConfig?.title }}
+          <v-spacer />
           <v-btn-toggle class="mr-5" v-model="displayType" mandatory>
             <v-btn
               v-for="item in displayTypeItems"
@@ -45,7 +32,6 @@
 import { ref } from 'vue'
 import { configManager } from '@/services/application-config'
 import { useSsdPi } from '@/services/useSsdPi/index.ts'
-import { computed } from 'vue'
 import WindowComponent from '@/components/general/WindowComponent.vue'
 import TimeSeriesComponent from '@/components/timeseries/TimeSeriesComponent.vue'
 import { DisplayType } from '@/lib/display/DisplayConfig'
@@ -61,25 +47,12 @@ const props = withDefaults(defineProps<Props>(), {
 })
 
 const baseUrl = configManager.get('VITE_FEWS_WEBSERVICES_URL')
-const selectedPlot = ref(0)
 
-const { displays, displayConfig } = useSsdPi(
+const { displayConfig } = useSsdPi(
   baseUrl,
   () => props.panelId,
   () => props.objectId,
-  selectedPlot,
 )
-
-const plotIds = computed(() => {
-  if (displays.value?.length) {
-    const ids = displays.value.map((d) => {
-      return d.title
-    })
-    return ids
-  } else {
-    return []
-  }
-})
 
 interface DisplayTypeItem {
   icon: string
