b502db29190b47e12292f36f276d2e1499d61841
hvangeffen <hunter.vangeffen@vortech.nl>
2023-10-19 11:48:43 +0200

    Add accept button to splash

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -1,6 +1,12 @@
 <template>
-  <v-dialog v-model="showDialog" max-width="900" @click:outside="showDialog = false">
-    <v-img :src="splashUrl" @click="showDialog = false"/>
+  <v-dialog v-model="showDialog" max-width="900" persistent>
+    <v-card>
+      <v-img :src="splashUrl" />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn @click="showDialog = false">I ACCEPT</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-dialog>
 </template>
 
@@ -10,6 +16,6 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class SplashScreen extends Vue {
   showDialog: boolean = true
-  splashUrl: string = `${process.env.BASE_URL}splash.png`
+  splashUrl: string = `${process.env.BASE_URL}images/splash.png`
 }
 </script>
