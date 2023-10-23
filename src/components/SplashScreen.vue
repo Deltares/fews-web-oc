1a76b690461e906c4a825453a14aff090b7db143
hvangeffen <hunter.vangeffen@vortech.nl>
2023-10-23 21:38:53 +0200

    Add disclaimer component and use it for splash

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -1,21 +1,31 @@
 <template>
-  <v-dialog v-model="showDialog" max-width="900" persistent>
-    <v-card>
+  <v-dialog v-model="showDialog" max-width="900">
+    <v-card v-if="!showDisclaimer">
       <v-img :src="splashUrl" />
-      <v-card-actions>
-        <v-spacer />
-        <v-btn @click="showDialog = false">I ACCEPT</v-btn>
-      </v-card-actions>
+      <a class="terms-of-use-link" @click="showDisclaimer = true">Terms of Use</a>
     </v-card>
+    <DisclaimerText v-else />
   </v-dialog>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import DisclaimerText from '@/components/DisclaimerText.vue'
 
-@Component
+@Component({components: {
+  DisclaimerText
+}})
 export default class SplashScreen extends Vue {
   showDialog: boolean = true
+  showDisclaimer: boolean = false
   splashUrl: string = `${process.env.BASE_URL}images/splash.png`
 }
 </script>
+
+<style scoped>
+.terms-of-use-link {
+  position: absolute;
+  bottom: 12px;
+  right: 180px;
+}
+</style>
