c3f0c28fbe8f6ed5bef96a685812eb4fa8aaf0a6
Werner Kramer <werner.kramer@vortech.nl>
2023-11-03 16:15:42 +0100

    Fix linter issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Ensure the @ import alias still works
-  base:
-    process.env.NODE_ENV === 'production' ? process.env.BASE_URL : '/',
+  base: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
