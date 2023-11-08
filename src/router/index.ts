03c87863ff4077d21fcf1e2d2993818c4bbbd671
Werner Kramer <werner.kramer@vortech.nl>
2023-11-08 08:17:58 +0100

    Do not use defineAsyncComponent

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,36 +12,22 @@ import Silent from '../views/auth/Silent.vue'
 import { configManager } from '../services/application-config'
 import { authenticationManager } from '../services/authentication/AuthenticationManager'
 import { useConfigStore } from '../stores/config.ts'
-import { defineAsyncComponent } from 'vue'
-const SystemMonitorDisplayView = defineAsyncComponent(
-  () => import('../views/SystemMonitorDisplayView.vue'),
-)
-const SchematicStatusDisplayView = defineAsyncComponent(
-  () => import('../views/SchematicStatusDisplayView.vue'),
-)
-const SSDTimeSeriesDisplay = defineAsyncComponent(
-  () => import('../components/ssd/SsdTimeSeriesDisplay.vue'),
-)
-const SpatialDisplayView = defineAsyncComponent(
-  () => import('../views/SpatialDisplayView.vue'),
-)
-const SpatialDisplay = defineAsyncComponent(
-  () => import('../components/spatialdisplay/SpatialDisplay.vue'),
-)
-const TimeSeriesDisplayView = defineAsyncComponent(
-  () => import('../views/TimeSeriesDisplayView.vue'),
-)
-const TopologyDisplayView = defineAsyncComponent(
-  () => import('../views/TopologyDisplayView.vue'),
-)
-const TimeSeriesDisplay = defineAsyncComponent(
-  () => import('../components/timeseries/TimeSeriesDisplay.vue'),
-)
 
-const UserSettingsView = defineAsyncComponent(
-  () => import('../views/UserSettingsView.vue'),
-)
-const Empty = defineAsyncComponent(() => import('../views/Empty.vue'))
+const SystemMonitorDisplayView = () =>
+  import('../views/SystemMonitorDisplayView.vue')
+const SchematicStatusDisplayView = () =>
+  import('../views/SchematicStatusDisplayView.vue')
+const SSDTimeSeriesDisplay = () =>
+  import('../components/ssd/SsdTimeSeriesDisplay.vue')
+const SpatialDisplayView = () => import('../views/SpatialDisplayView.vue')
+const SpatialDisplay = () =>
+  import('../components/spatialdisplay/SpatialDisplay.vue')
+const TimeSeriesDisplayView = () => import('../views/TimeSeriesDisplayView.vue')
+const TopologyDisplayView = () => import('../views/TopologyDisplayView.vue')
+const TimeSeriesDisplay = () =>
+  import('../components/timeseries/TimeSeriesDisplay.vue')
+const UserSettingsView = () => import('../views/UserSettingsView.vue')
+const Empty = () => import('../views/Empty.vue')
 
 const routesBase: Readonly<RouteRecordRaw[]> = [
   {
