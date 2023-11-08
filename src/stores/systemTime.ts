50013b0ba045f10d74419243f5ad69657d65a2bc
Werner Kramer <werner.kramer@vortech.nl>
2023-11-08 08:07:08 +0100

    Start clock on store use

--- src/stores/systemTime.ts
+++ src/stores/systemTime.ts
@@ -8,52 +8,60 @@ export interface SystemTimeStore {
   endTime: Date | null
   selectedInterval: string
 }
-export const useSystemTimeStore = defineStore({
-  id: 'systemTime',
-  state: (): SystemTimeStore => ({
-    systemTime: new Date(),
-    intervalTimer: undefined,
-    startTime: null,
-    endTime: null,
-    selectedInterval: 'default',
-  }),
-  actions: {
-    startClock() {
-      this.systemTime = new Date()
-      this.intervalTimer = setInterval(() => {
-        this.systemTime = new Date()
-      }, 1000)
-    },
-    stopClock() {
-      clearInterval(this.intervalTimer)
-      this.intervalTimer = undefined
-    },
-    changeInterval() {
-      if (!this.selectedInterval) return
 
-      if (this.selectedInterval === 'default') {
-        // Use the FEWS default time interval.
-        this.startTime = null
-        this.endTime = null
-      } else if (this.selectedInterval === 'custom') {
-        // Use the custom time interval.
-      } else {
-        const now = this.systemTime
-        const interval = this.selectedInterval.split('/')
-        if (interval.length === 2) {
-          this.endTime = DateTime.fromJSDate(now)
-            .plus(Duration.fromISO(interval[1]))
-            .toJSDate()
-          this.startTime = DateTime.fromJSDate(now)
-            .plus(Duration.fromISO(interval[0]))
-            .toJSDate()
-        } else if (interval.length === 1) {
+export const useSystemTimeStore = () => {
+  const store = defineStore({
+    id: 'systemTime',
+    state: (): SystemTimeStore => ({
+      systemTime: new Date(),
+      intervalTimer: undefined,
+      startTime: null,
+      endTime: null,
+      selectedInterval: 'default',
+    }),
+    actions: {
+      startClock() {
+        this.systemTime = new Date()
+        this.intervalTimer = setInterval(() => {
+          this.systemTime = new Date()
+        }, 1000)
+      },
+      stopClock() {
+        clearInterval(this.intervalTimer)
+        this.intervalTimer = undefined
+      },
+      changeInterval() {
+        if (!this.selectedInterval) return
+
+        if (this.selectedInterval === 'default') {
+          // Use the FEWS default time interval.
+          this.startTime = null
           this.endTime = null
-          this.startTime = DateTime.fromJSDate(now)
-            .plus(Duration.fromISO(interval[0]))
-            .toJSDate()
+        } else if (this.selectedInterval === 'custom') {
+          // Use the custom time interval.
+        } else {
+          const now = this.systemTime
+          const interval = this.selectedInterval.split('/')
+          if (interval.length === 2) {
+            this.endTime = DateTime.fromJSDate(now)
+              .plus(Duration.fromISO(interval[1]))
+              .toJSDate()
+            this.startTime = DateTime.fromJSDate(now)
+              .plus(Duration.fromISO(interval[0]))
+              .toJSDate()
+          } else if (interval.length === 1) {
+            this.endTime = null
+            this.startTime = DateTime.fromJSDate(now)
+              .plus(Duration.fromISO(interval[0]))
+              .toJSDate()
+          }
         }
-      }
+      },
     },
-  },
-})
+  })
+  const s = store()
+  if (s.intervalTimer === undefined) {
+    s.startClock()
+  }
+  return s
+}
