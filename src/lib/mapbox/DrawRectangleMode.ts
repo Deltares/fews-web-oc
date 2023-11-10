3486b487007793544325bed702ca4448b08a44a6
Werner Kramer <werner.kramer@vortech.nl>
2023-11-10 16:20:03 +0100

    Copilot refactor

--- src/lib/mapbox/DrawRectangleMode.ts
+++ src/lib/mapbox/DrawRectangleMode.ts
@@ -6,7 +6,7 @@ import {
   MapTouchEvent,
   DrawPolygon,
 } from '@mapbox/mapbox-gl-draw'
-import { GeoJSON } from 'geojson'
+import type { GeoJSON } from 'geojson'
 
 interface State {
   startPoint?: [number, number]
@@ -16,36 +16,19 @@ interface State {
 
 const doubleClickZoom = {
   enable: (ctx: any) => {
-    // library type definition is not complete
     setTimeout(() => {
-      // First check we've got a map and some context.
-      if (
-        !ctx.map ||
-        !ctx.map.doubleClickZoom ||
-        !ctx._ctx ||
-        !ctx._ctx.store ||
-        !ctx._ctx.store.getInitialConfigValue
-      )
-        return
-
-      // Now check initial state wasn't false (we leave it disabled if so)
-      if (!ctx._ctx.store.getInitialConfigValue('doubleClickZoom')) return
-      ctx.map.doubleClickZoom.enable()
+      ctx?.map?.doubleClickZoom?.enable?.()
     })
   },
   disable: (ctx: DrawCustomModeThis) => {
     setTimeout(() => {
-      if (!ctx.map || !ctx.map.doubleClickZoom) return
-
-      // Always disable here, as it's necessary in some cases.
-      ctx.map.doubleClickZoom.disable()
+      ctx?.map?.doubleClickZoom?.disable?.()
     })
   },
 }
 
 const DrawRectangle: DrawCustomMode = {
-  // When the mode starts this function will be called.
-  onSetup: function (this: DrawCustomModeThis, opts: any) {
+  onSetup(this: DrawCustomModeThis, opts: any) {
     const rectangle = this.newFeature({
       type: 'Feature',
       properties: {
@@ -69,19 +52,16 @@ const DrawRectangle: DrawCustomMode = {
       rectangle,
     }
   },
-  // support mobile taps
-  onTap: function (this: any, state: State, e: MapTouchEvent) {
-    // library type definition is not complete
-    // emulate 'move mouse' to update feature coords
-    if (state.startPoint) this.onMouseMove(state, e)
 
-    // emulate onClick
+  onTap(this: any, state: State, e: MapTouchEvent) {
+    if (state.startPoint) {
+      this.onMouseMove(state, e)
+    }
+
     this.onClick(state, e)
   },
-  // Whenever a user clicks on the map, Draw will call `onClick`
-  onClick: function (this: DrawCustomModeThis, state: State, e: MapMouseEvent) {
-    // if state.startPoint exist, means its second click
-    // change to simple_select mode
+
+  onClick(this: DrawCustomModeThis, state: State, e: MapMouseEvent) {
     if (
       state.startPoint &&
       (state.startPoint[0] !== e.lngLat.lng ||
@@ -92,65 +72,53 @@ const DrawRectangle: DrawCustomMode = {
       this.changeMode('simple_select', { featuresId: state.rectangle.id })
     }
 
-    // on the first click, save clicked point coords as starting for the rectangle
-    const startPoint: [number, number] = [e.lngLat.lng, e.lngLat.lat]
-    state.startPoint = startPoint
+    state.startPoint = [e.lngLat.lng, e.lngLat.lat]
   },
-  onMouseMove: function (
-    this: DrawCustomModeThis,
-    state: State,
-    e: MapMouseEvent,
-  ) {
-    // if startPoint, update the feature coordinates, using the bounding box concept
-    // we are simply using the startingPoint coordinates and the current Mouse Position
-    // coordinates to calculate the bounding box on the fly, which will be our rectangle
+
+  onMouseMove(this: DrawCustomModeThis, state: State, e: MapMouseEvent) {
     if (state.startPoint) {
-      state.rectangle.updateCoordinate(
-        '0.0',
-        state.startPoint[0],
-        state.startPoint[1],
-      ) // minX, minY - the starting point
-      state.rectangle.updateCoordinate('0.1', e.lngLat.lng, state.startPoint[1]) // maxX, minY
-      state.rectangle.updateCoordinate('0.2', e.lngLat.lng, e.lngLat.lat) // maxX, maxY
-      state.rectangle.updateCoordinate('0.3', state.startPoint[0], e.lngLat.lat) // minX, maxY
-      state.rectangle.updateCoordinate(
-        '0.4',
-        state.startPoint[0],
-        state.startPoint[1],
-      ) // minX, minY - ending point (equals to starting point)
+      const [minX, minY] = state.startPoint
+      const [maxX, maxY] = [e.lngLat.lng, e.lngLat.lat]
+      state.rectangle.updateCoordinate('0.0', minX, minY)
+      state.rectangle.updateCoordinate('0.1', maxX, minY)
+      state.rectangle.updateCoordinate('0.2', maxX, maxY)
+      state.rectangle.updateCoordinate('0.3', minX, maxY)
+      state.rectangle.updateCoordinate('0.4', minX, minY)
     }
   },
-  // Whenever a user clicks on a key while focused on the map, it will be sent here
-  onKeyUp: function (this: DrawCustomModeThis, state: State, e: KeyboardEvent) {
-    if (e.keyCode === 27) return this.changeMode('simple_select')
+
+  onKeyUp(this: DrawCustomModeThis, state: State, e: KeyboardEvent) {
+    if (e.keyCode === 27) {
+      this.changeMode('simple_select')
+    }
   },
-  onStop: function (this: DrawCustomModeThis, state: State) {
+
+  onStop(this: DrawCustomModeThis, state: State) {
     doubleClickZoom.enable(this)
     this.updateUIClasses({ mouse: 'none' })
     this.activateUIButton()
 
-    // check to see if we've deleted this feature
     if (
+      state.rectangle.id &&
       typeof state.rectangle.id === 'string' &&
-      this.getFeature(state.rectangle.id) === undefined
-    )
+      !this.getFeature(state.rectangle.id)
+    ) {
       return
+    }
 
-    // Remove the last added coordinate
     state.rectangle.removeCoordinate('0.4')
 
     if (state.rectangle.isValid()) {
       this.map.fire('draw.create', {
         features: [state.rectangle.toGeoJSON()],
       })
-    } else {
-      if (typeof state.rectangle.id === 'string') {
-        this.deleteFeature(state.rectangle.id, { silent: true })
-      }
+    } else if (state.rectangle.id && typeof state.rectangle.id === 'string') {
+      this.deleteFeature(state.rectangle.id, { silent: true })
       this.changeMode('simple_select', {}, { silent: true })
     }
   },
-  toDisplayFeatures: function (
+
+  toDisplayFeatures(
     this: DrawCustomModeThis,
     state: State,
     geojson: any,
@@ -159,16 +127,20 @@ const DrawRectangle: DrawCustomMode = {
     const isActivePolygon = geojson.properties.id === state.rectangle.id
     geojson.properties.active = isActivePolygon ? 'true' : 'false'
 
-    if (!isActivePolygon) return display(geojson)
-    // Only render the rectangular polygon if it has the starting point
-    if (!state.startPoint) return display(geojson)
+    if (!isActivePolygon || !state.startPoint) {
+      return display(geojson)
+    }
+
     return display(geojson)
   },
-  onTrash: function (this: DrawCustomModeThis, state: State) {
-    if (typeof state.rectangle.id === 'string') {
+
+  onTrash(this: DrawCustomModeThis, state: State) {
+    if (state.rectangle.id && typeof state.rectangle.id === 'string') {
       this.deleteFeature(state.rectangle.id, { silent: true })
     }
+
     this.changeMode('simple_select')
   },
 }
+
 export default DrawRectangle
