fd70ecf75c6bfeb65b2d0f880bc1ace50ef0d14e
Anne Markensteijn <anne.markensteijn@vortech.nl>
2023-11-15 16:07:30 +0100

    Add StreamlineLayer to a separate mapbox-gl map
    The streamline layer requires webgl2. The vue2mapbox v-mapbox cannot set
    this property when creating the map. So we create a new Map instance instead.

--- src/components/StreamlineMapboxLayer.vue
+++ src/components/StreamlineMapboxLayer.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
+  <div ref="mapboxgl-map">
     <slot></slot>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Inject, Prop, Vue } from "vue-property-decorator"
+import { Component, Prop, Vue } from "vue-property-decorator"
 import type { MapboxLayerOptions } from '@/components/AnimatedMapboxLayer.vue';
-import type { Map } from 'mapbox-gl';
+import { Map, type MapboxOptions } from 'mapbox-gl';
+import { WMSStreamlineLayer, type WMSStreamlineLayerOptions } from '@/lib/StreamLines/layer'
+import { StreamlineStyle } from "@/lib/StreamLines/render";
+
+function configureAutoAdjustSettings(layer: WMSStreamlineLayer): void {
+  // Attempt to fix frame rate issues by reducing the "quality" of the
+  // streamlines, i.e. reduce the number of substeps per frame.
+  const desiredFrameRate = 30
+  const margin = 5
+
+  let maxDisplacement = WMSStreamlineLayer.MAX_PARTICLE_DISPLACEMENT
+  const adjustFactor = 1.1
+  const maxDisplacementMin = 2
+  const maxDisplacementMax = 100
+  const adjustQuality = () => {
+    if (layer.fps < desiredFrameRate - margin) {
+      maxDisplacement = Math.min(
+        maxDisplacement * adjustFactor,
+        maxDisplacementMax
+      )
+    } else if (layer.fps > desiredFrameRate + margin) {
+      maxDisplacement = Math.max(
+        maxDisplacement / adjustFactor,
+        maxDisplacementMin
+      )
+    }
+    layer.updateVisualiserOptions({ maxDisplacement })
+  }
+  window.setInterval(adjustQuality, 100)
+}
 
 @Component({})
 export default class StreamlineMapboxLayer extends Vue {
@@ -18,12 +47,68 @@ export default class StreamlineMapboxLayer extends Vue {
   })
   layer!: MapboxLayerOptions | null
 
-  @Inject() getMap!: () => Map
-
   mapObject!: Map
+  isInitialized = false
+  currentLayer: string = ""
 
   deferredMountedTo(map: Map) {
-    this.mapObject = map
+    map.getCanvasContainer().style.visibility = 'hidden'
+    const div = this.$refs['mapboxgl-map'] as HTMLElement
+    const mapOptions: MapboxOptions = {
+      accessToken: this.$config.get('VUE_APP_MAPBOX_TOKEN'),
+      container: div,
+      style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+      minZoom: 2,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      useWebGL2: true
+    }
+    const mapGL = new Map(mapOptions)
+    mapGL.touchZoomRotate.disableRotation()
+    mapGL.touchPitch.disable()
+    this.mapObject = mapGL
+
+    this.mapObject.once("load", () => {
+      this.isInitialized = true
+      this.createLayer()
+    })
+  }
+
+  createLayer(): void {
+    if (!this.isInitialized) return
+
+    if (this.layer === null) return
+
+    if (this.layer.name === undefined || this.layer.time === undefined) {
+      return
+    }
+
+    if (this.layer.name !== this.currentLayer) {
+      this.currentLayer = this.layer.name
+      const layerOptions: WMSStreamlineLayerOptions = {
+        baseUrl: `${this.$config.get("VUE_APP_FEWS_WEBSERVICES_URL")}/wms`,
+        layer: this.currentLayer,
+        style: 'Select a style',
+        downsampleFactorWMS: 4,
+        streamlineStyle: StreamlineStyle.LightParticlesOnMagnitude,
+        numParticles: 10000,
+        particleSize: 4,
+        speedFactor: 0.2,
+        fadeAmountPerSecond: 3
+      }
+      const streamlineLayer = new WMSStreamlineLayer(layerOptions)
+      streamlineLayer.on('load', () => {
+        configureAutoAdjustSettings(streamlineLayer)
+      })
+      this.mapObject.addLayer(streamlineLayer)
+    }
   }
 }
 </script>
+
+<style>
+.mapboxgl-map {
+  height: 100%;
+  width: 100%;
+}
+</style>
