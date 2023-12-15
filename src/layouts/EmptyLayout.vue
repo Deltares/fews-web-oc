a6f5c4495f4a3b77c152a4faa3e547af3ae1a1f4
Dirk van Meeuwen <dirk.vanmeeuwen@vortech.nl>
2023-12-15 09:55:20 +0100

    Add font-family to custom style sheet

--- src/layouts/EmptyLayout.vue
+++ src/layouts/EmptyLayout.vue
@@ -37,7 +37,7 @@ body {
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: --var(--font-family, "Avenir, Helvetica, Arial, sans-serif");
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
