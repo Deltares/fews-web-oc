fa0b69f4050b98239b91a292aa848ec3eb628f92
Werner Kramer <werner.kramer@vortech.nl>
2023-12-18 14:02:50 +0100

    Add keyboard shortcuts, fix marks for positive elevation

--- src/components/wms/ElevationSlider.vue
+++ src/components/wms/ElevationSlider.vue
@@ -6,15 +6,15 @@
     :min="minValue"
     :marks="marks"
     :interval="interval"
-    @error="onError"
-    @update:model-value="onInputChange"
     :keydownHook="onSliderKeydown"
     hideLabel
+    :silent="true"
     direction="btt"
     tooltip="always"
     tooltipPlacement="left"
     height="200px"
     ref="sliderComponent"
+    @update:model-value="onInputChange"
   >
     <template v-slot:tooltip>
       <div
@@ -39,14 +39,12 @@
 </template>
 
 <script setup lang="ts">
-import { computed, nextTick, ref, onMounted, watch } from 'vue'
+import { computed, nextTick, ref, watch } from 'vue'
 import { useDebounceFn } from '@vueuse/core'
 import VueSlider from 'vue-slider-component'
 import 'vue-slider-component/theme/antd.css'
-
-function roundToNearest100(x: number) {
-  return Math.round(x / 100) * 100
-}
+import { watchEffect } from 'vue'
+import { scaleLinear } from 'd3-scale'
 
 // Get decimal places of float (e.g. floatPrecision(54.6545) == 4)
 function floatPrecision(a: number) {
@@ -84,19 +82,6 @@ const isEditing = ref(false)
 const tooltipInput = ref<HTMLElement>()
 const sliderComponent = ref<typeof VueSlider>()
 
-const stepSize = (props.maxValue - props.minValue) / numberOfMarks
-
-const onSliderKeydown = (e: KeyboardEvent) => {
-  if (e.key === 'Enter') {
-    activateEdit()
-  }
-  return true
-}
-
-const onError = (err: Error) => {
-  console.error(err)
-}
-
 const onInputChange = (value: number) => {
   currentValue.value = value
   emitModelValue(value)
@@ -112,6 +97,26 @@ const onKeydown = (e: KeyboardEvent) => {
   } else if (e.key === 'Escape') {
     closeTooltip()
   }
+}
+
+function onSliderKeydown(e: KeyboardEvent) {
+  let newValue: number | undefined = 0
+  switch (e.key) {
+    case 'ArrowRight':
+    case 'ArrowUp':
+      newValue = marks.value.find((value) => value > currentValue.value)
+      onInputChange(newValue ?? marks.value[marks.value.length - 1])
+      break
+    case 'ArrowLeft':
+    case 'ArrowDown':
+      newValue = marks.value.findLast((value) => value < currentValue.value)
+      onInputChange(newValue ?? marks.value[0])
+      break
+    case 'Enter':
+      activateEdit()
+      break
+  }
+  return false
 }
 
 const activateEdit = () => {
@@ -144,14 +149,6 @@ const acceptEdit = () => {
   closeTooltip()
 }
 
-onMounted(() => {
-  const innerMarks = Array.from(
-    { length: numberOfMarks - 1 },
-    (_, i) => (i + 1) * -roundToNearest100(stepSize),
-  )
-  marks.value = [props.maxValue, ...innerMarks, props.minValue]
-})
-
 const interval = computed(() => {
   return 10 ** -floatPrecision(props.maxValue - props.minValue)
 })
@@ -161,6 +158,15 @@ const onValueChange = () => {
 }
 
 watch(() => props.modelValue, onValueChange)
+
+watchEffect(() => {
+  const scale = scaleLinear(
+    [props.minValue, props.maxValue],
+    [props.minValue, props.maxValue],
+  )
+  const innerMarks = scale.ticks(numberOfMarks)
+  marks.value = [props.minValue, ...innerMarks, props.maxValue]
+})
 </script>
 
 <style scoped>
