46bf0a53bf43d6a02589394b78f5a7d014e56a4d
Werner Kramer <werner.kramer@vortech.nl>
2024-01-12 11:26:35 +0100

    Rest time series object

--- src/services/useTimeSeries/index.ts
+++ src/services/useTimeSeries/index.ts
@@ -11,6 +11,7 @@ import { DateTime, Interval } from 'luxon'
 import { Series } from '../../lib/timeseries/timeSeries'
 import { SeriesUrlRequest } from '../../lib/timeseries/timeSeriesResource'
 import { createTransformRequestFn } from '@/lib/requests/transformRequest'
+import { difference } from 'lodash-es'
 
 export interface UseTimeSeriesReturn {
   error: Ref<any>
