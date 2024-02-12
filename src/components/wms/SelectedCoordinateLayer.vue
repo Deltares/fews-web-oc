5766c3018b35e41cd5cccb3082d03f018eeca05a
Werner Kramer <werner.kramer@vortech.nl>
2024-02-12 20:56:18 +0100

    Convert SelectedCoordinateLayer

--- src/components/wms/SelectedCoordinateLayer.vue
+++ src/components/wms/SelectedCoordinateLayer.vue
@@ -1,12 +1,14 @@
 <template>
-  <MapboxLayer id="selected-coordinate-layer" :options="defaultLayerOptions" />
+  <mgl-geo-json-source :source-id="sourceId" :data="sourceData">
+    <mgl-circle-layer :layer-id="layerId" :paint="paintSpecification" />
+  </mgl-geo-json-source>
 </template>
 
 <script setup lang="ts">
-import { MapboxLayer, useMap } from '@studiometa/vue-mapbox-gl'
+import { MglGeoJsonSource, MglCircleLayer, useMap } from 'vue-maplibre-gl'
 import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson'
-import { Map, type CircleLayer, GeoJSONSource } from 'mapbox-gl'
-import { Ref, watch, computed } from 'vue'
+import { GeoJSONSource } from 'maplibre-gl'
+import { watch, computed } from 'vue'
 
 interface Props {
   latitude?: string
@@ -15,13 +17,16 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const { map } = useMap() as { map: Ref<Map> }
+const { map } = useMap()
 
 const emptyFeatureCollection: FeatureCollection<Geometry, GeoJsonProperties> = {
   type: 'FeatureCollection',
   features: [],
 }
 const layerSourceId = 'selected-coordinate-layer'
+
+const layerId = 'current-location-layer'
+const sourceId = 'current-location-source'
 
 const sourceData = computed<
   | GeoJSON.Feature<GeoJSON.Geometry>
@@ -42,25 +47,17 @@ const sourceData = computed<
 })
 
 watch(sourceData, () => {
-  const source = map.value.getSource(layerSourceId) as GeoJSONSource
-  if (source) source.setData(sourceData.value)
+  if (map) {
+    const source = map.getSource(layerSourceId) as GeoJSONSource
+    if (source) source.setData(sourceData.value)
+  }
 })
 
-const defaultLayerOptions: CircleLayer = {
-  id: layerSourceId,
-  type: 'circle',
-  source: {
-    type: 'geojson',
-    data: sourceData.value,
-  },
-  layout: {
-    visibility: 'visible',
-  },
-  paint: {
-    'circle-radius': 8,
-    'circle-color': 'blue',
-    'circle-stroke-color': 'white',
-    'circle-stroke-width': 2.5,
-  },
+const paintSpecification = {
+  'circle-radius': 5,
+  'circle-color': '#dfdfdf',
+  'circle-opacity': 0.75,
+  'circle-stroke-color': 'black',
+  'circle-stroke-width': 1.5,
 }
 </script>
