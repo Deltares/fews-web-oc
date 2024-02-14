29420b5790b51fe8f1ee76fa482477d266d1298b
hvangeffen <hunter.vangeffen@vortech.nl>
2024-02-14 09:57:34 +0100

    Set content security policy header for vite server

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+const contentSecurityPolicy = [
+  `default-src 'self'`,
+  `script-src 'self'`,
+  `font-src 'self' https://*.basemaps.cartocdn.com`,
+  `style-src 'self' https://api.mapbox.com 'unsafe-inline'`,
+  `worker-src blob:`,
+  `img-src 'self' data: blob: rwsos-dataservices-ont.avi.deltares.nl`,
+  `child-src blob:`,
+  `connect-src 'self' https://basemaps.cartocdn.com https://*.basemaps.cartocdn.com https://*.mapbox.com https://rwsos-dataservices-ont.avi.deltares.nl`,
+]
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    headers: {
+      'content-security-policy': contentSecurityPolicy.join('; '),
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
