9494760bbf3df0eb26e62ce309f4aaff85a15a5a
hvangeffen <hunter.vangeffen@vortech.nl>
2024-02-14 10:40:14 +0100

    Get fews url from env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,43 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-const contentSecurityPolicy = [
-  `default-src 'self'`,
-  `script-src 'self'`,
-  `font-src 'self' https://*.basemaps.cartocdn.com`,
-  `style-src 'self' https://api.mapbox.com 'unsafe-inline'`,
-  `worker-src blob:`,
-  `img-src 'self' data: blob: rwsos-dataservices-ont.avi.deltares.nl`,
-  `child-src blob:`,
-  `connect-src 'self' https://basemaps.cartocdn.com https://*.basemaps.cartocdn.com https://*.mapbox.com https://rwsos-dataservices-ont.avi.deltares.nl`,
-]
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    headers: {
-      'content-security-policy': contentSecurityPolicy.join('; '),
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag === 'schematic-status-display',
-          // ...
-        },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      headers: {
+        'content-security-policy': [
+          `default-src 'self'`,
+          `script-src 'self'`,
+          `font-src 'self' https://*.basemaps.cartocdn.com`,
+          `style-src 'self' https://api.mapbox.com 'unsafe-inline'`,
+          `worker-src blob:`,
+          `img-src 'self' data: blob: ${env.VITE_FEWS_WEBSERVICES_URL}`,
+          `child-src blob:`,
+          `connect-src 'self' https://basemaps.cartocdn.com https://*.basemaps.cartocdn.com https://*.mapbox.com ${env.VITE_FEWS_WEBSERVICES_URL}`,
+        ].join('; '),
       },
-    }),
-  ],
-  optimizeDeps: {
-    exclude: ['@deltares/fews-ssd-webcomponent'],
-  },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag === 'schematic-status-display',
+            // ...
+          },
+        },
+      }),
+    ],
+    optimizeDeps: {
+      exclude: ['@deltares/fews-ssd-webcomponent'],
+    },
+  }
 })
