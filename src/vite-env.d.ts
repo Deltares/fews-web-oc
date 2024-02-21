8be2a98270a56d42c2c17c42226f35b16f6c664b
Werner Kramer <werner.kramer@vortech.nl>
2024-02-21 16:52:54 +0100

    Add git hash and build date

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="vite/client" />
 
-declare const __COMMIT_HASH__: string
+declare const __GIT_HASH__: string
+declare const __GIT_TAG__: string
+declare const __BUILD_DATE__: string
