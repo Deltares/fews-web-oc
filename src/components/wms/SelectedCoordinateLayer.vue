5db6eb1e8889d2a687f5475ccb0973b39383ea72
hvangeffen <hunter.vangeffen@vortech.nl>
2024-02-27 11:11:24 +0100

    Let selected coordinate layer accept geojson

--- src/components/wms/SelectedCoordinateLayer.vue
+++ src/components/wms/SelectedCoordinateLayer.vue
@@ -1,52 +1,32 @@
 <template>
-  <mgl-geo-json-source :source-id="sourceId" :data="sourceData">
+  <mgl-geo-json-source :source-id="sourceId" :data="props.geoJson">
     <mgl-circle-layer :layer-id="layerId" :paint="paintSpecification" />
   </mgl-geo-json-source>
 </template>
 
 <script setup lang="ts">
 import { MglGeoJsonSource, MglCircleLayer } from 'vue-maplibre-gl'
-import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson'
-import { computed } from 'vue'
 
 interface Props {
-  latitude?: string
-  longitude?: string
+  geoJson?:
+    | GeoJSON.Feature<GeoJSON.Geometry>
+    | GeoJSON.FeatureCollection<GeoJSON.Geometry>
 }
 
-const props = defineProps<Props>()
-
-const emptyFeatureCollection: FeatureCollection<Geometry, GeoJsonProperties> = {
-  type: 'FeatureCollection',
-  features: [],
-}
+const props = withDefaults(defineProps<Props>(), {
+  geoJson: () => ({
+    type: 'FeatureCollection',
+    features: [],
+  }),
+})
 
 const layerId = 'selected-coordinate-layer'
 const sourceId = 'selected-coordinate-source'
 
-const sourceData = computed<
-  | GeoJSON.Feature<GeoJSON.Geometry>
-  | GeoJSON.FeatureCollection<GeoJSON.Geometry>
->(() => {
-  if (props.latitude && props.longitude) {
-    return {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [+props.longitude, +props.latitude],
-      },
-      properties: {},
-    }
-  }
-
-  return emptyFeatureCollection
-})
-
 const paintSpecification = {
-  'circle-radius': 5,
-  'circle-color': '#dfdfdf',
-  'circle-opacity': 0.75,
-  'circle-stroke-color': 'black',
-  'circle-stroke-width': 1.5,
+  'circle-radius': 8,
+  'circle-color': 'blue',
+  'circle-stroke-color': 'white',
+  'circle-stroke-width': 2.5,
 }
 </script>
