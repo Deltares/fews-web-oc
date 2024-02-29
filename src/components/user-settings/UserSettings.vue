e1f719bcb4f9536c8cf9b1b2cb04d1752e3ca2c9
Werner Kramer <werner.kramer@vortech.nl>
2024-02-29 13:37:24 +0100

    Reduce UI element size

--- src/components/user-settings/UserSettings.vue
+++ src/components/user-settings/UserSettings.vue
@@ -1,74 +1,63 @@
 <template>
   <div v-for="group in groups" :key="group" class="mb-2">
-    <div class="text-h5">{{ group }}</div>
-    <v-card density="compact">
-      <v-container>
-        <v-row
-          v-for="(setting, index) in store.listByGroup(group)"
-          :key="index"
-          :disabled="setting?.disabled"
+    <v-list-subheader>{{ group }}</v-list-subheader>
+    <v-list-item
+      v-for="(setting, index) in store.listByGroup(group)"
+      :key="index"
+      :disabled="setting?.disabled"
+    >
+      <v-select
+        v-if="setting.type === 'oneOfMultiple'"
+        :label="setting.label"
+        v-model="setting.value"
+        :items="setting.items"
+        :disabled="setting.disabled"
+        variant="solo-filled"
+        density="compact"
+        item-title="value"
+        item-value="value"
+        :item-props="true"
+        flat
+        hide-details
+        @click.preventDefault
+        @update:modelValue="onValueChange(setting)"
+      >
+        <template #item="{ props }">
+          <v-list-item v-bind="props" :disabled="props?.disabled === true">
+            <template #prepend>
+              <v-icon>
+                {{ props.icon }}
+              </v-icon>
+            </template>
+          </v-list-item>
+        </template>
+      </v-select>
+      <v-switch
+        v-else-if="setting.type === 'boolean'"
+        v-model="setting.value"
+        color="primary"
+        :label="setting.label"
+        :disabled="setting.disabled"
+        hide-details
+        @update:modelValue="onValueChange(setting)"
+        class="d-flex justify-end"
+      >
+      </v-switch>
+      <template v-slot:append>
+        <v-btn
+          color="grey-lighten-1"
+          icon="mdi-information"
+          variant="text"
+          class="flex-0-0 align-self-center"
+          @click="onFavoriteChange(setting)"
         >
-          <v-col class="d-flex align-self-center" cols="5">
-            {{ setting.label }}
-          </v-col>
-          <v-col class="d-flex justify-end py-1">
-            <v-select
-              v-if="setting.type === 'oneOfMultiple'"
-              :label="setting.label"
-              v-model="setting.value"
-              :items="setting.items"
-              :disabled="setting.disabled"
-              variant="solo-filled"
-              density="compact"
-              item-title="value"
-              item-value="value"
-              :item-props="true"
-              hide-details
-              single-line
-              flat
-              @update:modelValue="onValueChange(setting)"
-            >
-              <template #item="{ props }">
-                <v-list-item
-                  v-bind="props"
-                  :disabled="props?.disabled === true"
-                >
-                  <template #prepend>
-                    <v-icon>
-                      {{ props.icon }}
-                    </v-icon>
-                  </template>
-                </v-list-item>
-              </template>
-            </v-select>
-            <v-switch
-              v-else-if="setting.type === 'boolean'"
-              v-model="setting.value"
-              color="primary"
-              :disabled="setting.disabled"
-              hide-details
-              @update:modelValue="onValueChange(setting)"
-              class="d-flex justify-end"
-            >
-            </v-switch>
-            <v-btn
-              color="grey-lighten-1"
-              icon="mdi-information"
-              variant="text"
-              class="flex-0-0 align-self-center"
-              @click="onFavoriteChange(setting)"
-            >
-              <v-icon>{{
-                setting.favorite ? 'mdi-star' : 'mdi-star-outline'
-              }}</v-icon>
-              <v-tooltip activator="parent" location="top">
-                Favorite
-              </v-tooltip>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+          <v-icon>{{
+            setting.favorite ? 'mdi-star' : 'mdi-star-outline'
+          }}</v-icon>
+          <v-tooltip activator="parent" location="top"> Favorite </v-tooltip>
+        </v-btn>
+      </template>
+    </v-list-item>
   </div>
 </template>
 
