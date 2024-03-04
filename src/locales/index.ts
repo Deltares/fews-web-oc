7a30ab311217aaf4132c5389b8332d4030a76c15
Martina Pippi <martina.pippi@vortech.nl>
2024-03-04 16:09:33 +0100

    add datetimeFormats i18n

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,6 +1,7 @@
 import { en, nl } from 'vuetify/locale'
 import enCustom from './en.json'
 import nlCustom from './nl.json'
+import type { DateTimeFormat } from '@intlify/core-base'
 
 export const messages = {
   en: {
@@ -16,3 +17,37 @@ export const messages = {
     ...nlCustom,
   },
 }
+
+export const datetimeFormats: Record<keyof typeof messages, DateTimeFormat> = {
+  en: {
+    time: {
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZoneName: 'short',
+    },
+    datetimeslider: {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true,
+    },
+  },
+  nl: {
+    time: {
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZoneName: 'short',
+    },
+    datetimeslider: {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    },
+  },
+} as const
