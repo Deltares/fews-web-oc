8d02657313ac0077a928c9eb3659bbaf7b383cf5
Werner Kramer <werner.kramer@vortech.nl>
2024-03-15 09:09:09 +0100

    Add useBaseLayer composable

---
+++ src/services/useBaseLayers/index.ts
@@ -0,0 +1,28 @@
+
+import type { ShallowRef } from 'vue'
+import { shallowRef } from 'vue'
+import Basemaps from '@/assets/base-layers.json'
+import { MglDefaults } from 'vue-maplibre-gl'
+
+
+export interface UseBaseLayersReturn {
+  baseLayerStyle: ShallowRef<string|object>
+  baseLayers: ShallowRef<(string|object)[]>
+}
+
+export function useBaseLayers(): UseBaseLayersReturn {
+
+  const baseLayerStyle = shallowRef<string|object>(MglDefaults.style)
+  const baseLayers = shallowRef<(string|object)[]>(Basemaps.baseLayers)
+  const defaultLayerId = Basemaps.default
+  const defaultLayer = Basemaps.baseLayers.find((layer: any) => layer.id === defaultLayerId)
+  if ( defaultLayer?.style) {
+    console.log('defaultLayer.style', defaultLayer.style)
+    baseLayerStyle.value = defaultLayer.style
+  }
+
+  return {
+    baseLayerStyle,
+    baseLayers
+  }
+}
