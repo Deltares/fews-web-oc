1d50d4cdab22c50630d7971da8fb207f3b6b17d7
Anne Markensteijn <anne.markensteijn@vortech.nl>
2024-03-26 16:09:23 +0100

    Use icon instead of circle to indicate locations
    Use a different icon to highlight the selected location, since a different icon-color only works for sdf icons

--- src/components/wms/LocationsLayer.vue
+++ src/components/wms/LocationsLayer.vue
@@ -3,15 +3,16 @@
     :source-id="locationsSourceId"
     :data="props.locationsGeoJson"
   >
-    <mgl-circle-layer
+    <mgl-symbol-layer
       :layer-id="locationsLayerId"
+      :layout="layoutSpecification"
       :paint="paintSpecification"
     />
   </mgl-geo-json-source>
 </template>
 
 <script setup lang="ts">
-import { MglCircleLayer, MglGeoJsonSource, useMap } from 'vue-maplibre-gl'
+import { MglSymbolLayer, MglGeoJsonSource, useMap } from 'vue-maplibre-gl'
 import { FeatureCollection, Geometry } from 'geojson'
 import { type Location } from '@deltares/fews-pi-requests'
 import {
@@ -37,15 +38,34 @@ const props = withDefaults(defineProps<Props>(), {
 
 const emit = defineEmits(['click'])
 
+const defaultIconSize = 0.35
+
+const layoutSpecification = {
+  'icon-size': defaultIconSize,
+  'icon-allow-overlap': true,
+  'symbol-sort-key': 1,
+}
+
 const paintSpecification = {
-  'circle-radius': 5,
-  'circle-color': '#dfdfdf',
-  'circle-opacity': 0.75,
-  'circle-stroke-color': 'black',
-  'circle-stroke-width': 1.5,
+  'icon-opacity': 0.75,
 }
 
 const { map } = useMap()
+
+if (map) {
+  map.on('load', async () => {
+    map.loadImage('/images/favicon.ico', function (error, image) {
+      if (error) throw error
+      if (!map.hasImage('favicon') && image !== undefined && image !== null)
+        map.addImage('favicon', image)
+    })
+    map.loadImage('/images/logo.png', function (error, image) {
+      if (error) throw error
+      if (!map.hasImage('logo') && image !== undefined && image !== null)
+        map.addImage('logo', image)
+    })
+  })
+}
 
 const locationsLayerId = 'location-layer'
 const locationsSourceId = 'location-source'
@@ -102,34 +122,27 @@ watch(
 function highlightSelectedLocationOnMap() {
   if (!map?.getSource(locationsSourceId)) return
   const locationId = props.selectedLocationId ?? 'noLayerSelected'
-  map.setPaintProperty(locationsLayerId, 'circle-color', [
+  map.setLayoutProperty(locationsLayerId, 'icon-image', [
     'match',
     ['get', 'locationId'],
     locationId,
-    '#0c1e38', // color for selected location
-    '#dfdfdf', // default color
+    'logo', // icon for selected location
+    'favicon', // default icon
   ])
-  map.setPaintProperty(locationsLayerId, 'circle-stroke-color', [
+  map.setLayoutProperty(locationsLayerId, 'icon-size', [
     'match',
     ['get', 'locationId'],
     locationId,
-    'white', // stroke color for selected location
-    'black', // default stroke color
+    defaultIconSize * 1.1, // size for selected location
+    defaultIconSize, // default size
   ])
-  map.setPaintProperty(locationsLayerId, 'circle-radius', [
+  map.setLayoutProperty(locationsLayerId, 'symbol-sort-key', [
     'match',
     ['get', 'locationId'],
     locationId,
-    7, // radius for selected location
-    5, // default radius
-  ]),
-    map.setLayoutProperty(locationsLayerId, 'circle-sort-key', [
-      'match',
-      ['get', 'locationId'],
-      locationId,
-      2,
-      1,
-    ])
+    2, // sort key for selected location
+    1, // default sort key
+  ])
 }
 
 function onLocationClick(event: MapLayerMouseEvent | MapLayerTouchEvent): void {
