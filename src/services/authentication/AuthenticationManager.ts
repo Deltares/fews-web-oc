7832404b2f6f6529062e0b80d95dfe010a77e383
ekkelenk <rudie.ekkelenkamp@deltares.nl>
2024-03-27 14:31:25 +0100

    Formatter.

--- src/services/authentication/AuthenticationManager.ts
+++ src/services/authentication/AuthenticationManager.ts
@@ -18,7 +18,11 @@ export class AuthenticationManager {
   }
 
   public getAccessToken(): string {
-    if (configManager.get('VITE_REQUEST_HEADER_AUTHORIZATION') !== RequestHeaderAuthorization.BEARER) return ''
+    if (
+      configManager.get('VITE_REQUEST_HEADER_AUTHORIZATION') !==
+      RequestHeaderAuthorization.BEARER
+    )
+      return ''
     if (this.user !== null) {
       return this.user.access_token
     }
