261f0b003abee6733e1c3ad738facb76754afab0
Cees Voesenek <cees.voesenek@vortech.nl>
2024-04-17 11:49:26 +0200

    Load SVG icons as bitmaps before adding them to the map

--- src/lib/location-icons/load.ts
+++ src/lib/location-icons/load.ts
@@ -22,6 +22,29 @@ function addDefaultSelectedLocationIconToMap(map: Map): void {
   })
 }
 
+async function convertSvgToBitmap(
+  url: string,
+  width: number,
+  height: number,
+): Promise<ImageBitmap> {
+  // We cannot directly set the SVG as the image source to to CSP headers, so
+  // we load the SVG as a blob, then create a data URL and use that as the image
+  // source.
+  const response = await fetch(url)
+  const blob = await response.blob()
+  const imageUrl = URL.createObjectURL(blob)
+
+  const image = new Image(width, height)
+  image.src = imageUrl
+  await image.decode()
+
+  // Finally, we create an image bitmap from the image and free the data URL.
+  const bitmap = await createImageBitmap(image)
+  URL.revokeObjectURL(imageUrl)
+
+  return bitmap
+}
+
 function addCustomLocationIconsToMap(
   map: Map,
   locations: FeatureCollection<Geometry, Location>,
@@ -30,10 +53,20 @@ function addCustomLocationIconsToMap(
   for (const iconName of locationIcons) {
     if (map.hasImage(iconName)) continue
 
-    map.loadImage(getResourcesIconsUrl(iconName), function (error, image) {
-      if (error) throw error
-      if (image) map.addImage(iconName, image)
-    })
+    const url = getResourcesIconsUrl(iconName)
+
+    if (url.endsWith('.svg')) {
+      // Load SVG as 128x128 bitmap.
+      convertSvgToBitmap(url, 128, 128).then((image) => {
+        map.addImage(iconName, image)
+      })
+    } else {
+      // Load bitmap image.
+      map.loadImage(url, function (error, image) {
+        if (error) throw error
+        if (image) map.addImage(iconName, image)
+      })
+    }
   }
 }
 
