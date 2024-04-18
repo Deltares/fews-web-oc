12f16cf7517032dbe9005cf702478cbf3dcded72
hvangeffen <hunter.vangeffen@vortech.nl>
2024-04-18 16:14:52 +0200

    Add check for equidistant

--- src/lib/timeseries/types/SeriesHeader.ts
+++ src/lib/timeseries/types/SeriesHeader.ts
@@ -1,7 +1,10 @@
+import { TimeStep } from "@deltares/fews-pi-requests"
+
 export interface SeriesHeader {
   name?: string
   location?: string
   parameter?: string
   source?: string
   unit?: string
+  timeStep?: TimeStep
 }
