33218e4839ba3f3087d29055d75347dfc5010be2
hvangeffen <hunter.vangeffen@vortech.nl>
2024-04-26 11:27:36 +0200

    Fix formatting

--- src/lib/utils/resolveCSSVariable.ts
+++ src/lib/utils/resolveCSSVariable.ts
@@ -1,8 +1,8 @@
 /**
  * Resolves a CSS variable to its value
-  * @param {string} cssVariable - The CSS variable to resolve.
-  * @returns {string} The value of the CSS variable.
-  */
+ * @param {string} cssVariable - The CSS variable to resolve.
+ * @returns {string} The value of the CSS variable.
+ */
 export function resolveCSSVariable(cssVariable: string): string {
   const property = cssVariable.replace('var(', '').replace(')', '')
   return getComputedStyle(document.documentElement).getPropertyValue(property)
