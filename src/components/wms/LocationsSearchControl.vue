9ce4de0de30f6eb0f3bd898965cd2d51b29ecef2
Werner Kramer <werner.kramer@vortech.nl>
2024-05-07 07:17:18 +0200

    Global search component for locations

--- src/components/wms/LocationsSearchControl.vue
+++ src/components/wms/LocationsSearchControl.vue
@@ -1,66 +1,39 @@
 <template>
-  <v-tooltip
-    :open-delay="tooltipOpenDelay"
-    :disabled="isExpanded || !selectedLocationId"
+  <v-chip
+    v-if="hasLocations"
+    class="locations-control__chip"
+    :class="{ 'pr-0': showLocations }"
+    pill
+    label
   >
-    <template #activator="{ props: autocompleteProps }">
-      <v-chip
-        v-if="hasLocations"
-        class="locations-layer__chip"
-        :class="{ 'pr-0': showLocations }"
-        pill
-        label
-      >
-        <v-btn
-          @click="showLocations = !showLocations"
-          density="compact"
-          variant="plain"
-          icon
-        >
-          <v-icon>{{
-            showLocations ? 'mdi-map-marker' : 'mdi-map-marker-off'
-          }}</v-icon>
-        </v-btn>
-        <v-autocomplete
-          v-if="showLocations"
-          v-bind="autocompleteProps"
-          class="locations-search"
-          v-model="selectedLocation"
-          label="Search Locations"
-          single-line
-          hide-details
-          rounded="0"
-          :items="props.locations"
-          item-title="locationName"
-          item-value="locationId"
-          return-object
-          :style="autocompleteStyle"
-          density="compact"
-          @update:focused="(isFocused) => (isExpanded = isFocused)"
-        >
-          <template #item="{ item, props }">
-            <v-tooltip :open-delay="tooltipOpenDelay">
-              <template #activator="{ props: itemProps }">
-                <v-list-item
-                  v-bind="{ ...itemProps, ...props }"
-                  :width="width"
-                  :max-width="maxWidth"
-                  density="compact"
-                />
-              </template>
-              <span>{{ getTooltipTextFromId(item.value) }}</span>
-            </v-tooltip>
-          </template>
-        </v-autocomplete>
-      </v-chip>
-    </template>
-    <span>{{ getTooltipText(selectedLocation) }}</span>
-  </v-tooltip>
+    <v-btn
+      @click="showLocations = !showLocations"
+      density="compact"
+      variant="plain"
+      icon
+    >
+      <v-icon>{{
+        showLocations ? 'mdi-map-marker' : 'mdi-map-marker-off'
+      }}</v-icon>
+    </v-btn>
+
+    <v-btn
+      variant="text"
+      v-if="showLocations"
+      class="locations-search text-capitalize"
+      hide-details
+      @click="showLocationsSearch"
+      >{{
+        selectedLocationId ? selectedLocationId : 'Search locations'
+      }}</v-btn
+    >
+  </v-chip>
 </template>
 
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue'
 import { type Location } from '@deltares/fews-pi-requests'
+import { useGlobalSearchState } from '@/stores/globalSearch'
 
 interface Props {
   locations?: Location[]
@@ -76,34 +49,16 @@ const props = withDefaults(defineProps<Props>(), {
   tooltipOpenDelay: 500,
 })
 
-const showLocations = defineModel<boolean>('showLocations', { default: true})
+const showLocations = defineModel<boolean>('showLocations', { default: true })
+
+const state = useGlobalSearchState()
 
 const emit = defineEmits(['changeLocationId'])
 
-const isExpanded = ref(false)
 const selectedLocation = ref<Location | null>(null)
 
 const hasLocations = computed(() => {
   return props.locations?.length
-})
-
-function convertWidthToCss(inputWidth: number | string | undefined) {
-  if (inputWidth === undefined) return undefined
-
-  let width = inputWidth
-  if (typeof width === 'string') {
-    // Try to parse width as a number, if it fails, assume it's a CSS string.
-    const numericWidth = Number(width)
-    if (!isNaN(numericWidth)) width = numericWidth
-  }
-
-  return typeof width === 'number' ? `${width}px` : width
-}
-const autocompleteStyle = computed(() => {
-  return {
-    width: convertWidthToCss(props.width),
-    'max-width': convertWidthToCss(props.maxWidth),
-  }
 })
 
 function getLocationFromId(locationId: string | null) {
@@ -114,36 +69,11 @@ function getLocationFromId(locationId: string | null) {
   )
 }
 
-function degreesToString(raw: number): string {
-  let remainder = raw
-
-  const degrees = Math.floor(raw)
-  remainder -= degrees
-
-  remainder *= 60
-  const minutes = Math.floor(remainder)
-  remainder -= minutes
-
-  const seconds = (remainder * 60).toFixed(2)
-
-  return `${degrees}°${minutes}'${seconds}''`
-}
-
-function getTooltipText(location: Location | null): string {
-  if (!location) return ''
-
-  let tooltip = `${location.locationName}`
-  if (location.lat && location.lon) {
-    const lat = degreesToString(+location.lat)
-    const lon = degreesToString(+location.lon)
-    tooltip += ` (${lat} N, ${lon} E)`
-  }
-  return tooltip
-}
-
-function getTooltipTextFromId(locationId: string): string {
-  const location = getLocationFromId(locationId)
-  return getTooltipText(location)
+function showLocationsSearch() {
+  state.items = props.locations.map((l) => {
+    return { id: l.locationId, name: l.locationName ?? '' }
+  })
+  state.active = true
 }
 
 watch(
@@ -161,16 +91,7 @@ function onSelectLocation(newValue: Location | null) {
 </script>
 
 <style>
-.locations-search > .v-field__overlay {
-  display: none;
-}
-
-/* Hide horizontal scrollbar in autocomplete dropdown menu on FireFox. */
-.v-autocomplete__content > .v-list {
-  overflow-x: hidden !important;
-}
-
-.locations-layer__chip {
+.locations-control__chip {
   font-size: 0.825em;
   z-index: 1000;
   backdrop-filter: blur(5px);
