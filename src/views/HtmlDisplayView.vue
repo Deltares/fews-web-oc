c8ccf3fee5647ccee5e3d22bb5e0260170d6a578
hvangeffen <hunter.vangeffen@vortech.nl>
2024-06-10 13:28:29 +0200

    Add agree to terms dialog

---
+++ src/views/HtmlDisplayView.vue
@@ -0,0 +1,21 @@
+<template>
+  <HtmlDisplay :url="url" />
+</template>
+
+<script setup lang="ts">
+import HtmlDisplay from '@/components/general/HtmlDisplay.vue'
+import { useConfigStore } from '@/stores/config'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const configStore = useConfigStore()
+const url = computed(() => {
+  const components = Object.values(configStore.components)
+  const matchingComponent = components.find(
+    (c) => c.id === 'htmlDisplay' && c.path === route.params.path,
+  )
+  return matchingComponent?.url
+})
+</script>
