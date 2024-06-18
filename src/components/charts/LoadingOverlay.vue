7c2b296f1f5222ca0fb840ee67511b7aac3f5948
hvangeffen <hunter.vangeffen@vortech.nl>
2024-06-18 15:57:08 +0200

    Add loading overlay to chart

---
+++ src/components/charts/LoadingOverlay.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="loading-overlay">
+    <v-icon size="x-large">mdi-loading mdi-spin</v-icon>
+  </div>
+</template>
+
+<style scoped>
+.loading-overlay {
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
