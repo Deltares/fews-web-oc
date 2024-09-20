24dc86d305cc839d9f154748fa0de2b113111bd3
Werner Kramer <werner.kramer@vortech.nl>
2024-09-20 13:51:56 +0200

    Treeshake vuetify with scss variables

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,13 +1,9 @@
 // Vuetify
-import 'vuetify/styles'
+import '@/styles/main.scss'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
-import * as directives from 'vuetify/directives'
-import * as components from 'vuetify/components'
 
 const vuetify = createVuetify({
-  components,
-  directives,
 })
 
 export default vuetify
