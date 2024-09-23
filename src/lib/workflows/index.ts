4a8e4632a0a177c9c5a8c32004fdedac9b634441
hvangeffen <hunter.vangeffen@vortech.nl>
2024-09-23 15:45:03 +0200

    Use E deg N deg for lat lng string

--- src/lib/workflows/index.ts
+++ src/lib/workflows/index.ts
@@ -2,6 +2,6 @@ import { LngLat } from 'maplibre-gl'
 
 export function coordinateToString(coordinate: LngLat | null) {
   return coordinate
-    ? `Lat: ${coordinate.lat.toFixed(2)} Lng: ${coordinate.lng.toFixed(2)}`
+    ? `${coordinate.lng.toFixed(2)}°E ${coordinate.lat.toFixed(2)}°N`
     : '—'
 }
