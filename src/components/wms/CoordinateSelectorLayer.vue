dade4ae0bb7d0fd0f2a02dddbfc6a8de350e1851
hvangeffen <hunter.vangeffen@vortech.nl>
2024-09-23 15:45:36 +0200

    Use mdi-marker for workflow location marker

--- src/components/wms/CoordinateSelectorLayer.vue
+++ src/components/wms/CoordinateSelectorLayer.vue
@@ -1,5 +1,9 @@
 <template>
-  <mgl-marker v-if="coordinate" v-model:coordinates="coordinate" draggable />
+  <mgl-marker v-if="coordinate" v-model:coordinates="coordinate" draggable>
+    <template #marker>
+      <v-icon size="2rem" color="primary">mdi-map-marker</v-icon>
+    </template>
+  </mgl-marker>
 </template>
 
 <script setup lang="ts">
