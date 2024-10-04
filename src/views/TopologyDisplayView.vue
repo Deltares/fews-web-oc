23a60bbeb8c22f6e3c1342152c7bc386f4e2f381
Werner Kramer <werner.kramer@vortech.nl>
2024-10-04 10:24:52 +0200

    Remove needless undefined check

--- src/views/TopologyDisplayView.vue
+++ src/views/TopologyDisplayView.vue
@@ -305,8 +305,8 @@ watchEffect(() => {
   }
 
   // Create the displayTabs for the active node.
-  if (node === undefined) return
   displayTabs.value = displayTabsForNode(node, parentNodeIdNodeId)
+
   externalLink.value = node.url
 })
 
