c3e7404d2d07b826f96c3b589e04e4e826932c3e
Werner Kramer <werner.kramer@vortech.nl>
2024-10-25 16:06:30 +0200

    Clean up simple plot and add empty plot

--- src/views/ObservablePlotView.vue
+++ src/views/ObservablePlotView.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-select v-model="scriptName" :items="scripts"></v-select>
+  <v-select
+    label="Select plot"
+    v-model="scriptName"
+    :items="scripts"
+  ></v-select>
   <ObservablePlot :options="options" :key="key"></ObservablePlot>
 </template>
 <script setup lang="ts">
@@ -8,18 +12,19 @@ import ObservablePlot from '@/components/ObservablePlot.js'
 
 const options = ref<any>('')
 const key = ref('')
-const scripts = ref(['simple', 'soundings'])
-const scriptName = ref('simple')
+const scripts = ref(['empty', 'simple', 'soundings'])
+const scriptName = ref('empty')
 
 onMounted(onUpdateChart)
 
 watch(scriptName, onUpdateChart)
 
 async function onUpdateChart() {
-  const { default: code } = await import(`@/components/plots/${scriptName.value}.ts`)
+  const { default: code } = await import(
+    `@/components/plots/${scriptName.value}.ts`
+  )
   console.log(code)
   key.value = scriptName.value
   options.value = code
 }
-
 </script>
