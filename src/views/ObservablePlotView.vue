0d405b838fc46b9ecbd7cd1dd3bb3cc129d4b955
Werner Kramer <werner.kramer@vortech.nl>
2024-10-25 15:54:28 +0200

    Dynamic load scripts

--- src/views/ObservablePlotView.vue
+++ src/views/ObservablePlotView.vue
@@ -1,18 +1,25 @@
 <template>
+  <v-select v-model="scriptName" :items="scripts"></v-select>
   <ObservablePlot :options="options" :key="key"></ObservablePlot>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import ObservablePlot from '@/components/ObservablePlot.js'
 
 const options = ref<any>('')
-const key = ref(0)
+const key = ref('')
+const scripts = ref(['simple', 'soundings'])
+const scriptName = ref('simple')
 
-onMounted(async () => {
-  const scriptName = 'soundings'
-  const { default: code } = await import(`@/components/plots/${scriptName}.ts`)
+onMounted(onUpdateChart)
+
+watch(scriptName, onUpdateChart)
+
+async function onUpdateChart() {
+  const { default: code } = await import(`@/components/plots/${scriptName.value}.ts`)
   console.log(code)
-  key.value = 1
+  key.value = scriptName.value
   options.value = code
-})
+}
+
 </script>
