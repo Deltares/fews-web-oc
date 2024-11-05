64d7b4c8e558be1cdbde77f9bbd8cf6fa79d1aa0
Cees Voesenek <cees.voesenek@vortech.nl>
2024-11-05 15:20:14 +0100

    Tweak tasks view layout

--- src/components/tasksdisplay/TasksDisplay.vue
+++ src/components/tasksdisplay/TasksDisplay.vue
@@ -1,56 +1,89 @@
 <template>
-  <v-stepper v-model="currentStep" class="w-100" :items="steps">
-    <template #item.1>
-      <div class="d-flex flex-column">
-        <v-select
-          v-model="selectedWhatIfTemplateId"
-          :items="whatIfScenarios.templates.value"
-          item-value="id"
-          item-title="title"
-          label="Select what-if scenario template"
-          hide-details
+  <div class="w-100 h-100 pa-4 d-flex flex-column gr-2">
+    <v-stepper v-model="currentStep" class="w-100" :items="steps">
+      <template #item.1>
+        <div class="d-flex flex-column gr-4 mx-2">
+          <v-select
+            v-model="selectedWhatIfTemplateId"
+            :items="whatIfScenarios.templates.value"
+            item-value="id"
+            item-title="title"
+            label="Select what-if scenario template"
+            hide-details
+          />
+          <v-card>
+            <v-card-text>
+              <v-text-field
+                v-model="description"
+                label="Description"
+                hide-details
+              />
+              <v-checkbox
+                v-model="doApproveImmediately"
+                label="Approve immediately"
+                hide-details
+              />
+            </v-card-text>
+          </v-card>
+          <div class="d-flex flex-column align-end">
+            <ExpectedWorkflowRuntime :workflow-id="workflowId" />
+            <AvailableWorkflowServers :workflow-id="workflowId" />
+          </div>
+        </div>
+      </template>
+      <template #item.2>
+        <json-forms
+          class="pt-2"
+          :schema="whatIfScenarios.formSchemas.value?.properties"
+          :uischema="whatIfScenarios.formSchemas.value?.ui"
+          :data="selectedProperties"
+          :renderers="Object.freeze(vuetifyRenderers)"
+          :ajv="undefined"
+          validation-mode="NoValidation"
+          :config="jsonFormsConfig"
+          @change="onPropertiesChange"
         />
-        <ExpectedWorkflowRuntime :workflow-id="workflowId" />
-        <AvailableWorkflowServers :workflow-id="workflowId" />
-        <v-text-field v-model="description" label="Description" />
-      </div>
-    </template>
-    <template #item.2>
-      <json-forms
-        :schema="whatIfScenarios.formSchemas.value?.properties"
-        :uischema="whatIfScenarios.formSchemas.value?.ui"
-        :data="selectedProperties"
-        :renderers="Object.freeze(vuetifyRenderers)"
-        :ajv="undefined"
-        validation-mode="NoValidation"
-        :config="jsonFormsConfig"
-        @change="onPropertiesChange"
-      />
-    </template>
-    <template #item.3>
-      <WhatIfPropertiesSummary
-        :properties="selectedProperties"
-        :form-schemas="whatIfScenarios.formSchemas.value"
-      />
-    </template>
-    <template #actions="{ prev, next }">
-      <div class="d-flex px-6">
-        <v-btn @click="prev">Previous</v-btn>
-        <v-spacer />
-        <v-btn @click="next" :disabled="!canContinue" :color="nextButtonColor">
-          {{ nextButtonLabel }}
-        </v-btn>
-      </div>
-    </template>
-  </v-stepper>
+      </template>
+      <template #item.3>
+        <WhatIfPropertiesSummary
+          :properties="selectedProperties"
+          :form-schemas="whatIfScenarios.formSchemas.value"
+        />
+      </template>
+      <template #actions="{ prev, next }">
+        <div class="d-flex px-6 pb-6">
+          <v-btn @click="prev" :disabled="isFirstStep">Previous</v-btn>
+          <v-spacer />
+          <v-btn
+            @click="onClickNextOrSubmit(next)"
+            :disabled="!canContinue || isSubmitted"
+            :color="nextButtonColor"
+          >
+            {{ nextButtonLabel }}
+          </v-btn>
+        </div>
+      </template>
+    </v-stepper>
+    <v-alert
+      v-if="isSubmitted && !hasSubmitError"
+      class="flex-0-0"
+      type="success"
+    >
+      Task submitted successfully.
+    </v-alert>
+    <v-alert v-if="isSubmitted && hasSubmitError" class="flex-0-0" type="error">
+      Failed to submit task: {{ submitErrorMessage }}
+    </v-alert>
+  </div>
 </template>
 <script setup lang="ts">
 import jsonFormsConfig from '@/assets/JsonFormsConfig.json'
 
 import { vuetifyRenderers } from '@jsonforms/vue-vuetify'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 import {
+  SubmitStatus,
   useWhatIfScenarios,
   WhatIfProperties,
 } from '@/services/useWhatIfScenarios'
@@ -74,6 +107,7 @@ const whatIfScenarios = useWhatIfScenarios(
 )
 
 const currentStep = ref<number>(1)
+const isFirstStep = computed<boolean>(() => currentStep.value === 1)
 const isLastStep = computed<boolean>(() => currentStep.value === 3)
 const nextButtonLabel = computed<string>(() =>
   isLastStep.value ? 'Submit' : 'Next',
@@ -87,6 +121,17 @@ const workflowId = computed(
 )
 
 const description = ref<string>('')
+const doApproveImmediately = ref<boolean>(false)
+
+const isSubmitted = ref<boolean>(false)
+const hasSubmitError = ref<boolean>(false)
+const submitErrorMessage = ref<string>('')
+watch(currentStep, () => {
+  // Reset submit status if we change steps after submission; this means we can
+  // submit another (or the same...) job.
+  isSubmitted.value = false
+  submitErrorMessage.value = ''
+})
 
 // FIXME: why does json-forms not support v-model? :-(
 const selectedProperties = ref<WhatIfProperties>({})
@@ -116,4 +161,24 @@ const hasAllRequiredProperties = computed<boolean>(() => {
     (property) => property in selectedProperties.value,
   )
 })
+
+function onClickNextOrSubmit(next: () => void): void {
+  if (isLastStep.value) {
+    submit()
+  } else {
+    next()
+  }
+}
+
+async function submit(): Promise<void> {
+  const result = await whatIfScenarios.submit(selectedProperties.value)
+
+  isSubmitted.value = true
+  if (result.status === SubmitStatus.Success) {
+    hasSubmitError.value = false
+  } else {
+    hasSubmitError.value = true
+    submitErrorMessage.value = result.error
+  }
+}
 </script>
