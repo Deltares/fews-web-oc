1dd089ab3b6405bc0dca129427aa5f88a5e3cd5a
Cees Voesenek <cees.voesenek@vortech.nl>
2024-11-11 16:25:49 +0100

    Use a single runtime value instead of a range

--- src/services/useWorkflowRuntime/index.ts
+++ src/services/useWorkflowRuntime/index.ts
@@ -1,46 +1,23 @@
 import { asyncComputed } from '@vueuse/core'
-import { toValue, type MaybeRefOrGetter } from 'vue'
+import { computed, toValue, type MaybeRefOrGetter } from 'vue'
 
 export function useWorkflowRuntime(
   workflowId: MaybeRefOrGetter<string | null>,
 ) {
+  const runtimeInSeconds = computed<number | null>(() => 5)
+
   const numAvailableServers = asyncComputed<number | null>(async () => {
     const _workflowId = toValue(workflowId)
     if (_workflowId === null) return null
     return fetchNumAvailableServers(_workflowId)
   })
 
-  async function fetchRuntimePercentileInSeconds(
+  async function fetchNumAvailableServers(
     _workflowId: string,
-    percentile: number,
   ): Promise<number> {
-    // TODO: fetch percentiles of runtime in seconds for the specified workflow.
-    const factor = Math.random()
-    return factor * percentile * 5 * 60
-  }
-
-  async function fetchNumAvailableServers(workflowId: string): Promise<number> {
     // TODO: fetch the number of servers available ofr the specified workflow.
     return Math.round(5 * Math.random()) + 1
   }
 
-  async function getRuntimeRangeInSeconds(
-    lowerPercentile: number,
-    upperPercentile: number,
-  ): Promise<[number, number] | null> {
-    const _workflowId = toValue(workflowId)
-    if (_workflowId === null) return null
-
-    const lower = await fetchRuntimePercentileInSeconds(
-      _workflowId,
-      lowerPercentile,
-    )
-    const upper = await fetchRuntimePercentileInSeconds(
-      _workflowId,
-      upperPercentile,
-    )
-    return [lower, upper]
-  }
-
-  return { getRuntimeRangeInSeconds, numAvailableServers }
+  return { runtimeInSeconds, numAvailableServers }
 }
