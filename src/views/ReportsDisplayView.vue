cdc1acd130c82de9d7661acb096db161f134e8f3
hvangeffen <hunter.vangeffen@vortech.nl>
2024-11-12 09:46:00 +0100

    Make toolbar the same size as top bar and remove report id from dropdown

--- src/views/ReportsDisplayView.vue
+++ src/views/ReportsDisplayView.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="reports?.length" class="d-flex flex-column h-100 w-100">
-    <v-toolbar class="py-1" density="compact">
+    <v-toolbar density="compact">
       <template v-if="reports.length === 1">
         <div class="ml-5">{{ reportToTitle(reports[0]) }}</div>
         <v-spacer />
@@ -106,7 +106,7 @@ function reportItemToId(item: ReportItem) {
 }
 
 function reportItemToTitle(item: ReportItem) {
-  return `${item.timeZero} - ${item.reportId}`
+  return `${item.timeZero}`
 }
 
 function reportToTitle(item: Report) {
