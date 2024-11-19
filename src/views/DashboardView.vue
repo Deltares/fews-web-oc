150410f8bd251cd7090c4801c2483368720eeaff
hvangeffen <hunter.vangeffen@vortech.nl>
2024-11-19 10:25:58 +0100

    Add support for dashboard from topology

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,53 +1,25 @@
 <template>
-  <div class="dashboard-wrapper">
-    <v-toolbar>
-      <v-select
-        v-model="selectedDashboard"
-        :items="dashboards"
-        return-object
-        hide-details
-        item-text="title"
-        item-value="id"
-        label="Dashboard"
-        class="px-2"
-        menu-icon="mdi-chevron-down"
-        variant="solo-filled"
-        dense
-      />
-      <v-spacer />
-    </v-toolbar>
-    <DashboardDisplay v-if="selectedDashboard" :dashboard="selectedDashboard" />
-  </div>
+  <DashboardDisplay v-if="dashboard" :dashboard="dashboard" />
+  <v-alert v-else>No dashboard available</v-alert>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed } from 'vue'
 import DashboardDisplay from '@/components/general/DashboardDisplay.vue'
+import type { TopologyNode } from '@deltares/fews-pi-requests'
 
-async function getDashboards() {
-  const data = await fetch('/dashboards.json')
-  return data.json()
+interface Props {
+  topologyNode?: TopologyNode
 }
 
-const selectedDashboard = ref()
+const props = defineProps<Props>()
 
-const dashboards = ref()
-onMounted(async () => {
-  dashboards.value = await getDashboards()
-  selectedDashboard.value = dashboards.value[0]
+const dashboard = computed(() => {
+  if (!props.topologyNode) return
+  return {
+    id: props.topologyNode.id,
+    css: `${props.topologyNode.id}.css`,
+    panels: props.topologyNode.topologyNodes ?? [],
+  }
 })
 </script>
-
-<style scoped>
-.dashboard-wrapper {
-  display: grid;
-  grid-template-rows: auto 1fr;
-  height: 100%;
-  width: 100%;
-}
-
-.dashboard-body {
-  height: 100%;
-  width: 100%;
-}
-</style>
