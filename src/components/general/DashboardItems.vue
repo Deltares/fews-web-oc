b7004319f0dfddafb9455de780f1c8f778102538
hvangeffen <hunter.vangeffen@vortech.nl>
2024-12-11 15:56:21 +0100

    Add icons to dashboard tabs and make more compact

--- src/components/general/DashboardItems.vue
+++ src/components/general/DashboardItems.vue
@@ -2,12 +2,13 @@
   <v-tabs
     v-model="tab"
     v-if="items.length > 1"
-    color="primary"
+    bg-color="primary"
     align-tabs="center"
-    class="flex-0-0"
+    class="flex-0-0 w-100"
+    density="compact"
   >
-    <v-tab v-for="item in items">
-      {{ item.component }}
+    <v-tab v-for="item in componentItems" :prepend-icon="item.icon">
+      {{ item.componentName }}
     </v-tab>
   </v-tabs>
 
@@ -27,34 +28,34 @@ import type { DashboardItem } from '@/lib/dashboard/types'
 import {
   getComponentForName,
   getComponentPropsForNode,
+  getIconForComponentName,
 } from '@/lib/topology/dashboard'
-import type { TopologyNode } from '@deltares/fews-pi-requests'
+import { useTopologyNodesStore } from '@/stores/topologyNodes'
 import { computed, ref, watch } from 'vue'
 
 interface Props {
   items: DashboardItem[]
-  topologyMap: Map<string, TopologyNode>
 }
 
 const props = defineProps<Props>()
 
 const tab = ref(0)
 
-function getTopologyNodeById(id: string) {
-  return props.topologyMap.get(id)
-}
+const topologyNodesStore = useTopologyNodesStore()
 
 const componentItems = computed(() => {
   return props.items.map((item) => {
     const componentName = item.component
-    const topologyNode = getTopologyNodeById(item.topologyNodeId)
+    const topologyNode = topologyNodesStore.getNodeById(item.topologyNodeId)
     const component = getComponentForName(componentName)
     const componentProps = getComponentPropsForNode(componentName, topologyNode)
+    const icon = getIconForComponentName(componentName)
     return {
       component,
       componentProps,
       componentName,
       topologyNode,
+      icon,
     }
   })
 })
