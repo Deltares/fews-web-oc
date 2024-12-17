ddb83e4cacfb0afbceed2df079117c0f0fa83145
hvangeffen <hunter.vangeffen@vortech.nl>
2024-12-17 12:17:15 +0100

    Add basemap control

--- src/components/map/MapComponent.vue
+++ src/components/map/MapComponent.vue
@@ -25,42 +25,37 @@ import { configManager } from '@/services/application-config'
 import { authenticationManager } from '@/services/authentication/AuthenticationManager'
 import {
   MglAttributionControl,
-  MglDefaults,
   MglMap,
   MglScaleControl,
-  useMap,
 } from '@indoorequal/vue-maplibre-gl'
-import type { ResourceType, RequestParameters, LngLatBounds } from 'maplibre-gl'
-import { useBaseLayers } from '@/services/useBaseLayers'
-import { useUserSettingsStore } from '@/stores/userSettings'
-import { useTemplateRef, watch } from 'vue'
+import type {
+  ResourceType,
+  RequestParameters,
+  LngLatBounds,
+  Map,
+} from 'maplibre-gl'
+import { computed, useTemplateRef, watch } from 'vue'
 import { transformStyle } from '@/lib/map'
+import { useComponentSettingsStore } from '@/stores/componentSettings'
 
 interface Props {
   bounds?: LngLatBounds
+  baseMapId?: string
 }
 
 defineProps<Props>()
 
-const settings = useUserSettingsStore()
-
-MglDefaults.style =
-  'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json'
-
-const { baseLayerStyle } = useBaseLayers(
-  () => settings.get('ui.map.theme')?.value as string | undefined,
-)
-
-const initialStyle = baseLayerStyle.value
-
 const mapRef = useTemplateRef('map')
 
-watch(baseLayerStyle, (newBaseStyle) => {
+const componentSettingsStore = useComponentSettingsStore()
+const selectedStyle = computed(() => componentSettingsStore.selectedStyle)
+const initialStyle = selectedStyle.value
+
+watch(selectedStyle, (newBaseStyle) => {
   if (!newBaseStyle) return
 
-  // NOTE: We have to get mapkey because useMap uses inject and we are not a child of MglMap
-  const mapKey = mapRef.value?.mapKey
-  const map = useMap(mapKey).map
+  // @ts-expect-error map is not exposed in the types
+  const map: Map | undefined = mapRef.value?.map
   map?.setStyle(newBaseStyle, { transformStyle })
 })
 
