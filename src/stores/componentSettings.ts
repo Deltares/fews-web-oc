c6c39e0b85f3a484e310ce5a90d8c291b855deb6
hvangeffen <hunter.vangeffen@vortech.nl>
2024-12-17 12:17:15 +0100

    Add basemap control

--- src/stores/componentSettings.ts
+++ src/stores/componentSettings.ts
@@ -1,23 +1,31 @@
 import { getResourcesStaticUrl } from '@/lib/fews-config'
 import { defineStore } from 'pinia'
+import DefaultBaseMaps from '@/assets/DefaultBaseMaps.json'
 import type {
+  BaseMap,
   ComponentSettings,
   ComponentSettingsResponse,
   Declarations,
   OverlayLocation,
 } from '@/lib/topology/componentSettings'
+import { useDark } from '@vueuse/core'
 
 interface State {
   settings: ComponentSettings[]
   declarations?: Declarations
   selectedOverlayIds: string[]
-  selectedBaseMapId?: string
+  selectedBaseMapId: string
+}
+
+function isKeyOfDefaultStyle(key: string): key is keyof typeof DefaultBaseMaps {
+  return key in DefaultBaseMaps
 }
 
 export const useComponentSettingsStore = defineStore('componentSettings', {
   state: (): State => ({
     settings: [],
     selectedOverlayIds: [],
+    selectedBaseMapId: 'automatic',
   }),
   getters: {
     overlays: (state) => state.declarations?.overlays?.locations ?? [],
@@ -26,9 +34,33 @@ export const useComponentSettingsStore = defineStore('componentSettings', {
         .map((id) => this.overlays.find((o) => o.id === id))
         .filter((o) => o !== undefined)
     },
-    basemaps: (state) => {
-      return state.declarations?.baseMaps
+    baseMaps: (state) => {
+      const defaultBaseMaps: BaseMap[] = Object.values(DefaultBaseMaps)
+      const baseMaps = state.declarations?.baseMaps ?? []
+      return [
+        ...defaultBaseMaps,
+        ...baseMaps,
+      ]
     },
+    selectedBaseMap(): BaseMap {
+      if (this.selectedBaseMapId === 'automatic') {
+        const isDark = useDark()
+        return DefaultBaseMaps[isDark.value ? 'dark' : 'light']
+      }
+      if (isKeyOfDefaultStyle(this.selectedBaseMapId)) {
+        return DefaultBaseMaps[this.selectedBaseMapId]
+      }
+      // FIXME: What to do if the selected base map is not found?
+      //        Could happen when persisting the selected base map
+      return this.baseMaps.find((b) => b.id === this.selectedBaseMapId) ?? this.baseMaps[0]
+    },
+    selectedStyle(): string {
+      const style = this.selectedBaseMap.style
+      if (style.startsWith('/')) {
+        return getResourcesStaticUrl(style.slice(1))
+      }
+      return style
+    }
   },
   actions: {
     async fetchState() {
