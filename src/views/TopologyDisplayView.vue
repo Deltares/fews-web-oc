e93ce4436521d35c85829f935202588abe503728
Cees Voesenek <cees.voesenek@vortech.nl>
2024-12-18 13:12:17 +0100

    Add task runs panel
    This adds a button to the application bar that opens a task runs menu,
    showing currently running tasks and their properties.

--- src/views/TopologyDisplayView.vue
+++ src/views/TopologyDisplayView.vue
@@ -97,7 +97,7 @@ import { useWorkflowsStore } from '@/stores/workflows'
 import type { TopologyNode } from '@deltares/fews-pi-requests'
 import type { WebOcTopologyDisplayConfig } from '@deltares/fews-pi-requests'
 
-import { computed, ref, StyleValue, watch, watchEffect } from 'vue'
+import { computed, onUnmounted, ref, StyleValue, watch, watchEffect } from 'vue'
 import {
   onBeforeRouteUpdate,
   RouteLocationNormalized,
@@ -115,6 +115,7 @@ import {
   type DisplayTab,
 } from '@/lib/topology/displayTabs.js'
 import { useTopologyNodesStore } from '@/stores/topologyNodes'
+import { useAvailableWorkflowsStore } from '@/stores/availableWorkflows'
 
 interface Props {
   topologyId?: string
@@ -131,6 +132,7 @@ const props = defineProps<Props>()
 const configStore = useConfigStore()
 const settings = useUserSettingsStore()
 const workflowsStore = useWorkflowsStore()
+const availableWorkflowsStore = useAvailableWorkflowsStore()
 
 const menuType = computed(() => {
   const configured = settings.get('ui.hierarchical-menu-style')?.value as string
@@ -158,6 +160,26 @@ const activeNode = computed(() => {
   }
   return node
 })
+
+// Set preferred workflow IDs for the running tasks menu, if this node has
+// associated workflows.
+watch(
+  activeNode,
+  (node) => {
+    const primaryWorkflowId = node?.workflowId ? [node.workflowId] : []
+    const secondaryWorkflowIds =
+      node?.secondaryWorkflows?.map(
+        (workflow) => workflow.secondaryWorkflowId,
+      ) ?? []
+    // Note: this list of workflow IDs may be empty, in which case we have no
+    //       preferred workflow.
+    const workflowIds = [...primaryWorkflowId, ...secondaryWorkflowIds]
+    availableWorkflowsStore.setPreferredWorkflowIds(workflowIds)
+  },
+  { immediate: true },
+)
+// Clear the preferred workflow IDs when we unmount.
+onUnmounted(() => availableWorkflowsStore.clearPreferredWorkflowIds())
 
 const secondaryWorkflows = computed(() => {
   if (!activeNode.value?.secondaryWorkflows) return null
