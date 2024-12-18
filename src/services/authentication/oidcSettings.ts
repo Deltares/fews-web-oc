43156567e32c287d41762e1817eb93e6642dc67d
hvangeffen <hunter.vangeffen@vortech.nl>
2024-12-18 14:25:42 +0100

    Remove creation of oidc settings on file import as it clashes with test environment

--- src/services/authentication/oidcSettings.ts
+++ src/services/authentication/oidcSettings.ts
@@ -1,17 +1,18 @@
 import { UserManagerSettings } from 'oidc-client-ts'
 
-const baseUrl = window.location.origin + import.meta.env.BASE_URL
-const oidcSettings: UserManagerSettings = {
-  authority: `${import.meta.env.VITE_AUTH_AUTHORITY}`,
-  metadataUrl: `${import.meta.env.VITE_AUTH_METADATA_URL}`,
-  client_id: `${import.meta.env.VITE_AUTH_ID}`,
-  redirect_uri: `${baseUrl}auth/callback`,
-  silent_redirect_uri: `${baseUrl}auth/silent`,
-  response_type: `code`,
-  scope: `${import.meta.env.VITE_AUTH_SCOPE}`,
-  post_logout_redirect_uri: `${baseUrl}auth/logout`,
-  monitorSession: false,
-  automaticSilentRenew: true,
+export function getOidcSettings() {
+  const baseUrl = window.location.origin + import.meta.env.BASE_URL
+  const oidcSettings: UserManagerSettings = {
+    authority: `${import.meta.env.VITE_AUTH_AUTHORITY}`,
+    metadataUrl: `${import.meta.env.VITE_AUTH_METADATA_URL}`,
+    client_id: `${import.meta.env.VITE_AUTH_ID}`,
+    redirect_uri: `${baseUrl}auth/callback`,
+    silent_redirect_uri: `${baseUrl}auth/silent`,
+    response_type: `code`,
+    scope: `${import.meta.env.VITE_AUTH_SCOPE}`,
+    post_logout_redirect_uri: `${baseUrl}auth/logout`,
+    monitorSession: false,
+    automaticSilentRenew: true,
+  }
+  return oidcSettings
 }
-
-export default oidcSettings
