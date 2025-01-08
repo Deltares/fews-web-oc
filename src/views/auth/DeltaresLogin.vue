a57274bf9fc633a4134df070d7275d201b5d42f7
HYDROTEC\tobiasg <tobias.gehrmann@hydrotec.de>
2025-01-08 14:40:49 +0100

    Text and Icon configuration for login page via app-config.json

--- src/views/auth/DeltaresLogin.vue
+++ src/views/auth/DeltaresLogin.vue
@@ -1,6 +1,6 @@
 <template>
   <v-btn @click="login" color="primary" variant="elevated">
-    <v-icon>mdi-microsoft-windows</v-icon>Sign in
+    <v-icon>{{ loginIcon }}</v-icon>{{ loginText }}
   </v-btn>
 </template>
 
@@ -8,10 +8,15 @@
 import { useRoute } from 'vue-router'
 import { authenticationManager } from '../../services/authentication/AuthenticationManager.js'
 
+import { configManager } from '@/services/application-config';
+
 const route = useRoute()
 
 function login(): void {
   const redirect = route.query.redirect ?? '/'
   authenticationManager.userManager.signinRedirect({ state: redirect })
 }
+
+const loginText = configManager.get('LOGIN_TEXT') ?? "Sign in";
+const loginIcon = configManager.get('LOGIN_ICON') ?? "mdi-microsoft-windows";
 </script>
