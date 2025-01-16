924f41966b06abf7c2577a3d6ab3db88191ca9c2
hvangeffen <hunter.vangeffen@vortech.nl>
2025-01-16 13:32:32 +0100

    Fix mouseUp not being called sometimes and make dragging smooth 1 to 1

--- src/services/useHorizontalScroll/index.ts
+++ src/services/useHorizontalScroll/index.ts
@@ -1,59 +1,49 @@
 export function useHorizontalScroll() {
-  let pos = { top: 0, left: 0, x: 0, y: 0 }
+  let lastPos = { x: 0, y: 0 }
+  let moveHandler: (event: MouseEvent) => void
+  let upHandler: (event: MouseEvent) => void
 
   function mouseWheelHandler(event: WheelEvent): void {
     if (!event.currentTarget) return
     const element = event.currentTarget as HTMLElement
 
     element.scrollLeft += event.deltaY
-    pos.left = element.scrollLeft
-  }
-
-  function mouseMoveHandler(event: MouseEvent): void {
-    if (!event.currentTarget) return
-    const element = event.currentTarget as HTMLElement
-
-    const dx = event.clientX - pos.x
-    const dy = event.clientY - pos.y
-
-    pos.left = element.scrollLeft
-
-    element.scrollTop = pos.top - dy
-    element.scrollLeft = pos.left - dx
-
-    element.style.cursor = 'grabbing'
   }
 
   function mouseDownHandler(event: MouseEvent): void {
     if (!event.currentTarget) return
     const element = event.currentTarget as HTMLElement
-    const { scrollLeft, scrollTop } = element
 
-    pos = {
-      // The current scroll
-      left: scrollLeft,
-      top: scrollTop,
-      // Get the current mouse position
+    lastPos = {
       x: event.clientX,
       y: event.clientY,
     }
 
-    element.addEventListener('pointermove', mouseMoveHandler)
-    element.addEventListener('pointerup', mouseUpHandler)
+    moveHandler = (event) => mouseMoveHandler(event, element)
+    upHandler = () => mouseUpHandler(element)
+
+    document.addEventListener('pointermove', moveHandler)
+    document.addEventListener('pointerup', upHandler)
   }
 
-  function mouseUpHandler(event: MouseEvent): void {
-    if (!event.currentTarget) return
-    const element = event.currentTarget as HTMLElement
-    const { scrollLeft, scrollTop } = element
+  function mouseMoveHandler(event: MouseEvent, element: HTMLElement): void {
+    const dx = event.clientX - lastPos.x
 
-    pos.left = scrollLeft
-    pos.top = scrollTop
+    lastPos = {
+      x: event.clientX,
+      y: event.clientY,
+    }
 
+    element.scrollLeft -= dx
+
+    element.style.cursor = 'grabbing'
+  }
+
+  function mouseUpHandler(element: HTMLElement): void {
     element.style.cursor = 'inherit'
 
-    element.removeEventListener('pointermove', mouseMoveHandler)
-    element.removeEventListener('pointerup', mouseUpHandler)
+    document.removeEventListener('pointermove', moveHandler)
+    document.removeEventListener('pointerup', upHandler)
   }
 
   return {
