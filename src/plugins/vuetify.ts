5d2f68fd058113a0b27dca246a58acf9e65020ea
hvangeffen <hunter.vangeffen@vortech.nl>
2025-01-20 12:01:04 +0100

    Remove variant="plain" from buttons and set default to text

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,6 +3,12 @@ import '@/styles/main.scss'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 
-const vuetify = createVuetify({})
+const vuetify = createVuetify({
+  defaults: {
+    VBtn: {
+      variant: 'text',
+    },
+  },
+})
 
 export default vuetify
