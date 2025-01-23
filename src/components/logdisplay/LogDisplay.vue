2f0cc5c70f710a5f28420c1aff011924d7fc4618
hvangeffen <hunter.vangeffen@vortech.nl>
2025-01-23 16:02:00 +0100

    Get logdisplays from back end and move types to util

--- src/components/logdisplay/LogDisplay.vue
+++ src/components/logdisplay/LogDisplay.vue
@@ -1,7 +1,27 @@
 <template>
-  <OperatorLog timeOut="1000" />
+  <OperatorLog v-if="logDisplay" :logDisplay />
+  <div v-else>
+    <v-alert type="warning" class="ma-3">No logs display available</v-alert>
+  </div>
 </template>
 
 <script setup lang="ts">
 import OperatorLog from '@/components/logdisplay/OperatorLog.vue'
+import { useLogDisplaysStore } from '@/stores/logDisplays'
+import type { TopologyNode } from '@deltares/fews-pi-requests'
+import { computed } from 'vue'
+
+interface Props {
+  topologyNode?: TopologyNode
+}
+
+const props = defineProps<Props>()
+
+const logDisplaysStore = useLogDisplaysStore()
+logDisplaysStore.fetch()
+
+const logDisplay = computed(() => {
+  const id = props.topologyNode?.logDisplay?.id
+  return logDisplaysStore.getById(id)
+})
 </script>
