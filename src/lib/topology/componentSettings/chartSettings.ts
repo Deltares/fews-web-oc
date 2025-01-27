d41a2319d9c11fb19efd52cf1a86d2c25e959d39
hvangeffen <hunter.vangeffen@vortech.nl>
2025-01-27 16:53:17 +0100

    Move component settings to seperate files and use new chartSettings

---
+++ src/lib/topology/componentSettings/chartSettings.ts
@@ -0,0 +1,150 @@
+export interface ChartSettings {
+  general: General
+  actions: Actions
+  timeseriesChart: TimeseriesChart
+  timeseriesTable: TimeseriesTable
+  verticalProfileChart: VerticalProfileChart
+  verticalProfileTable: VerticalProfileTable
+  metaDataPanel: MetaDataPanel
+}
+
+export const defaultChartSettings: ChartSettings = {
+  general: {
+    startPanel: 'timeseriesChart',
+    hideToolbar: false,
+    showLocationNames: true,
+  },
+  actions: {
+    panelPlacement: {
+      defaultPlacement: 'right',
+      allowedPlacement: 'all',
+    },
+    downloadData: true,
+    downloadMetaData: true,
+    downloadFigure: true,
+  },
+  timeseriesChart: {
+    show: true,
+    legend: {
+      show: true,
+      numberOfLines: 'all',
+      placement: 'above chart',
+    },
+    xAxis: {
+      show: true,
+      xTicks: true,
+      xLabel: true,
+    },
+    yAxis: {
+      show: true,
+      yTicks: true,
+      yLabel: true,
+    },
+  },
+  timeseriesTable: {
+    show: true,
+    allowDateTimeSorting: true,
+    sortDateTimeColumn: 'descending',
+  },
+  verticalProfileChart: {
+    show: true,
+    legend: {
+      show: true,
+      numberOfLines: 1,
+      placement: 'above chart',
+    },
+    xAxis: {
+      show: true,
+      xTicks: true,
+      xLabel: true,
+    },
+    yAxis: {
+      show: true,
+      yTicks: true,
+      yLabel: true,
+    },
+  },
+  verticalProfileTable: {
+    show: true,
+    allowDepthColumnSorting: true,
+    sortDepthColumn: 'descending',
+  },
+  metaDataPanel: {
+    show: true,
+  },
+}
+
+interface General {
+  startPanel:
+    | 'metaDataPanel'
+    | 'timeseriesChart'
+    | 'timeseriesTable'
+    | 'verticalProfileChart'
+    | 'verticalProfileTable'
+  hideToolbar: boolean | 'auto'
+  showLocationNames: boolean
+}
+
+interface PanelPlacement {
+  defaultPlacement: 'right'
+  allowedPlacement: 'right' | 'all' | 'left' | 'bottom' | 'detached'
+}
+
+interface Actions {
+  panelPlacement: PanelPlacement
+  downloadData: boolean
+  downloadMetaData: boolean
+  downloadFigure: boolean
+}
+
+interface Legend {
+  show: boolean
+  numberOfLines: number | 'all'
+  placement:
+    | 'auto'
+    | 'above chart'
+    | 'under chart'
+    | 'inside upper right'
+    | 'inside lower right'
+    | 'inside upper left'
+    | 'inside lower left'
+}
+
+interface Axis {
+  show: boolean
+  xTicks?: boolean
+  xLabel?: boolean
+  yTicks?: boolean
+  yLabel?: boolean
+  yLabelPlacement?: 'auto' | 'top' | 'beside'
+}
+
+interface TimeseriesChart {
+  show: boolean
+  legend: Legend
+  xAxis: Axis
+  yAxis: Axis
+}
+
+interface TimeseriesTable {
+  show: boolean
+  allowDateTimeSorting: boolean
+  sortDateTimeColumn: 'descending' | 'ascending'
+}
+
+interface VerticalProfileChart {
+  show: boolean
+  legend: Legend
+  xAxis: Axis
+  yAxis: Axis
+}
+
+interface VerticalProfileTable {
+  show: boolean
+  allowDepthColumnSorting: boolean
+  sortDepthColumn: 'descending' | 'ascending'
+}
+
+interface MetaDataPanel {
+  show: boolean
+}
