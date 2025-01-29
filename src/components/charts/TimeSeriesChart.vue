71c4bb3dbf9fc5798dbc8eb93137812382f7c53d
hvangeffen <hunter.vangeffen@vortech.nl>
2025-01-29 14:14:14 +0100

    Set chart height to 100% if less then 800px

--- src/components/charts/TimeSeriesChart.vue
+++ src/components/charts/TimeSeriesChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="chart-with-chips mt-1"
+    class="chart-with-chips"
     :class="{ 'vertical-profile': verticalProfile }"
   >
     <ChartLegend
@@ -414,6 +414,8 @@ onBeforeUnmount(() => {
   position: relative;
   flex-direction: column;
   flex: 1 1 80%;
+  min-height: 150px;
+  height: 100%;
   max-height: 800px;
 }
 
