aa8a407c6b566416dec9ee2026e1f863ff25fdc7
hvangeffen <hunter.vangeffen@vortech.nl>
2025-01-30 16:32:24 +0100

    Only get splashscreen from static resources

--- src/components/dialog/StartupDialog.vue
+++ src/components/dialog/StartupDialog.vue
@@ -16,19 +16,16 @@ import SplashScreenDialog from '@/components/dialog/SplashScreenDialog.vue'
 import TermsOfUseDialog from '@/components/dialog/TermsOfUseDialog.vue'
 import packageConfig from '@/../package.json'
 import { computed } from 'vue'
-import { getLocalOrRemoteFileUrl } from '@/lib/fews-config'
+import { getResourcesStaticUrl } from '@/lib/fews-config'
 import { useConfigStore } from '@/stores/config'
 import { asyncComputed, useStorage } from '@vueuse/core'
 
 const configStore = useConfigStore()
 
-const imagesBaseUrl = `${import.meta.env.BASE_URL}images/`
-
 const splashSrc = asyncComputed(async () => {
   if (!configStore.general.splashScreen) return
-  const localPath = `${imagesBaseUrl}${configStore.general.splashScreen}`
-  const remoteResource = configStore.general.splashScreen
-  return await getLocalOrRemoteFileUrl(localPath, remoteResource)
+
+  return getResourcesStaticUrl(configStore.general.splashScreen)
 })
 
 const shouldAgreeToTerms = computed(
