2aa205173125832017e515e995abd601220e4fc3
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-07 16:22:48 +0800

    Get what if scenarios and what if templates from backend

--- src/components/tasksdisplay/TasksDisplay.vue
+++ src/components/tasksdisplay/TasksDisplay.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="w-75 mx-auto d-flex flex-column gr-4 pa-4">
     <v-select
-      v-model="selectedWorkflow"
-      :items="whatIfWorkflows"
+      v-model="selectedWhatIfTemplate"
+      :items="whatIfTemplates"
+      item-title="name"
+      label="Select what-if scenario template"
+      hide-details
+      return-object
+      class="flex-0-0"
+    />
+    <v-select
+      v-model="selectedWhatIfScenario"
+      :items="whatIfScenarios"
       item-title="name"
       label="Select what-if scenario template"
       hide-details
@@ -14,8 +23,8 @@
       <v-card-text>
         <json-forms
           class="pt-2"
-          :schema="whatIfScenarios.formSchemas.value?.properties"
-          :uischema="whatIfScenarios.formSchemas.value?.ui"
+          :schema="jsonSchema"
+          :uischema="jsonUiSchema"
           :data="selectedProperties"
           :renderers="Object.freeze(vuetifyRenderers)"
           :ajv="undefined"
@@ -54,95 +63,121 @@
 import jsonFormsConfig from '@/assets/JsonFormsConfig.json'
 
 import { vuetifyRenderers } from '@jsonforms/vue-vuetify'
-import { computed, ref } from 'vue'
+import { computed, ref, watchEffect } from 'vue'
 
-import {
-  SubmitStatus,
-  useWhatIfScenarios,
-  WhatIfProperties,
-} from '@/services/useWhatIfScenarios'
+import { useWhatIfScenarios } from '@/services/useWhatIfScenarios'
 
 import { JsonForms } from '@jsonforms/vue'
 import ExpectedWorkflowRuntime from './ExpectedWorkflowRuntime.vue'
 import AvailableWorkflowServers from './AvailableWorkflowServers.vue'
 import { useAvailableWorkflowsStore } from '@/stores/availableWorkflows'
-import { TopologyNode } from '@deltares/fews-pi-requests'
+import {
+  TopologyNode,
+  WhatIfScenarioDescriptor,
+  WhatIfTemplate,
+} from '@deltares/fews-pi-requests'
 import { WorkflowItem } from '@/lib/workflows'
 import { getWorkflowIdsForNode } from '@/lib/workflows/tasks'
+import { configManager } from '@/services/application-config'
+import { useWhatIfTemplates } from '@/services/useWhatIfTemplate'
+import {
+  generateDefaultUISchema,
+  generateJsonSchema,
+  getJsonDataFromProperties,
+  ScenarioData,
+} from '@/lib/whatif'
 
 const availableWorkflowsStore = useAvailableWorkflowsStore()
 
 interface Props {
-  nodeId?: string | string[]
   topologyNode?: TopologyNode
 }
 const props = defineProps<Props>()
 
-const nodeId = computed(() => {
-  if (Array.isArray(props.nodeId)) {
-    return props.nodeId[props.nodeId.length - 1]
-  }
-  return props.nodeId ?? ''
-})
+const selectedWhatIfTemplate = ref<WhatIfTemplate>()
+const selectedWhatIfScenario = ref<WhatIfScenarioDescriptor>()
+const selectedProperties = ref<ScenarioData>({})
 
 const workflowIds = computed<string[]>(() =>
   props.topologyNode ? getWorkflowIdsForNode(props.topologyNode) : [],
 )
-const workflows = computed<WorkflowItem[]>(() =>
-  workflowIds.value.map((id) => availableWorkflowsStore.byId(id)),
+
+const whatIfWorkflows = computed<WorkflowItem[]>(() =>
+  workflowIds.value
+    .map((id) => availableWorkflowsStore.byId(id))
+    .filter((wf) => wf.whatIfTemplateId !== undefined),
 )
 
-const selectedWorkflow = ref<WorkflowItem>()
-const whatIfWorkflows = computed(() =>
-  workflows.value.filter((wf) => wf.whatIfTemplateId !== undefined),
-)
-const whatIfScenarios = useWhatIfScenarios(
-  nodeId,
-  availableWorkflowsStore.whatIfTemplateIds,
-  () => selectedWorkflow.value?.whatIfTemplateId ?? null,
+const selectedWorkflow = computed(() =>
+  whatIfWorkflows.value.find(
+    (wf) => wf.whatIfTemplateId === selectedWhatIfTemplate.value?.id,
+  ),
 )
 
-const whatIfScenarioName = ref<string>('')
+const whatIfTemplateIds = computed(() =>
+  whatIfWorkflows.value
+    .map((wf) => wf.whatIfTemplateId)
+    .filter((id) => id !== undefined),
+)
+
+const baseUrl = configManager.get('VITE_FEWS_WEBSERVICES_URL')
+const { whatIfTemplates } = useWhatIfTemplates(baseUrl, whatIfTemplateIds)
+const { whatIfScenarios } = useWhatIfScenarios(
+  baseUrl,
+  () => selectedWhatIfTemplate.value?.id,
+)
+
+const jsonSchema = computed(() =>
+  generateJsonSchema(selectedWhatIfTemplate.value?.properties),
+)
+const jsonUiSchema = computed(() =>
+  generateDefaultUISchema(selectedWhatIfTemplate.value?.properties),
+)
+
+watchEffect(() => {
+  selectedProperties.value = getJsonDataFromProperties(
+    selectedWhatIfScenario.value?.properties,
+  )
+})
 
 const doShowConfiguration = computed<boolean>(
   () => selectedWorkflow.value !== undefined,
 )
 
 const hasAllRequiredProperties = computed<boolean>(() => {
-  const propertiesSchema = whatIfScenarios.formSchemas.value?.properties
-  if (!propertiesSchema) return false
-  const requiredProperties = propertiesSchema.required ?? []
+  if (!jsonSchema.value) return false
+
+  const requiredProperties = jsonSchema.value.required ?? []
   return requiredProperties.every(
     (property) => property in selectedProperties.value,
   )
 })
+
 const canSubmit = computed<boolean>(() => {
   const hasSelectedWorkflow = selectedWorkflow.value !== undefined
   const hasAllProperties = hasAllRequiredProperties.value
-  const hasScenarioName = whatIfScenarioName.value.trim() !== ''
-  return hasSelectedWorkflow && hasAllProperties && hasScenarioName
+  return hasSelectedWorkflow && hasAllProperties
 })
 
 const isSubmitted = ref<boolean>(false)
 const hasSubmitError = ref<boolean>(false)
 const submitErrorMessage = ref<string>('')
-// FIXME: why does json-forms not support v-model? :-(
-const selectedProperties = ref<WhatIfProperties>({})
-function onPropertiesChange(event: { data?: WhatIfProperties }): void {
+
+function onPropertiesChange(event: { data: ScenarioData }): void {
   const updatedProperties = event.data
   if (!updatedProperties) return
   selectedProperties.value = updatedProperties
 }
 
 async function submit(): Promise<void> {
-  const result = await whatIfScenarios.submit(selectedProperties.value)
-
-  isSubmitted.value = true
-  if (result.status === SubmitStatus.Success) {
-    hasSubmitError.value = false
-  } else {
-    hasSubmitError.value = true
-    submitErrorMessage.value = result.error
-  }
+  // const result = await submitWhatIf(selectedProperties.value)
+  //
+  // isSubmitted.value = true
+  // if (result.status === "success") {
+  //   hasSubmitError.value = false
+  // } else {
+  //   hasSubmitError.value = true
+  //   submitErrorMessage.value = result.error
+  // }
 }
 </script>
---
+++ src/lib/whatif/utils.ts
@@ -0,0 +1,146 @@
+import type {
+  ControlElement,
+  JsonSchema7,
+  UISchemaElement,
+  VerticalLayout,
+} from '@jsonforms/core'
+
+import type {
+  WhatIfScenarioDescriptor,
+  WhatIfTemplate,
+} from '@deltares/fews-pi-requests'
+
+export type TemplateProperty = NonNullable<WhatIfTemplate['properties']>[number]
+export type ScenarioProperty = NonNullable<
+  WhatIfScenarioDescriptor['properties']
+>[number]
+export type ScenarioData = Record<string, ScenarioProperty['value']>
+
+/**
+ * Generates a JSON schema based on an array of SecondaryWorkflowProperty objects.
+ * @param properties An array of SecondaryWorkflowProperty objects.
+ * @returns The generated JSON schema.
+ */
+export function generateJsonSchema(
+  properties: WhatIfTemplate['properties'],
+): JsonSchema7 {
+  const schemaProperties: NonNullable<JsonSchema7['properties']> = {}
+
+  properties?.forEach((property) => {
+    schemaProperties[property.id] =
+      convertPropertyToJsonSchemaProperty(property)
+  })
+
+  return {
+    type: 'object',
+    properties: schemaProperties,
+  }
+}
+
+/**
+ * Generates a default UI schema based on an array of SecondaryWorkflowProperty objects.
+ * @param properties An array of SecondaryWorkflowProperty objects.
+ * @returns The generated default UI schema.
+ */
+export function generateDefaultUISchema(
+  properties: WhatIfTemplate['properties'],
+): UISchemaElement {
+  const uiSchema: VerticalLayout = {
+    type: 'VerticalLayout',
+    elements: [],
+  }
+
+  properties?.forEach((property) => {
+    const element: ControlElement = {
+      type: 'Control',
+      scope: `#/properties/${property.id}`,
+      label: property.name,
+    }
+
+    switch (property.type) {
+      case 'dateTime':
+        element.options = {
+          format: 'date-time',
+        }
+        break
+      case 'number':
+      case 'integer':
+        element.options = {
+          format: 'number',
+        }
+        break
+      case 'boolean':
+        element.options = {
+          format: 'checkbox',
+        }
+        break
+      case 'configFile':
+      case 'whatIfTemplateTemplateId':
+      case 'enumProperty':
+      case 'string':
+        break
+    }
+
+    uiSchema.elements.push(element)
+  })
+
+  return uiSchema
+}
+
+export function getJsonDataFromProperties(
+  properties: WhatIfScenarioDescriptor['properties'],
+) {
+  const data: ScenarioData = {}
+
+  properties?.forEach((property) => {
+    data[property.id] = property.value
+  })
+
+  return data
+}
+
+function convertPropertyToJsonSchemaProperty(
+  property: TemplateProperty,
+): JsonSchema7 {
+  switch (property.type) {
+    case 'string':
+      return {
+        type: 'string',
+        default: property.defaultValue,
+      }
+    case 'enumProperty':
+      return {
+        type: 'string',
+        enum: property.values.map((value) => value.code),
+        default: property.defaultValue,
+      }
+    case 'number':
+      return {
+        type: 'number',
+        default: property.defaultValue,
+        minimum: property.minValue,
+        maximum: property.maxValue,
+      }
+    case 'integer':
+      return {
+        type: 'integer',
+        default: property.defaultValue,
+      }
+    case 'boolean':
+      return {
+        type: 'boolean',
+        default: property.defaultValue,
+      }
+    case 'dateTime':
+      return {
+        type: 'string',
+        format: 'date-time',
+        default: property.defaultValue,
+      }
+    case 'whatIfTemplateTemplateId':
+    case 'configFile':
+      return {
+        type: 'string',
+      }
+  }
+}
