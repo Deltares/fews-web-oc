8a2aa061e52ac072b223412a6ea3be8d70bab292
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-12 08:11:53 +0100

    Get basemaps from general config

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -8,11 +8,6 @@ import { computed, onMounted } from 'vue'
 import { useComponentSettingsStore } from '@/stores/componentSettings'
 import DashboardDisplay from '@/components/general/DashboardDisplay.vue'
 import type { TopologyNode } from '@deltares/fews-pi-requests'
-import {
-  UserSettingsWithIcon,
-  useUserSettingsStore,
-} from '@/stores/userSettings'
-import { BaseMap } from '@/lib/topology/componentSettings'
 import { useDashboard } from '@/services/useDashboard'
 import { configManager } from '@/services/application-config'
 
@@ -23,27 +18,10 @@ interface Props {
 const props = defineProps<Props>()
 
 const componentSettingsStore = useComponentSettingsStore()
-const userSettings = useUserSettingsStore()
 
 onMounted(async () => {
   await componentSettingsStore.fetchState()
-  updateUserSettingBaseMaps()
 })
-
-function updateUserSettingBaseMaps() {
-  // FIXME: Remove once baseMaps have moved out of the component settings
-  const items = componentSettingsStore.baseMaps.map(convertBaseMapToUserSetting)
-  const current = userSettings.get('ui.map.theme')
-  if (current?.type !== 'oneOfMultiple') return
-  current.items = items
-}
-
-function convertBaseMapToUserSetting(baseMap: BaseMap): UserSettingsWithIcon {
-  return {
-    value: baseMap.id,
-    icon: baseMap.icon,
-  }
-}
 
 const dashboardId = computed(() => props.topologyNode?.dashboardPanels?.[0]?.id)
 
