95f93468370afd2c54ff4e7a2fffa585d84761c0
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-12 08:12:11 +0100

    Use component settings types from pi-requests

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -5,3 +5,7 @@ export type ComponentProps<T> = T extends new (...angs: any) => {
   : T extends (props: infer P, ...args: any) => any
     ? P
     : {}
+
+export type DeepRequired<T> = Required<{
+    [K in keyof T]: T[K] extends Required<T[K]> ? T[K] : DeepRequired<T[K]>
+}>
