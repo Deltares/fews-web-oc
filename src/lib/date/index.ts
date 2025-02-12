78ef0a216d4b144110c828016ed3f51aa30eee24
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-12 09:57:59 +0800

    Show dash for missing dates

--- src/lib/date/index.ts
+++ src/lib/date/index.ts
@@ -64,25 +64,26 @@ export function convertFewsPiDateTimeToJsDate(
  * @returns string suitable for use as a FEWS PI query parameter.
  */
 export function convertJSDateToFewsPiParameter(date: Date): string {
-  return toISOString(date) + 'Z'
+  return date.toISOString().replace(/\.\d{3}Z$/, 'Z')
 }
 
-export function toHumanReadableDate(date: Date | string | undefined): string {
-  if (!date) {
-    return ''
+export function toHumanReadableDate(
+  date: Date | string | undefined | null,
+): string {
+  if (date === undefined || date === null) {
+    return '—'
   }
   if (typeof date === 'string') {
     return toHumanReadableDate(new Date(date))
   }
   // dd/MM/yyyy, HH:mm:ss
-  return date
-    .toLocaleString('en-GB', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: false,
-    })
+  return date.toLocaleString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  })
 }
