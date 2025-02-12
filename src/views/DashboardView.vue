712cee8a27e47a153b32efe82feb4240bcca297a
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-12 22:08:41 +0800

    Wait for dashboard request and css loading

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
   <DashboardDisplay v-if="dashboard" :dashboard="dashboard" />
-  <v-alert v-else>No dashboard available</v-alert>
+  <v-alert v-else-if="dashboardId && isReady">No dashboard available</v-alert>
 </template>
 
 <script setup lang="ts">
@@ -21,5 +21,5 @@ const props = defineProps<Props>()
 const dashboardId = computed(() => props.topologyNode?.dashboardPanels?.[0]?.id)
 
 const baseUrl = configManager.get('VITE_FEWS_WEBSERVICES_URL')
-const { dashboard } = useDashboard(baseUrl, dashboardId)
+const { dashboard, isReady } = useDashboard(baseUrl, dashboardId)
 </script>
