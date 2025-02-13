c509ea6df28768928f80c767ddbe607f2d127abb
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-13 08:26:16 +0100

    Fix scrollbars and incosistencies with main

--- src/components/charts/TimeSeriesChart.vue
+++ src/components/charts/TimeSeriesChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="chart-with-chips mt-1"
+    class="chart-with-chips"
     :class="{ 'vertical-profile': verticalProfile }"
   >
     <ChartLegend
@@ -409,7 +409,7 @@ onBeforeUnmount(() => {
   display: flex;
   position: relative;
   flex-direction: column;
-  flex: 1 1 80%;
+  flex: 0 0 auto;
   height: clamp(150px, 100%, 400px);
 }
 
