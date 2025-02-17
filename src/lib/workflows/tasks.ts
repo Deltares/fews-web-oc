7c679b1f39c38a8d76c51c1c5304faa365154333
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-17 15:54:44 +0800

    Only use secondary workflows for whatif panels

--- src/lib/workflows/tasks.ts
+++ src/lib/workflows/tasks.ts
@@ -2,7 +2,9 @@ import { TopologyNode } from '@deltares/fews-pi-requests'
 
 export function getWorkflowIdsForNode(node: TopologyNode): string[] {
   return [
-    node.workflowId,
+    // NOTE: For now only consider secondary workflows as this would add
+    //       what if panels to older configurations.
+    // node.workflowId,
     node.secondaryWorkflows?.map((wf) => wf.secondaryWorkflowId),
   ]
     .flat()
