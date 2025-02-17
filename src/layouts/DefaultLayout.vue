f9ab4fd3017bc49ac0ee1b7c46bcf48a53529147
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-17 15:55:28 +0800

    Add border to main side panel

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -144,7 +144,7 @@
             <router-view></router-view>
           </Suspense>
         </div>
-        <div class="flex-0-0 h-100" id="main-side-panel"></div>
+        <div class="border-s flex-0-0 h-100" id="main-side-panel"></div>
       </div>
       <div class="alerts__container" v-if="alertsStore.hasAlerts">
         <v-alert
