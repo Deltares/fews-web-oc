a976f49ad022da819cd47a014961796d07aa39e8
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-20 11:22:50 +0800

    Remove unused extra component settings

--- src/lib/topology/componentSettings/schematicStatusDisplaySettings.ts
+++ src/lib/topology/componentSettings/schematicStatusDisplaySettings.ts
@@ -1,14 +1,11 @@
 import type { DeepRequired } from '@/lib/utils/types'
 import type { SSDSettings as PiSSDSettings } from '@deltares/fews-pi-requests'
 
-export type SSDSettings = DeepRequired<PiSSDSettings> & {
-  dateTimeSliderEnabled: boolean
-}
+export type SSDSettings = DeepRequired<PiSSDSettings>
 
 export const defaultSchematicStatusDisplaySettings: SSDSettings = {
   zoomEnabled: false,
   singleClickAction: true,
   doubleClickAction: true,
   useBrowserStyle: false,
-  dateTimeSliderEnabled: true,
 }
