14155412b22fef1786004beb8f9b7ae5a8e96d3e
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-24 16:24:05 +0100

    Remove running task panel to be added later

--- src/lib/period/types.ts
+++
@@ -1,9 +0,0 @@
-export interface AbsolutePeriod {
-  startTimestamp: number
-  endTimestamp: number
-}
-
-export interface RelativePeriod {
-  startOffsetSeconds: number
-  endOffsetSeconds: number
-}
--- src/lib/taskruns/index.ts
+++ src/lib/taskruns/index.ts
@@ -1,3 +1,2 @@
-export * from './convert'
 export * from './status'
 export * from './types'
