c80ceef91be9ddf1714daf07148431db7ff30008
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-24 16:24:56 +0100

    Rename remaing tasks to whatif

--- src/router/index.ts
+++ src/router/index.ts
@@ -237,7 +237,7 @@ export const dynamicRoutes: Readonly<RouteRecordRaw[]> = [
         ],
       },
       {
-        path: 'tasks',
+        path: 'whatif',
         name: 'TopologyWhatIfDisplay',
         component: WhatIfDisplayView,
         props: true,
