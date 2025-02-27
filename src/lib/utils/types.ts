ccda241b823aaac00af1f18cf1095459f45f3377
hvangeffen <hunter.vangeffen@vortech.nl>
2025-02-27 14:45:23 +0100

    Get logs from backend

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -9,3 +9,10 @@ export type ComponentProps<T> = T extends new (...angs: any) => {
 export type DeepRequired<T> = Required<{
   [K in keyof T]: T[K] extends Required<T[K]> ? T[K] : DeepRequired<T[K]>
 }>
+
+export const arrayOfAll =
+  <T>() =>
+  <U extends T[]>(
+    array: U & ([T] extends [U[number]] ? unknown : 'Invalid') & { 0: T },
+  ) =>
+    array
