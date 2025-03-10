580a9638331f4c59c0b2b2f70c5ac70c55d611bd
Elmar Geers <elmar.geers@hydrotec.de>
2025-03-10 14:26:15 +0100

    linter

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ fetch(`${import.meta.env.BASE_URL}app-config.json`)
       locale: configManager.getIfAvailable('LOCALE') ?? 'en_EN',
       fallbackLocale: 'en_EN',
       messages: await localeMessages(),
-      fallbackWarn: false
+      fallbackWarn: false,
     })
     app.use(i18n)
     app.use(router)
