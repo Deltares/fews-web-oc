135c9db3f422c7607dbb17dc4cad4b8a449a2cfa
Anne Markensteijn <anne.markensteijn@vortech.nl>
2025-03-18 15:25:42 +0100

    Display very basic information in threshold display

--- src/views/TopologyDisplayView.vue
+++ src/views/TopologyDisplayView.vue
@@ -62,7 +62,7 @@
     </v-menu>
   </Teleport>
   <div class="d-flex w-100 h-100">
-    <ThresholdInformationDisplay></ThresholdInformationDisplay>
+    <ThresholdInformationDisplay :topologyNode="topologyNode"></ThresholdInformationDisplay>
     <router-view v-slot="{ Component }">
       <keep-alive include="SpatialDisplay">
         <component
