e2dec872a186b522ae2174f783537079d1b060b7
Anne Markensteijn <anne.markensteijn@vortech.nl>
2025-03-19 14:17:51 +0100

    Add information per location
    Group on warning level

--- src/components/general/ThresholdInformationDisplay.vue
+++ src/components/general/ThresholdInformationDisplay.vue
@@ -3,17 +3,28 @@
     <v-data-iterator :items=warningLevels>
       <template v-slot:default="{ items }">
         <v-list density="compact">
-          <v-list-item
-            v-for="item in items"
-            :key="item.raw.id">
-            <template v-slot:prepend>
-              <v-avatar start :image="item.raw.icon" rounded size="x-small"></v-avatar>
-            </template>
-            {{ item.raw.name }}
-            <template v-slot:append>
-              <v-avatar end :text="`${item.raw.count}`"></v-avatar>
-            </template>
-          </v-list-item>
+          <v-list-group v-for="item in items"
+            :key="item.raw.id" :value="item.raw.id">
+            <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+            >
+              <template v-slot:prepend>
+                <v-avatar start :image="item.raw.icon" rounded size="x-small"></v-avatar>
+              </template>
+              {{ item.raw.name }}
+              <template v-slot:append>
+                <v-avatar end :text="`${item.raw.count}`"></v-avatar>
+              </template>
+            </v-list-item>
+          </template>
+
+            <v-list-item v-for="crossing in item.raw.thresholdCrossing"
+              :key="crossing.locationId"
+              >
+              {{ crossing.locationId }}
+            </v-list-item>
+          </v-list-group>
         </v-list>
       </template>
     </v-data-iterator>
@@ -34,16 +45,20 @@ interface Props {
 const props = defineProps<Props>()
 
 const baseUrl = configManager.get('VITE_FEWS_WEBSERVICES_URL')
-const { thresholds } = useTopologyThresholds(baseUrl, () => props.topologyNode?.id)
+const { thresholds: thresholdsArray } = useTopologyThresholds(baseUrl, () => props.topologyNode?.id)
 
 const warningLevels = computed(() => {
-  if (thresholds.value === undefined || thresholds.value.length === 0) return []
-  return thresholds.value[0].levelThresholdWarningLevels?.map((warningLevel) => {
+  if (thresholdsArray.value === undefined || thresholdsArray.value.length === 0) return []
+  const thresholds = thresholdsArray.value[0]
+  return thresholds.levelThresholdWarningLevels?.map((warningLevel) => {
     return {
       name: warningLevel.name,
       id: warningLevel.id,
       count: warningLevel.count,
       icon: warningLevel.icon ? getResourcesIconsUrl(warningLevel.icon) : undefined,
+      thresholdCrossing: thresholds.levelThresholdCrossings?.filter(
+        (crossing) => crossing.warningLevelId == warningLevel.id
+      )
     }
   })
 })
