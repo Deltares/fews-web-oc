f929f7646a5c627092f18c85b2d290bc8f585928
hvangeffen <hunter.vangeffen@vortech.nl>
2025-03-20 12:34:12 +0100

    Add initial taskrunid support for filter actions use

--- src/services/useDisplayConfig/index.ts
+++ src/services/useDisplayConfig/index.ts
@@ -1,4 +1,5 @@
 import {
+  ActionResult,
   PiWebserviceProvider,
   type ActionsResponse,
 } from '@deltares/fews-pi-requests'
@@ -148,6 +149,7 @@ export function useDisplayConfigFilter(
   filter: MaybeRefOrGetter<Filter | undefined>,
   startTime: MaybeRefOrGetter<Date | undefined>,
   endTime: MaybeRefOrGetter<Date | undefined>,
+  taskRunIds?: MaybeRefOrGetter<string[] | undefined>,
 ): UseDisplayConfigReturn {
   const piProvider = new PiWebserviceProvider(baseUrl, {
     transformRequestFn: createTransformRequestFn(),
@@ -160,27 +162,26 @@ export function useDisplayConfigFilter(
   watchEffect(async () => {
     const _filter = toValue(filter)
     if (_filter === undefined) return
+
     if (isFilterActionsFilter(_filter)) {
       if (!_filter.filterId) return
-      response.value = await piProvider.getFilterActions(_filter)
+
+      const _taskRunIds = toValue(taskRunIds)
+
+      const taskRunsFilter = {
+        ..._filter,
+        taskRunIds: _taskRunIds?.join(','),
+        currentForecastsAlwaysVisible: true,
+      }
+
+      const _response = await piProvider.getFilterActions(
+        _taskRunIds ? taskRunsFilter : _filter,
+      )
+      response.value = _response
     } else if (isTimeSeriesGridActionsFilter(_filter)) {
-      response.value = await piProvider.getTimeSeriesGridActions(_filter)
-      response.value.results.forEach((result) => {
-        result.requests.forEach((request) => {
-          request.key = MD5(request.request).toString()
-        })
-        if (result.config?.timeSeriesDisplay.subplots) {
-          let i = 0
-          result.config.timeSeriesDisplay.subplots.forEach((subPlot) => {
-            subPlot.items.forEach((item) => {
-              if (item.request === undefined) {
-                item.request = `${result.requests[0].key}[${i}]`
-              }
-              i++
-            })
-          })
-        }
-      })
+      const _response = await piProvider.getTimeSeriesGridActions(_filter)
+      addIndexToKeys(_response.results)
+      response.value = _response
     } else {
       displayConfig.value = null
       displays.value = null
@@ -212,4 +213,23 @@ export function useDisplayConfigFilter(
   }
 
   return shell
+}
+
+function addIndexToKeys(results: ActionResult[]) {
+  results.forEach((result) => {
+    result.requests.forEach((request) => {
+      request.key = MD5(request.request).toString()
+    })
+    if (result.config?.timeSeriesDisplay.subplots) {
+      let i = 0
+      result.config.timeSeriesDisplay.subplots.forEach((subPlot) => {
+        subPlot.items.forEach((item) => {
+          if (item.request === undefined) {
+            item.request = `${result.requests[0].key}[${i}]`
+          }
+          i++
+        })
+      })
+    }
+  })
 }
