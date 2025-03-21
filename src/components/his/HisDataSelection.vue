fe34090ca35fce2840a533b0aa251dfbdff5ecc0
hvangeffen <hunter.vangeffen@vortech.nl>
2025-03-21 10:18:24 +0100

    Add intial HIS map and charts for node

---
+++ src/components/his/HisDataSelection.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-select
+    :items="['item1', 'item2', 'item3']"
+    label="name"
+    item-text="name"
+    item-value="id"
+    dense
+    outlined
+    hide-details
+    class="pa-4"
+  />
+</template>
