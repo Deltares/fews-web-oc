00a01c906d8d83051dc3c6eb54186167f164255d
hvangeffen <hunter.vangeffen@vortech.nl>
2025-03-21 10:18:24 +0100

    Add intial HIS map and charts for node

---
+++ src/components/his/HisCharts.vue
@@ -0,0 +1,13 @@
+<template>
+  <TimeSeriesDisplay :nodeId="topologyNodeId" />
+</template>
+
+<script setup lang="ts">
+import TimeSeriesDisplay from '@/components/timeseries/TimeSeriesDisplay.vue'
+
+interface Props {
+  topologyNodeId?: string
+}
+
+defineProps<Props>()
+</script>
---
+++ src/components/his/HisMap.vue
@@ -0,0 +1,41 @@
+<template>
+  <MapComponent :bounds :baseMapId>
+    <slot />
+  </MapComponent>
+</template>
+
+<script setup lang="ts">
+import MapComponent from '@/components/map/MapComponent.vue'
+import { convertBoundingBoxToLngLatBounds } from '@/services/useWms'
+import { useUserSettingsStore } from '@/stores/userSettings'
+import type { BoundingBox } from '@deltares/fews-pi-requests'
+import type { LngLatBounds } from 'maplibre-gl'
+import { computed, ref, watch } from 'vue'
+
+interface Props {
+  boundingBox?: BoundingBox
+}
+
+const props = defineProps<Props>()
+
+const userSettingsStore = useUserSettingsStore()
+const baseMapId = computed(
+  () => (userSettingsStore.get('ui.map.theme')?.value as string) ?? 'automatic',
+)
+
+const bounds = ref<LngLatBounds>()
+watch(
+  () => props.boundingBox,
+  (newBoundingBox) => {
+    if (!newBoundingBox) return
+
+    const newBounds = convertBoundingBoxToLngLatBounds(newBoundingBox)
+
+    const boundsChanged = bounds.value?.toString() !== newBounds.toString()
+    if (boundsChanged) {
+      bounds.value = newBounds
+    }
+  },
+  { immediate: true },
+)
+</script>
