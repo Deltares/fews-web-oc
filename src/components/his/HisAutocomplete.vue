e80860eed77335d48f28ab19c013325760a14dff
hvangeffen <hunter.vangeffen@vortech.nl>
2025-03-25 12:17:17 +0100

    Add parameter selection his

---
+++ src/components/his/HisAutocomplete.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-autocomplete
+    v-model="selectedIds"
+    :items="items"
+    :item-value="getItemValue"
+    :item-title="getItemTitle"
+    :label="label"
+    clearable
+    density="compact"
+    variant="outlined"
+    multiple
+    hide-details
+    :prepend-icon="icon"
+    class="pa-4"
+  >
+    <template #selection="{ item, index }">
+      <span v-if="index < 3">{{ item.title }}</span>
+      <span v-else-if="index === 3">
+        ... ({{ selectedIds.length }} selected)
+      </span>
+    </template>
+    <template #append-inner>
+      <v-chip>{{ items.length }}</v-chip>
+    </template>
+  </v-autocomplete>
+</template>
+
+<script setup lang="ts" generic="T, K">
+interface Props {
+  items: T[]
+  label: string
+  icon?: string
+  getItemValue?: (item: T) => K
+  getItemTitle?: (item: T) => string
+}
+
+defineProps<Props>()
+
+const selectedIds = defineModel<K[]>('selectedIds', {
+  required: true,
+})
+</script>
