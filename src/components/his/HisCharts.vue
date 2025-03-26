809065c6d6e58e5527efe14302c46fb76d6a3746
hvangeffen <hunter.vangeffen@vortech.nl>
2025-03-26 11:52:16 +0100

    Remove timeseries toolbar his

--- src/components/his/HisCharts.vue
+++ src/components/his/HisCharts.vue
@@ -1,14 +1,20 @@
 <template>
-  <TimeSeriesWindowComponent
-    :displayConfig="displayConfig"
-    :settings="settings.charts"
-  />
+  <div class="pt-2 h-100 w-100">
+    <TimeSeriesComponent
+      :config="displayConfig ?? undefined"
+      :displayType="DisplayType.TimeSeriesChart"
+      :settings="settings.charts"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import TimeSeriesWindowComponent from '@/components/timeseries/TimeSeriesWindowComponent.vue'
-import { DisplayConfig } from '@/lib/display/DisplayConfig';
-import { getDefaultSettings, type ComponentSettings } from '@/lib/topology/componentSettings';
+import TimeSeriesComponent from '@/components/timeseries/TimeSeriesComponent.vue'
+import { DisplayConfig, DisplayType } from '@/lib/display/DisplayConfig'
+import {
+  getDefaultSettings,
+  type ComponentSettings,
+} from '@/lib/topology/componentSettings'
 
 interface Props {
   displayConfig: DisplayConfig | null
