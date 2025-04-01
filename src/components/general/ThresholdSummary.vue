07dbdf1100d4710336109b2dd03c357facedd6cc
Anne Markensteijn <anne.markensteijn@vortech.nl>
2025-04-01 15:40:47 +0800

    Add button to open more detailed threshold information

--- src/components/general/ThresholdSummary.vue
+++ src/components/general/ThresholdSummary.vue
@@ -1,28 +1,33 @@
 <template>
   <Teleport to="#main-side-panel-left" defer>
-    <div v-if="warningLevels?.length" class="threshold-summary h-100 flex-wrap">
-      <v-list-item
-        v-for="warningLevel in warningLevels"
-        :key="warningLevel.id"
-        label
-        size="small"
-        density="compact"
-        class="ma-0 pa-1"
-      >
-        <div class="d-flex align-center flex-column">
-          <v-list-item-title>
-            <v-img width="40px" :src="warningLevel.icon"></v-img>
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            {{ warningLevel.name }}
-          </v-list-item-subtitle>
-        </div>
-        <template v-slot:append>
-          <v-list-item-subtitle>
-            {{ warningLevel.count }}
-          </v-list-item-subtitle>
-        </template>
-      </v-list-item>
+    <div v-if="warningLevels?.length" class="threshold-summary h-100 d-flex justify-center flex-column flex-wrap">
+      <div class="align-self-end flex-0-0">
+        <v-btn disabled icon="mdi-menu-open"></v-btn>
+      </div>
+      <div class="d-flex flex-1-0 flex-column justify-center">
+        <v-list-item
+          v-for="warningLevel in warningLevels"
+          :key="warningLevel.id"
+          label
+          size="small"
+          density="compact"
+          class="ma-0 pa-1 w-100"
+        >
+          <div class="d-flex align-center flex-column">
+            <v-list-item-title>
+              <v-img width="40px" :src="warningLevel.icon"></v-img>
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ warningLevel.name }}
+            </v-list-item-subtitle>
+          </div>
+          <template v-slot:append>
+            <v-list-item-subtitle>
+              {{ warningLevel.count }}
+            </v-list-item-subtitle>
+          </template>
+        </v-list-item>
+      </div>
     </div>
   </Teleport>
 </template>
@@ -72,9 +77,5 @@ const warningLevels = computed(() => {
 <style scoped>
 .threshold-summary {
   width: 80px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  flex: 0 0 auto;
 }
 </style>
