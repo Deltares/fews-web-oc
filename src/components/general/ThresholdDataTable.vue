99258a36f72dc11aa840ca8717e956fd67f40181
Anne Markensteijn <anne.markensteijn@vortech.nl>
2025-04-07 10:34:04 +0800

    Add ThresholdDataTable component
    Format threshold details as key-value pairs

---
+++ src/components/general/ThresholdDataTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="table-container">
+    <table class="data-table">
+      <thead></thead>
+      <tbody>
+        <tr v-for="row in tableData">
+          <td class="text-high-emphasis">
+            {{ row.header }}
+            <span class="text-medium-emphasis">
+              {{ row.value }}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Row {
+  header: string
+  value: string | undefined | null
+}
+
+interface Props {
+  tableData: Row[]
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped>
+.table-container {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+
+.data-table {
+  border-collapse: collapse;
+}
+
+.data-table th,
+.data-table td {
+  text-align: left;
+  padding-right: 10px;
+}
+
+.data-table td {
+  padding-bottom: 5px;
+  font-size: 0.75em;
+}
+</style>
