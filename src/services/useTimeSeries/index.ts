02684a7e776e12eb9f43ea385e9a6953a17634be
hvangeffen <hunter.vangeffen@vortech.nl>
2025-04-15 17:29:42 +0800

    Remove lastUpdated from useTimeSeries and expose refresh

--- src/services/useTimeSeries/index.ts
+++ src/services/useTimeSeries/index.ts
@@ -32,6 +32,7 @@ export interface UseTimeSeriesReturn {
   isLoading: Ref<boolean>
   loadingSeriesIds: Ref<string[]>
   interval: Pausable
+  refresh: () => void
 }
 
 const TIMESERIES_POLLING_INTERVAL = 1000 * 30
@@ -55,7 +56,6 @@ function timeZoneOffsetString(offset: number): string {
 export function useTimeSeries(
   baseUrl: string,
   requests: MaybeRefOrGetter<ActionRequest[]>,
-  lastUpdated: MaybeRefOrGetter<Date | undefined>,
   options: MaybeRefOrGetter<UseTimeSeriesOptions>,
   selectedTime?: MaybeRefOrGetter<Date | undefined>,
 ): UseTimeSeriesReturn {
@@ -65,7 +65,7 @@ export function useTimeSeries(
   const loadingSeriesIds = ref<string[]>([])
   const isLoading = computed(() => loadingSeriesIds.value.length > 0)
 
-  watch([lastUpdated, selectedTime ?? ref(), requests, options], () => {
+  watch([selectedTime ?? ref(), requests, options], () => {
     loadTimeSeries()
   })
 
@@ -219,6 +219,7 @@ export function useTimeSeries(
     isLoading,
     loadingSeriesIds,
     interval,
+    refresh: loadTimeSeries,
   }
 }
 
