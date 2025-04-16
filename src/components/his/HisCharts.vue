0fb894cfe8b8dc5a8810a939a0770a9cdd39187a
hvangeffen <hunter.vangeffen@vortech.nl>
2025-04-16 08:51:42 +0800

    Use timeSeriesChart directly for his charts

--- src/components/his/HisCharts.vue
+++ src/components/his/HisCharts.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="pt-2 h-100 w-100">
-    <TimeSeriesComponent
-      :config="displayConfig ?? undefined"
-      :displayType="DisplayType.TimeSeriesChart"
-      :settings="settings.charts"
+  <div class="pt-2 h-100 w-100 d-flex">
+    <TimeSeriesChart
+      v-for="subplot in displayConfig.subplots"
+      :config="subplot"
+      :series="series"
+      :key="subplot.id"
+      :zoomHandler="sharedZoomHandler"
+      :settings="settings.charts.timeSeriesChart"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import TimeSeriesComponent from '@/components/timeseries/TimeSeriesComponent.vue'
-import { DisplayConfig, DisplayType } from '@/lib/display/DisplayConfig'
-import { type ComponentSettings } from '@/lib/topology/componentSettings'
+import TimeSeriesChart from '@/components/charts/TimeSeriesChart.vue'
+import type { DisplayConfig } from '@/lib/display/DisplayConfig'
+import type { Series } from '@/lib/timeseries/timeSeries'
+import type { ComponentSettings } from '@/lib/topology/componentSettings'
+import { ZoomHandler, ZoomMode } from '@deltares/fews-web-oc-charts'
 
 interface Props {
-  displayConfig: DisplayConfig | null
+  displayConfig: DisplayConfig
+  series: Record<string, Series>
   settings: ComponentSettings
 }
 
 defineProps<Props>()
+
+const sharedZoomHandler = new ZoomHandler({
+  sharedZoomMode: ZoomMode.X,
+})
 </script>
