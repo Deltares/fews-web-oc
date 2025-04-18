bb33b5195bda59ef6c55e4957abc1f86dc58e971
hvangeffen <hunter.vangeffen@vortech.nl>
2025-04-18 15:00:07 +0800

    Add resampling timesteps selection

--- src/components/his/functions/HisTimeResampling.vue
+++ src/components/his/functions/HisTimeResampling.vue
@@ -11,7 +11,16 @@
   <HisAutocomplete
     v-model="selectedResamplingMethods"
     :items="resamplingMethods"
-    label="Resampling method"
+    label="Resampling methods"
+    :getItemValue="(item) => item"
+    :getItemTitle="(item) => item.label"
+    multiple
+  />
+
+  <HisAutocomplete
+    v-model="selectedResamplingTimeSteps"
+    :items="availableTimeStepsStore.resamplingTimeSteps"
+    label="Time steps"
     :getItemValue="(item) => item"
     :getItemTitle="(item) => item.label"
     multiple
@@ -37,12 +46,13 @@ import {
   UseDisplayConfigOptions,
 } from '@/services/useDisplayConfig'
 import { useUserSettingsStore } from '@/stores/userSettings'
-import { filterActionsFilter } from '@deltares/fews-pi-requests'
+import { filterActionsFilter, type TimeSteps } from '@deltares/fews-pi-requests'
 import { configManager } from '@/services/application-config'
 import { useTimeSeries } from '@/services/useTimeSeries'
 import { type ResamplingMethod, resamplingMethods } from '@/lib/his/resampling'
 import { ChartSeries } from '@/lib/charts/types/ChartSeries'
 import { uniq } from 'lodash-es'
+import { useAvailableTimeStepsStore } from '@/stores/availableTimeSteps'
 
 interface Props {
   filterId?: string
@@ -59,6 +69,9 @@ const baseUrl = configManager.get('VITE_FEWS_WEBSERVICES_URL')
 
 const selectedTimeseries = ref<ChartSeries[]>([])
 const selectedResamplingMethods = ref<ResamplingMethod[]>([])
+const selectedResamplingTimeSteps = ref<TimeSteps[]>([])
+
+const availableTimeStepsStore = useAvailableTimeStepsStore()
 
 const allSeries = computed(
   () => props.displayConfig?.subplots.flatMap((s) => s.series) ?? [],
@@ -100,12 +113,14 @@ const filter = computed(() => {
     locationIds: selectedLocationIds.value.join(','),
     parameterIds: selectedParameterIds.value.join(','),
     // @ts-ignore: FIXME: fix in types
-    resamplingMethods: selectedResamplingMethods.value.map(
-      (method) => method.value,
-    ),
+    resamplingMethods: selectedResamplingMethods.value
+      .map((method) => method.value)
+      .join(','),
     // TODO: add option for this
     resamplingOmitMissing: true,
-    resamplingTimeStepIds: ['1_day'],
+    resamplingTimeStepIds: selectedResamplingTimeSteps.value
+      .map((timeStep) => timeStep.id)
+      .join(','),
     includeNonResampled: true,
     ...displayConfigOptions.value,
   }
