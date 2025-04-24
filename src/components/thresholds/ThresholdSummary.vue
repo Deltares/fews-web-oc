68a7e1e9b1e1f0a109858675f0d72b445ef87321
Werner Kramer <werner.kramer@vortech.nl>
2025-04-24 15:03:59 +0200

    Refactor ThresholdSummary and ThresholdsParameter components for improved layout

--- src/components/thresholds/ThresholdSummary.vue
+++ src/components/thresholds/ThresholdSummary.vue
@@ -6,80 +6,35 @@
     @click="onExpansionPanelToggle"
     :ripple="false"
   >
-    <div class="d-flex h-100 w-100">
-      <div class="threshold-border vh-100 flex-0-0" />
-      <v-card-text class="border-s py-2 h-100 w-100">
-        <div class="d-flex w-100">
-          <div class="d-flex ga-1 w-100">
-            <div
-              :class="{ 'text-truncate': !expanded }"
-              class="overflow-hidden mt-2"
-            >
-              {{ maxLocationName }}
-            </div>
-            <v-chip
-              v-if="crossings.length > 1"
-              size="small"
-              density="compact"
-              label
-              :text="crossings.length"
-              class="flex-0-0 mt-2 ms-1"
-            />
-            <v-spacer />
-            <div class="flex-0-0">
-              <div :class="{ 'text-wrap': expanded }" class="text-right">
-                {{ maxCrossing.maxValue }} {{ maxParameterUnit }}
-              </div>
-              <v-list-item-subtitle
-                :class="{ 'text-wrap': expanded, 'text-wrap-no': !expanded }"
-              >
-                {{ maxTimeString }}
-              </v-list-item-subtitle>
-            </div>
-          </div>
-        </div>
-      </v-card-text>
-    </div>
+    <template v-for="(crossing, index) in crossings"
+    >
+    <ThresholdsParameter
+      v-if="index === 0 || expanded"
+      :key="crossing.parameterId"
+      :crossing="crossing"
+    >
+      <template #append v-if="crossings.length > 1 && !expanded">
+        <v-chip
+          size="small"
+          density="compact"
+          variant="flat"
+          :text="crossings.length"
+          color="#00BBF0"
+        />
+      </template>
+    </ThresholdsParameter>
+  </template>
   </v-card>
-  <ThresholdsParameter
-    v-if="expanded"
-    v-for="crossing in crossings"
-    :key="crossing.parameterId"
-    :crossing="crossing"
-    class="py-1"
-  />
 </template>
 <script setup lang="ts">
-import { computed } from 'vue'
 import ThresholdsParameter from '@/components/thresholds/ThresholdsParameter.vue'
 import { LevelThresholdCrossings } from '@deltares/fews-pi-requests'
-import { toShortHumanReadableDate } from '@/lib/date'
-import { useParametersStore } from '@/stores/parameters'
-import { useLocationNamesStore } from '@/stores/locationNames'
 
 interface Props {
   crossings: LevelThresholdCrossings[]
 }
-const props = defineProps<Props>()
 
-const parameterStore = useParametersStore()
-const locationNamesStore = useLocationNamesStore()
-
-const maxCrossing = computed(() => props.crossings[0])
-const maxColor = computed(() => maxCrossing.value.color)
-const maxParameter = computed(() =>
-  parameterStore.byId(maxCrossing.value.parameterId),
-)
-const maxParameterUnit = computed(() => maxParameter.value?.unit)
-
-const maxTimeString = computed(() =>
-  toShortHumanReadableDate(maxCrossing.value.maxValueTime),
-)
-const maxLocationName = computed(
-  () =>
-    locationNamesStore.byId(maxCrossing.value.locationId) ??
-    maxCrossing.value.locationId,
-)
+defineProps<Props>()
 
 const expanded = defineModel<boolean>('expanded', {
   required: false,
@@ -93,14 +48,3 @@ function onExpansionPanelToggle() {
   }
 }
 </script>
-
-<style scoped>
-.text-wrap {
-  white-space: normal;
-}
-
-.threshold-border {
-  background-color: v-bind(maxColor);
-  width: 7px;
-}
-</style>
--- src/components/thresholds/ThresholdsParameter.vue
+++ src/components/thresholds/ThresholdsParameter.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="parameter-container d-flex align-center ps-1 pe-4">
-    <div class="flex-0-0">
-      <v-img :src="crossing.icon" width="20" height="20" />
-    </div>
-    <div class="w-100 ms-2">
-      <div class="d-flex">
-        <div>
-          {{ parameterName }}
-        </div>
-        <v-spacer />
-        <div class="ps-1 flex-0-0">
-          {{ crossing.maxValue }} {{ parameterUnit }}
-        </div>
-      </div>
-      <div class="d-flex">
+  <div class="threshold_field d-flex d-row w-100">
+    <div class="threshold_field__border flex-0-0 vh-100" />
+    <div
+      class="d-flex flex-1-1 flex-row align-center ps-1 pe-4 my-1 "
+    >
+      <div class="ms-2 d-flex flex-0-0 flex-column">
+        <span>
+          {{ maxLocationName }}
+        </span>
         <v-list-item-subtitle>
-          {{ crossing.warningLevelName }}
+          {{ parameterName }}
         </v-list-item-subtitle>
-        <v-spacer />
-        <v-list-item-subtitle class="flex-0-0">
+      </div>
+      <div class="ms-2 flex-column">
+        <span class="d-inline"> <slot name="append"></slot> </span>
+      </div>
+      <v-spacer/>
+      <div class="threshold_field__value_date ms-2 d-flex flex-column align-end">
+        <span> {{ crossing.maxValue }} {{ parameterUnit }} </span>
+        <v-list-item-subtitle>
           {{ timeToMaxString }}
         </v-list-item-subtitle>
       </div>
@@ -32,6 +32,7 @@ import type { LevelThresholdCrossings } from '@deltares/fews-pi-requests'
 import { useNow } from '@vueuse/core'
 import { computed } from 'vue'
 import { useParametersStore } from '@/stores/parameters'
+import { useLocationNamesStore } from '@/stores/locationNames'
 
 interface Props {
   crossing: LevelThresholdCrossings
@@ -40,9 +41,12 @@ interface Props {
 const props = defineProps<Props>()
 
 const parameterStore = useParametersStore()
+const locationNamesStore = useLocationNamesStore()
 
 const NOW_REFRESH_INTERVAL = 1000
 const now = useNow({ interval: NOW_REFRESH_INTERVAL })
+
+const maxColor = computed(() => props.crossing.color)
 
 const timeToMaxString = computed(() =>
   toDateAbsDifferenceString(now.value, props.crossing.maxValueTime, {
@@ -57,9 +61,15 @@ const parameter = computed(() => {
 
 const parameterName = computed(
   () =>
-    parameter.value?.shortName ??
     parameter.value?.name ??
+    parameter.value?.shortName ??
     props.crossing.parameterId,
+)
+
+const maxLocationName = computed(
+  () =>
+    locationNamesStore.byId(props.crossing.locationId) ??
+    props.crossing.locationId,
 )
 
 const parameterUnit = computed(() => {
@@ -68,7 +78,16 @@ const parameterUnit = computed(() => {
 </script>
 
 <style scoped>
-.parameter-container {
+.threshold_field__border {
+  background-color: v-bind(maxColor);
+  width: 7px;
+}
+
+.threshold_field__value_date {
+  flex: 0 0 100px;
+}
+
+.threshold_field {
   font-size: 0.875rem;
 }
 </style>
