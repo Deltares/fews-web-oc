cf05fdd6168980723fca14de0c951d41450c44d7
hvangeffen <hunter.vangeffen@vortech.nl>
2025-04-29 14:56:07 +0200

    Change hisCharts to only require subplots not full config

--- src/components/his/HisCharts.vue
+++ src/components/his/HisCharts.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pt-2 h-100 w-100 d-flex">
     <TimeSeriesChart
-      v-for="subplot in displayConfig.subplots"
+      v-for="subplot in subplots"
       :config="subplot"
       :series="series"
       :key="subplot.id"
@@ -13,13 +13,13 @@
 
 <script setup lang="ts">
 import TimeSeriesChart from '@/components/charts/TimeSeriesChart.vue'
-import type { DisplayConfig } from '@/lib/display/DisplayConfig'
+import { ChartConfig } from '@/lib/charts/types/ChartConfig'
 import type { Series } from '@/lib/timeseries/timeSeries'
 import type { ComponentSettings } from '@/lib/topology/componentSettings'
 import { ZoomHandler, ZoomMode } from '@deltares/fews-web-oc-charts'
 
 interface Props {
-  displayConfig: DisplayConfig
+  subplots: ChartConfig[]
   series: Record<string, Series>
   settings: ComponentSettings
 }
