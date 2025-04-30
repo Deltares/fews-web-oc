7dad27f73b4b10acca2d15a85d4a14eb0259a652
hvangeffen <hunter.vangeffen@vortech.nl>
2025-04-30 17:06:39 +0200

    Add ability to remove charts from collection

---
+++ src/components/his/HisCollectionCharts.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="overflow-y-auto">
+    <HisCollectionChart
+      v-for="chart in collection.charts"
+      :collection
+      :chart
+      :series
+      :zoomHandler
+      :settings
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HisCollectionChart from './HisCollectionChart.vue'
+import type { Collection } from '@/lib/his'
+import type { Series } from '@/lib/timeseries/timeSeries'
+import type { ComponentSettings } from '@/lib/topology/componentSettings'
+import { ZoomHandler, ZoomMode } from '@deltares/fews-web-oc-charts'
+
+interface Props {
+  collection: Collection
+  series: Record<string, Series>
+  settings: ComponentSettings
+}
+
+defineProps<Props>()
+
+const zoomHandler = new ZoomHandler({
+  sharedZoomMode: ZoomMode.X,
+})
+</script>
