24e86e08d23f1be28b26c24f93d6e56210bbc2f8
Werner Kramer <werner.kramer@vortech.nl>
2025-05-06 11:43:38 +0200

    Add product browser components and related functionality
    - Introduced `ProductsBrowserView`, `ProductsBrowserTable`, and `ProductDetails` components for displaying product metadata.
    - Implemented `ReactiveIframe` for embedding content and `productTools` for managing product metadata retrieval.
    - Added routing for product browsing and integrated product metadata fetching logic in `useProducts`.
    - Created `attributeNames.json` for attribute labeling.

---
+++ src/components/products/ProductsBrowserView.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="d-flex flex-row h-100 w-100">
+    <ProductsBrowserTable :products="products" class="product-browser__table" />
+    <div class="flex-1-1 h-100 flex-column position-relative">
+      <v-toolbar density="compact" absolute>
+        <v-btn
+          v-if="viewMode === 'html'"
+          color="primary"
+          prepend-icon="mdi-pencil"
+          variant="flat"
+          >edit</v-btn
+        >
+        <v-spacer />
+        <v-toolbar-items>
+          <v-btn append-icon="mdi-chevron-down" class="me-4">
+            {{ timeZero }}
+            <v-menu activator="parent">
+              <v-list density="compact">
+                <v-item-group>
+                  <v-list-item>2025-02-01T12:00:00Z</v-list-item>
+                  <v-list-item>2025-02-01T00:00:00Z</v-list-item>
+                </v-item-group>
+              </v-list>
+            </v-menu>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <iframe
+        v-if="viewMode === 'iframe'"
+        :src="src"
+        class="pdf-iframe"
+      ></iframe>
+      <div v-else class="products-browser-view__canvas overflow-y-auto w-100">
+        <img
+          v-if="viewMode === 'img'"
+          :src="src"
+        />
+        <ReactiveIframe v-else :src="src"></ReactiveIframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import ProductsBrowserTable from '@/components/products/ProductsBrowserTable.vue'
+import ReactiveIframe from '@/components/products/ReactiveIFrame.vue'
+import { getProductURL } from './productTools'
+import { createTransformRequestFn } from '@/lib/requests/transformRequest'
+import { ProductsMetaDataFilter } from '@deltares/fews-pi-requests'
+import { useProducts } from '@/services/useProducts'
+
+interface Props {
+  productId?: string
+}
+
+function getFileExtension(url: string): string {
+  const urlParts = url.split('.')
+  return urlParts[urlParts.length - 1]
+}
+
+function getViewMode(extension: string): string{
+  switch (extension) {
+    case 'html':
+      return 'html'
+    case 'png':
+    case 'jpg':
+    case 'jpeg':
+      return 'img'
+    case 'pdf':
+    default:
+      return 'iframe'
+  }
+}
+
+
+const props = defineProps<Props>()
+const src = ref('')
+const viewMode = ref('')
+const timeZero = ref('')
+const filter = ref<ProductsMetaDataFilter>({
+  versionKey: ['archiveProductId'],
+})
+
+const { products, getProductByKey } = useProducts(filter)
+
+watchEffect(async () => {
+  if (props.productId) {
+    const productMetaData = getProductByKey(props.productId)
+    if (!productMetaData) {
+      src.value = ''
+      return
+    }
+    const url = getProductURL(productMetaData)
+    const extension = getFileExtension(url)
+
+    viewMode.value = getViewMode(extension)
+
+    const transformRequest = createTransformRequestFn()
+    const request = await transformRequest(new Request(url, {}))
+    const reponse = await fetch(request)
+    const urlObject = URL.createObjectURL(await reponse.blob())
+    src.value = urlObject
+  } else {
+    src.value = ''
+  }
+})
+</script>
+
+<style scoped>
+.products-browser-view__canvas {
+  position: absolute;
+  top: 48px;
+  bottom: 0;
+  background-color: #e0e0e0;
+}
+
+img {
+  box-sizing: border-box;
+  margin: auto;
+  box-shadow: 0 0.5mm 2mm rgba(0, 0, 0, 0.3);
+  position: absolute;
+  left: 20px;
+  top: 20px;
+}
+
+.pdf-iframe {
+  position: absolute;
+  width: 100%;
+  top: 48px;
+  height: calc(100% - 48px);
+  border: none;
+  box-sizing: border-box;
+  background-color: white;
+  box-shadow: 0 0.5mm 2mm rgba(0, 0, 0, 0.3);
+}
+
+.product-browser__table {
+  width: 600px;
+  height: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+</style>
