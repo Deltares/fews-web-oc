dbd4bea0f52ec1ee00667f53faf0b8cdfd04e09b
hvangeffen <hunter.vangeffen@vortech.nl>
2025-05-13 16:29:01 +0200

    Add initial image download for his

--- src/components/his/HisCollectionChart.vue
+++ src/components/his/HisCollectionChart.vue
@@ -23,7 +23,7 @@
             title="Remove"
             prepend-icon="mdi-delete"
             density="compact"
-            @click="removeChart(chart)"
+            @click="removeChart()"
           />
           <v-list-item
             title="Download"
@@ -35,12 +35,13 @@
             title="Save as Image"
             prepend-icon="mdi-image"
             density="compact"
-            @click="() => {}"
+            @click="downloadChartImage()"
           />
         </v-list>
       </v-menu>
     </v-card-title>
     <TimeSeriesChart
+      ref="chart"
       :config
       :series
       :zoomHandler
@@ -53,10 +54,15 @@
 import TimeSeriesChart from '@/components/charts/TimeSeriesChart.vue'
 import { timeSeriesDisplayToChartConfig } from '@/lib/charts/timeSeriesDisplayToChartConfig'
 import type { Collection, Chart } from '@/lib/analysis'
+import { downloadImageBitmapAsPng } from '@/lib/download'
+import {
+  convertSvgElementToImageBitmap,
+  createExportableSvgElement,
+} from '@/lib/svg'
 import type { Series } from '@/lib/timeseries/timeSeries'
 import type { ComponentSettings } from '@/lib/topology/componentSettings'
 import type { ZoomHandler } from '@deltares/fews-web-oc-charts'
-import { computed } from 'vue'
+import { computed, useTemplateRef } from 'vue'
 
 interface Props {
   collection: Collection
@@ -69,6 +75,7 @@ interface Props {
 }
 
 const props = defineProps<Props>()
+const chartRef = useTemplateRef('chart')
 
 const domain = computed<[Date, Date] | undefined>(() => {
   return props.chart.type === 'filter'
@@ -80,10 +87,38 @@ const config = computed(() =>
   timeSeriesDisplayToChartConfig(props.chart.subplot, domain.value),
 )
 
-function removeChart(chart: Chart) {
+function removeChart() {
   props.collection.charts.splice(
-    props.collection.charts.findIndex((c) => c === chart),
+    props.collection.charts.findIndex((c) => c === props.chart),
     1,
   )
+}
+
+async function downloadChartImage() {
+  const chart = chartRef.value
+  const svg = chart?.getSvgElement()
+  if (!svg) {
+    throw new Error('Could not obtain SVG element for chart.')
+  }
+
+  // Get print and font style sheet contents and concatenate them into a
+  // self-contained complete stylesheet for exporting the SVG.
+  const getCssContents = async (file: string): Promise<string> => {
+    const response = await fetch(new URL(file, import.meta.env.BASE_URL))
+    return response.text()
+  }
+  const cssContents = await Promise.all(
+    ['weboc-default-style.css'].map(getCssContents),
+  )
+  const finalCssContents = cssContents.join('')
+
+  // Create exportable SVG element with embedded style sheet.
+  const exportSvg = createExportableSvgElement(svg, finalCssContents)
+
+  // Convert SVG to bitmap, then download it as PNG.
+  const targetSize: [number, number] = [1920, 1080]
+  const bitmap = await convertSvgElementToImageBitmap(exportSvg, targetSize)
+
+  await downloadImageBitmapAsPng(bitmap, 'chart.png')
 }
 </script>
