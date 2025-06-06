cc734b42746919207b83fe196eb8e3b706db2f67
hvangeffen <hunter.vangeffen@vortech.nl>
2025-06-06 09:37:06 +0200

    Add fix for correlation start and end times

--- src/lib/analysis/collection.ts
+++ src/lib/analysis/collection.ts
@@ -1,14 +1,20 @@
 import type { Collection } from './types'
 
-const TWO_DAYS_IN_MS = 2 * 24 * 60 * 60 * 1000
+const MS_IN_TWO_DAYS = 2 * 24 * 60 * 60 * 1000
+const MS_IN_FIVE_MINUTES = 5 * 60 * 1000
 
 export function createCollection(name: string): Collection {
   return {
     name,
     charts: [],
     settings: {
-      startTime: new Date(Date.now() - TWO_DAYS_IN_MS),
-      endTime: new Date(Date.now() + TWO_DAYS_IN_MS),
+      // FIXME: Currently the backend for correlation only supports 5 minute intervals
+      startTime: roundToNearestFiveMinutes(Date.now() - MS_IN_TWO_DAYS),
+      endTime: roundToNearestFiveMinutes(Date.now() + MS_IN_TWO_DAYS),
     },
   }
+}
+
+function roundToNearestFiveMinutes(time: number): Date {
+  return new Date(Math.round(time / MS_IN_FIVE_MINUTES) * MS_IN_FIVE_MINUTES)
 }
