b8e23d08326b73606c170765e65671af2fbdd9f9
hvangeffen <hunter.vangeffen@vortech.nl>
2025-06-11 03:00:31 +0200

    Add initial workflow chart support

---
+++ src/components/analysis/functions/AnalysisWorkflow.vue
@@ -0,0 +1,44 @@
+<template>
+  <WhatIfDisplay :workflows @postTask="addChart" />
+</template>
+
+<script setup lang="ts">
+import WhatIfDisplay from '@/components/tasksdisplay/WhatIfDisplay.vue'
+import { AsyncChart, Chart } from '@/lib/analysis'
+import { useAvailableWorkflowsStore } from '@/stores/availableWorkflows'
+import type { ToolboxWorkflow } from '@deltares/fews-pi-requests'
+import { computed } from 'vue'
+
+interface Props {
+  customToolBox: ToolboxWorkflow
+}
+const props = defineProps<Props>()
+
+interface Emits {
+  addChart: [chart: Chart]
+}
+const emit = defineEmits<Emits>()
+
+const workflowsStore = useAvailableWorkflowsStore()
+
+const workflows = computed(() => {
+  const workflow = workflowsStore.byId(props.customToolBox.workflowId)
+  return workflow ? [workflow] : []
+})
+
+function addChart(taskId: string) {
+  const result = props.customToolBox.results
+  const title = props.customToolBox.name
+
+  const chart: AsyncChart = {
+    id: crypto.randomUUID(),
+    type: 'async',
+    title,
+    subplot: { items: [] },
+    taskId,
+    result,
+  }
+
+  emit('addChart', chart)
+}
+</script>
