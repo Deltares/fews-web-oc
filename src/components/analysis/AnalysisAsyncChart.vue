28b69b7aaf43314ac6854a7c56548d0ad6478a08
hvangeffen <hunter.vangeffen@vortech.nl>
2025-06-11 03:03:54 +0200

    Use task run summary for analysis task display

--- src/components/analysis/AnalysisAsyncChart.vue
+++ src/components/analysis/AnalysisAsyncChart.vue
@@ -1,61 +1,38 @@
 <template>
   <AnalysisChartCard :chart="chart" v-bind="$attrs">
-    <v-card flat border class="ma-2" v-if="task">
-      <v-list-item>
-        <template #prepend>
-          <v-icon
-            :icon="getIconForTaskStatus(task.status)"
-            :color="getColorForTaskStatus(task.status)"
-            size="20"
-          />
-        </template>
-        <v-list-item-subtitle>
-          {{ toHumanReadableDate(task.timeZeroTimestamp) }}
-        </v-list-item-subtitle>
-
-        <v-list-item-title>{{ workflow?.name }}</v-list-item-title>
-        <v-list-item-subtitle>
-          {{ task.taskId }}
-        </v-list-item-subtitle>
-      </v-list-item>
-      <TaskRunProgress
-        v-if="task?.status === 'running'"
-        :dispatch-timestamp="task.dispatchTimestamp"
-        :expected-runtime-seconds="workflow?.expectedRuntimeSeconds ?? null"
-      />
-    </v-card>
+    <TaskRunSummary v-if="task" :task :canVisualize="false" class="mb-2 mx-3" />
+    <v-skeleton-loader
+      v-else
+      height="70"
+      type="list-item-two-line"
+      class="rounded-lg"
+    />
   </AnalysisChartCard>
 </template>
 
 <script setup lang="ts">
-import TaskRunProgress from '@/components/tasks/TaskRunProgress.vue'
+import TaskRunSummary from '@/components/tasks/TaskRunSummary.vue'
 import AnalysisChartCard from '@/components/analysis/AnalysisChartCard.vue'
 import type { AsyncChart } from '@/lib/analysis'
 import {
   convertFewsPiTaskRunToTaskRun,
-  getColorForTaskStatus,
-  getIconForTaskStatus,
   getTaskStatusCategory,
 } from '@/lib/taskruns'
 import { configManager } from '@/services/application-config'
 import { useTaskRuns } from '@/services/useTaskRuns'
 import { useTaskRunStatus } from '@/services/useTaskRunStatus'
-import { useAvailableWorkflowsStore } from '@/stores/availableWorkflows'
 import { computed, watchEffect } from 'vue'
-import { toHumanReadableDate } from '@/lib/date'
 
 interface Props {
   chart: AsyncChart
 }
 const props = defineProps<Props>()
 
-const taskRefreshInterval = 5000
-
 const { taskRunStatus, interval: taskRunStatusInterval } = useTaskRunStatus(
   () => ({
     taskId: props.chart.taskId,
   }),
-  taskRefreshInterval,
+  2000,
 )
 
 watchEffect(() => {
@@ -72,7 +49,7 @@ const baseUrl = configManager.get('VITE_FEWS_WEBSERVICES_URL')
 const { taskRuns, interval: taskRunInterval } = useTaskRuns(
   baseUrl,
   taskRunIds,
-  taskRefreshInterval,
+  5000,
 )
 const task = computed(() => {
   if (!taskRuns.value?.length) return
@@ -90,13 +67,5 @@ watchEffect(() => {
   if (category.value === 'completed' || category.value === 'failed') {
     taskRunInterval.value?.pause()
   }
-})
-
-const availableWorkflowsStore = useAvailableWorkflowsStore()
-const workflow = computed(() => {
-  const workflowId = task.value?.workflowId
-  if (!workflowId) return
-
-  return availableWorkflowsStore.byId(task.value.workflowId)
 })
 </script>
