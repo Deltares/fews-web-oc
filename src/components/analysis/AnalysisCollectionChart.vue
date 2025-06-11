48e4c62577b12e26ace4c13872d10c5ccce6ddd6
hvangeffen <hunter.vangeffen@vortech.nl>
2025-06-11 03:02:37 +0200

    Move chart card to seperate component and add dummy async chart

--- src/components/analysis/AnalysisCollectionChart.vue
+++ src/components/analysis/AnalysisCollectionChart.vue
@@ -1,59 +1,19 @@
 <template>
-  <v-card flat density="compact" color="transparent">
-    <v-card-title class="d-flex align-center">
-      <EditableTitle v-model="chart.title" class="ml-2" />
-      <v-spacer />
-      <v-menu location="bottom right">
-        <template #activator="{ props }">
-          <v-btn
-            icon="mdi-dots-horizontal"
-            variant="text"
-            density="compact"
-            v-bind="props"
-          />
-        </template>
-        <v-list density="compact">
-          <v-list-item
-            title="Edit"
-            prepend-icon="mdi-pencil"
-            density="compact"
-            @click="() => {}"
-          />
-          <v-list-item
-            title="Remove"
-            prepend-icon="mdi-delete"
-            density="compact"
-            @click="removeChart(chart)"
-          />
-          <v-list-item
-            title="Download"
-            prepend-icon="mdi-download"
-            density="compact"
-            @click="() => {}"
-          />
-          <v-list-item
-            title="Save as Image"
-            prepend-icon="mdi-image"
-            density="compact"
-            @click="() => {}"
-          />
-        </v-list>
-      </v-menu>
-    </v-card-title>
+  <AnalysisChartCard :chart="chart" v-bind="$attrs">
     <TimeSeriesChart
       :config
       :series
       :zoomHandler
       :settings="settings.charts.timeSeriesChart"
     />
-  </v-card>
+  </AnalysisChartCard>
 </template>
 
 <script setup lang="ts">
+import AnalysisChartCard from '@/components/analysis/AnalysisChartCard.vue'
 import TimeSeriesChart from '@/components/charts/TimeSeriesChart.vue'
-import EditableTitle from '@/components/general/EditableTitle.vue'
 import { timeSeriesDisplayToChartConfig } from '@/lib/charts/timeSeriesDisplayToChartConfig'
-import type { Collection, Chart } from '@/lib/analysis'
+import type { Chart } from '@/lib/analysis'
 import type { Series } from '@/lib/timeseries/timeSeries'
 import type { ComponentSettings } from '@/lib/topology/componentSettings'
 import type { ZoomHandler } from '@deltares/fews-web-oc-charts'
@@ -61,7 +21,6 @@ import { computed } from 'vue'
 import { TimeSeriesDisplaySubplot } from '@deltares/fews-pi-requests'
 
 interface Props {
-  collection: Collection
   chart: Chart
   subplot: TimeSeriesDisplaySubplot
   series: Record<string, Series>
@@ -75,11 +34,4 @@ const props = defineProps<Props>()
 const config = computed(() =>
   timeSeriesDisplayToChartConfig(props.subplot, props.domain),
 )
-
-function removeChart(chart: Chart) {
-  props.collection.charts.splice(
-    props.collection.charts.findIndex((c) => c === chart),
-    1,
-  )
-}
 </script>
