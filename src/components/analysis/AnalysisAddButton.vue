56a98ca49281e12bcb28dabfe7ce352bffffc1c4
hvangeffen <hunter.vangeffen@vortech.nl>
2025-06-16 05:31:51 +0200

    Revert analysis add button

--- src/components/analysis/AnalysisAddButton.vue
+++ src/components/analysis/AnalysisAddButton.vue
@@ -1,61 +1,21 @@
 <template>
   <v-btn
-    v-bind="props"
     variant="flat"
     :disabled="disabled"
     prepend-icon="mdi-plus"
-    :text="newChartTitle"
+    text="Create new chart"
     :color="disabled ? undefined : 'primary'"
     :loading="loading"
-    class="mr-2"
-    @click="emit('addToChart')"
+    @click="emit('click', $event)"
   />
-
-  <v-menu>
-    <template #activator="{ props }">
-      <v-btn
-        v-bind="props"
-        variant="flat"
-        :disabled="disabled"
-        prepend-icon="mdi-playlist-plus"
-        text="Add to chart"
-        :color="disabled ? undefined : 'primary'"
-        :loading="loading"
-      />
-    </template>
-    <v-list>
-      <v-list-item
-        v-for="chart in filterCharts"
-        :key="chart.id"
-        :title="chart.title"
-        prepend-icon="mdi-chart-bar"
-        density="compact"
-        @click="emit('addToChart', chart)"
-      />
-    </v-list>
-  </v-menu>
 </template>
 
 <script setup lang="ts">
-import { Chart } from '@/lib/analysis'
-import { computed } from 'vue'
-
 interface Props {
-  charts: Chart[]
   disabled?: boolean
   loading?: boolean
-  newChartTitle?: string
 }
-const props = withDefaults(defineProps<Props>(), {
-  newChartTitle: 'Create new chart',
-})
+defineProps<Props>()
 
-interface Emits {
-  addToChart: [chart?: Chart]
-}
-const emit = defineEmits<Emits>()
-
-const filterCharts = computed(() =>
-  props.charts.filter((chart) => chart.type === 'filter'),
-)
+const emit = defineEmits(['click'])
 </script>
