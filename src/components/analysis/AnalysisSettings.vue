68fe36a1202ee4ffc432de4cc146a18801604809
hvangeffen <hunter.vangeffen@vortech.nl>
2025-06-17 09:19:13 +0200

    Add collection rename

--- src/components/analysis/AnalysisSettings.vue
+++ src/components/analysis/AnalysisSettings.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card flat title="Settings">
+  <v-card flat title="Time Settings">
     <v-card-text>
       <div class="d-flex ga-2">
         <v-date-input
@@ -22,35 +22,49 @@
           display-format="fullDate"
         />
       </div>
-      <v-menu>
-        <template #activator="{ props }">
-          <v-btn
-            v-bind="props"
-            class="mt-2"
-            variant="tonal"
-            color="error"
-            text="Delete Collection"
-            prepend-icon="mdi-delete"
-          />
-        </template>
-        <template #default="{ isActive }">
-          <v-card
-            title="Delete Collection"
-            text="Are you sure you want to delete this collection?"
-          >
-            <v-card-actions>
-              <v-spacer />
-              <v-btn
-                variant="flat"
-                color="primary"
-                @click="isActive.value = false"
-                text="cancel"
-              />
-              <v-btn text="delete" @click="$emit('delete-collection')" />
-            </v-card-actions>
-          </v-card>
-        </template>
-      </v-menu>
+    </v-card-text>
+  </v-card>
+
+  <v-card flat title="Collection Settings">
+    <v-card-text>
+        <v-text-field
+          v-model="collection.name"
+          label="Rename"
+          variant="outlined"
+          hide-details
+          density="compact"
+          class="mb-2"
+        />
+
+        <v-menu>
+          <template #activator="{ props }">
+            <v-btn
+              v-bind="props"
+              variant="tonal"
+              color="error"
+              text="Delete Collection"
+              prepend-icon="mdi-delete"
+              hide-details
+            />
+          </template>
+          <template #default="{ isActive }">
+            <v-card
+              title="Delete Collection"
+              text="Are you sure you want to delete this collection?"
+            >
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  variant="flat"
+                  color="primary"
+                  @click="isActive.value = false"
+                  text="cancel"
+                />
+                <v-btn text="delete" @click="$emit('delete-collection')" />
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-menu>
     </v-card-text>
   </v-card>
 </template>
