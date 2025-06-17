11e0d41d806ffa0aa8426311e021359fb40e31f0
Werner Kramer <werner.kramer@vortech.nl>
2025-06-17 09:04:20 +0200

    Update Vite configuration to use new environment variables for proxy and CSP headers

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 5173,
       proxy: {
-        '/FewsWebServices/': `${env.DEVSERVER_PROXY_FEWS_PI}`,
+        '/FewsWebServices/': `${env.DEV_PROXY_FEWS_PI}`,
       },
       headers: {
         'content-security-policy': [
