af80acf7dcc4c703aef6b82536bb79920885a17f
Werner Kramer <werner.kramer@vortech.nl>
2025-06-20 17:33:10 +0200

    Runtime loading of remotes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import vuetify from './plugins/vuetify.js'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { defineCustomElements } from '@deltares/fews-ssd-webcomponent/loader'
+import { init } from '@module-federation/enhanced/runtime'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
 const pinia = createPinia()
@@ -27,6 +28,20 @@ fetch(`${import.meta.env.BASE_URL}app-config.json`)
     configManager.update(data)
     if (configManager.authenticationIsEnabled) {
       await authenticationManager.init(configManager.getUserManagerSettings())
+    }
+    const remoteManifest = configManager.get('VITE_FEWS_WEBOC_MF_MANIFEST_URL')
+    if (remoteManifest) {
+      console.log(`Loading remote manifest from: ${remoteManifest}`)
+      const initOptions = {
+        name: 'weboc_plugins',
+        remotes: [{
+          // mf-manifest.json is a file type generated in the new version of Module Federation build tools, providing richer functionality compared to remoteEntry
+          // Preloading depends on the use of the mf-manifest.json file type
+          name: 'weboc_mdba_plugins',
+          entry: remoteManifest,
+        }],
+      }
+      await init(initOptions)
     }
     app.use(router)
     app.mount('#app')
