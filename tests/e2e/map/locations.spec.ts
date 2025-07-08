3f6e479b3c7173a1a4862be61e8d75a7c2ec3a55
Werner Kramer <werner.kramer@vortech.nl>
2025-07-08 16:07:37 +0200

    Refactor E2E tests for threshold locations visibility in viewer_rivers_level_stations and viewer_rivers_critical_points; add tests for viewer_coastal_water_levels

--- tests/e2e/map/locations.spec.ts
+++ tests/e2e/map/locations.spec.ts
@@ -14,11 +14,17 @@ test('Thresholds locations should be visible for viewer_rivers_level_stations_fo
   await mapController('mainMap').setView({ zoom: 8, center: [31, -30] })
   await mapController('mainMap').waitToMapStable()
 
+  const watchLocations = mapLocator(
+    'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 1]]',
+  )
+  const watchLocationsCount = await watchLocations.count()
+  await expect(watchLocations, 'Watch locations visible').toBeVisibleOnMap()
+  await expect(watchLocationsCount, 'Watch locations count').toBe(1)
+
   const alertLocations = mapLocator(
     'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 2]]',
   )
-  const alertLocationsCount = await alertLocations.count()
-
+  const alertLocationsCount = await alertLocations.count()  
   await expect(alertLocations, 'Alert locations visible').toBeVisibleOnMap()
   await expect(alertLocationsCount, 'Alert locations count').toBe(1)
 
@@ -26,17 +32,8 @@ test('Thresholds locations should be visible for viewer_rivers_level_stations_fo
     'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 3]]',
   )
   const warningLocationsCount = await warningLocations.count()
-
   await expect(warningLocations, 'Warning locations visible').toBeVisibleOnMap()
   await expect(warningLocationsCount, 'Warning locations count').toBe(1)
-
-  const watchLocations = mapLocator(
-    'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 1]]',
-  )
-  const watchLocationsCount = await watchLocations.count()
-
-  await expect(watchLocations, 'Watch locations visible').toBeVisibleOnMap()
-  await expect(watchLocationsCount, 'Watch locations count').toBe(1)
 
   const noThresholdLocations = mapLocator(
     'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["!", ["has", "thresholdSeverity"]]]',
@@ -65,6 +62,14 @@ test('Thresholds locations should be visible for viewer_rivers_critical_points',
   await mapController('mainMap').setView({ zoom: 8, center: [31, -30] })
   await mapController('mainMap').waitToMapStable()
 
+  const watchLocations = mapLocator(
+    'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 1]]',
+  )
+  const watchLocationsCount = await watchLocations.count()
+
+  await expect(watchLocations, 'Watch locations visible').toBeHiddenOnMap()
+  await expect(watchLocationsCount, 'Watch locations count').toBe(0)
+
   const alertLocations = mapLocator(
     'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 2]]',
   )
@@ -81,6 +86,33 @@ test('Thresholds locations should be visible for viewer_rivers_critical_points',
   await expect(warningLocations, 'Warning locations visible').toBeVisibleOnMap()
   await expect(warningLocationsCount, 'Warning locations count').toBe(7)
 
+  const noThresholdLocations = mapLocator(
+    'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["!", ["has", "thresholdSeverity"]]]',
+  )
+  const noThresholdLocationsCount = await noThresholdLocations.count()
+  await expect(
+    noThresholdLocations,
+    'No threshold locations visible',
+  ).toBeVisibleOnMap()
+  await expect(noThresholdLocationsCount, 'No threshold locations count').toBe(
+    225,
+  )
+})
+
+test('Thresholds locations should be visible for viewer_coastal_water_levels', async ({
+  page,
+  mapLocator,
+  mapController,
+}) => {
+
+  await page.goto(
+    '/topology/node/viewer_coastal_water_levels/viewer_coastal_water_levels_d3d/map',
+  )
+
+  await mapController('mainMap').waitToMapLoaded()
+  await mapController('mainMap').setView({ zoom: 8, center: [31, -30] })
+  await mapController('mainMap').waitToMapStable()
+
   const watchLocations = mapLocator(
     'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 1]]',
   )
@@ -88,6 +120,30 @@ test('Thresholds locations should be visible for viewer_rivers_critical_points',
 
   await expect(watchLocations, 'Watch locations visible').toBeHiddenOnMap()
   await expect(watchLocationsCount, 'Watch locations count').toBe(0)
+
+  const alertLocations = mapLocator(
+    'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 2]]',
+  )
+  const alertLocationsCount = await alertLocations.count()
+
+  await expect(alertLocations, 'Alert locations visible').toBeHiddenOnMap()
+  await expect(alertLocationsCount, 'Alert locations count').toBe(0)
+
+  const warningLocations = mapLocator(
+    'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 3]]',
+  )
+  const warningLocationsCount = await warningLocations.count()
+
+  await expect(warningLocations, 'Warning locations visible').toBeHiddenOnMap()
+  await expect(warningLocationsCount, 'Warning locations count').toBe(0)
+
+  const watchPercentilesLocations = mapLocator(
+    'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["==", ["get", "thresholdSeverity"], 9]]',
+  )
+  const watchPercentilesLocationsCount = await watchPercentilesLocations.count()
+
+  await expect(watchPercentilesLocations, 'Warning locations visible').toBeVisibleOnMap()
+  await expect(watchPercentilesLocationsCount, 'Warning locations count').toBe(1)
 
   const noThresholdLocations = mapLocator(
     'map[id=mainMap] layer[id=weboc-layer-location-symbol] filter["all", ["!", ["has", "thresholdSeverity"]]]',
@@ -99,7 +155,8 @@ test('Thresholds locations should be visible for viewer_rivers_critical_points',
     'No threshold locations visible',
   ).toBeVisibleOnMap()
   await expect(noThresholdLocationsCount, 'No threshold locations count').toBe(
-    225,
+    10,
   )
 })
+
 
