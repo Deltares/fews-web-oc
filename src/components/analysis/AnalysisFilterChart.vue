046ae5d53bae2252bb9d91bae68b1491e9737505
hvangeffen <hunter.vangeffen@vortech.nl>
2025-07-09 14:42:34 +0200

    Fix correlation chart's not allowing for edits

--- src/components/analysis/AnalysisFilterChart.vue
+++ src/components/analysis/AnalysisFilterChart.vue
@@ -1,31 +1,26 @@
 <template>
-  <AnalysisChartCard :chart="chart" v-bind="$attrs" @edit="editing = true">
-    <TimeSeriesChart
-      :config
-      :series
-      :zoomHandler
-      :settings="settings.charts.timeSeriesChart"
-    />
-    <AnalysisChartEdit v-model="editing" :chart />
-  </AnalysisChartCard>
+  <AnalysisPlotChart
+    :chart
+    :config
+    :series
+    :settings
+    :zoomHandler
+    v-bind="$attrs"
+  />
 </template>
 
 <script setup lang="ts">
-import AnalysisChartCard from '@/components/analysis/AnalysisChartCard.vue'
-import AnalysisChartEdit from '@/components/analysis/AnalysisChartEdit.vue'
-import TimeSeriesChart from '@/components/charts/TimeSeriesChart.vue'
+import AnalysisPlotChart from '@/components/analysis/AnalysisPlotChart.vue'
 import { timeSeriesDisplayToChartConfig } from '@/lib/charts/timeSeriesDisplayToChartConfig'
-import type { PlotChart } from '@/lib/analysis'
+import type { FilterChart } from '@/lib/analysis'
 import type { Series } from '@/lib/timeseries/timeSeries'
 import type { ComponentSettings } from '@/lib/topology/componentSettings'
 import type { ZoomHandler } from '@deltares/fews-web-oc-charts'
-import { computed, ref } from 'vue'
-import { TimeSeriesDisplaySubplot } from '@deltares/fews-pi-requests'
+import { computed } from 'vue'
 import { getSubplotWithDomain } from '@/lib/display'
 
 interface Props {
-  chart: PlotChart
-  subplot: TimeSeriesDisplaySubplot
+  chart: FilterChart
   series: Record<string, Series>
   zoomHandler?: ZoomHandler
   settings: ComponentSettings
@@ -34,26 +29,16 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const domain = computed(() => {
-  if (props.chart.type === 'filter') {
-    return props.chart.domain ?? props.domain
-  }
-  return props.domain
-})
+const domain = computed(() => props.chart.domain ?? props.domain)
 
-const zoomHandler = computed(() => {
-  if (props.chart.type === 'filter' && props.chart.domain) {
-    return undefined
-  }
-  return props.zoomHandler
-})
+const zoomHandler = computed(() =>
+  props.chart.domain ? undefined : props.zoomHandler,
+)
 
 const config = computed(() =>
   getSubplotWithDomain(
-    timeSeriesDisplayToChartConfig(props.subplot),
+    timeSeriesDisplayToChartConfig(props.chart.subplot),
     domain.value,
   ),
 )
-
-const editing = ref(false)
 </script>
