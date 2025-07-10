20d11b08bae389069eb2cad56115d0b0e3488012
hvangeffen <hunter.vangeffen@vortech.nl>
2025-07-10 11:36:23 +0200

    Add legend range change tests

--- tests/e2e/map/legend.spec.ts
+++ tests/e2e/map/legend.spec.ts
@@ -11,7 +11,7 @@ async function switchToSystemUnits(page: Page) {
   await page.getByRole('button', { name: 'Close User Settings' }).click()
 }
 
-function locateLegendImage(page: Page) {
+function locateLegendSvg(page: Page) {
   return page.getByLabel('map legend').getByRole('img')
 }
 
@@ -22,15 +22,15 @@ test.describe('WMS Legend', () => {
       // FIXME: Requires a node with different units in the legend
       await page.goto('')
 
-      const legendImage = locateLegendImage(page)
+      const legendSvg = locateLegendSvg(page)
 
       // TODO: Add before text
-      await expect(legendImage).toContainText('')
+      await expect(legendSvg).toContainText('')
 
       await switchToSystemUnits(page)
 
       // TODO: Add after text
-      await expect(legendImage).toContainText('')
+      await expect(legendSvg).toContainText('')
     },
   )
 
@@ -41,12 +41,103 @@ test.describe('WMS Legend', () => {
       '/topology/node/viewer_meteorology_rainfall_forecast/viewer_meteorology_rainfall_forecast_saws_1x1/map/saws1',
     )
 
-    const legendImage = locateLegendImage(page)
+    const legendSvg = locateLegendSvg(page)
 
-    await expect(legendImage).toContainText('0210203050SAWS forecast (1x1 km)')
+    await expect(legendSvg).toContainText('0210203050SAWS forecast (1x1 km)')
 
     await switchToSystemUnits(page)
 
-    await expect(legendImage).toContainText('0210203050SAWS forecast (1x1 km)')
+    await expect(legendSvg).toContainText('0210203050SAWS forecast (1x1 km)')
+  })
+
+  test('when changing the range of the legend, the legend should update', async ({
+    page,
+  }) => {
+    await page.goto(
+      '/topology/node/viewer_meteorology_rainfall_forecast/viewer_meteorology_rainfall_forecast_saws_1x1/map/saws1',
+    )
+
+    const legendSvg = locateLegendSvg(page)
+    const originalText = '0210203050SAWS forecast (1x1 km)'
+
+    await expect(legendSvg).toContainText(originalText)
+
+    await page.getByRole('button', { name: 'Layer information' }).click()
+
+    const maxInput = page.getByRole('textbox', { name: 'Max Max' })
+    await maxInput.click()
+    await maxInput.fill('100')
+    await maxInput.press('Enter')
+
+    await expect(legendSvg).toContainText('04204060100SAWS forecast (1x1 km)')
+
+    const minInput = page.getByRole('textbox', { name: 'Min Min' })
+    await minInput.click()
+    await minInput.fill('20')
+    await minInput.press('Enter')
+
+    await expect(legendSvg).toContainText(
+      '2023.2365268100SAWS forecast (1x1 km)',
+    )
+
+    await page
+      .getByRole('button', { name: 'Reset current colour scale' })
+      .click()
+
+    await expect(legendSvg).toContainText(originalText)
+  })
+
+  test.fixme(
+    'when changing display units for layer with different units the range is reset',
+    async ({ page }) => {
+      // FIXME: Requires a node with different units in the legend
+      await page.goto('')
+
+      const legendSvg = locateLegendSvg(page)
+
+      // TODO: Add before text
+      await expect(legendSvg).toContainText('')
+
+      await page.getByRole('button', { name: 'Layer information' }).click()
+
+      const maxInput = page.getByRole('textbox', { name: 'Max Max' })
+      await maxInput.click()
+      await maxInput.fill('100')
+      await maxInput.press('Enter')
+
+      // TODO: Add after range change text
+      await expect(legendSvg).toContainText('')
+
+      await switchToSystemUnits(page)
+
+      // TODO: Add after switch to systems units text
+      await expect(legendSvg).toContainText('')
+    },
+  )
+
+  test('when changing display units for layer with same units the range is reset', async ({
+    page,
+  }) => {
+    await page.goto(
+      '/topology/node/viewer_meteorology_rainfall_forecast/viewer_meteorology_rainfall_forecast_saws_1x1/map/saws1',
+    )
+
+    const legendSvg = locateLegendSvg(page)
+    const originalText = '0210203050SAWS forecast (1x1 km)'
+
+    await expect(legendSvg).toContainText(originalText)
+
+    await page.getByRole('button', { name: 'Layer information' }).click()
+
+    const maxInput = page.getByRole('textbox', { name: 'Max Max' })
+    await maxInput.click()
+    await maxInput.fill('100')
+    await maxInput.press('Enter')
+
+    await expect(legendSvg).toContainText('04204060100SAWS forecast (1x1 km)')
+
+    await switchToSystemUnits(page)
+
+    await expect(legendSvg).toContainText(originalText)
   })
 })
