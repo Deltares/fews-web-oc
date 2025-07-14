c482b572515c760880c8da11e3aab8fdd2bb6d11
Rudie Ekkelenkamp <rudie.ekkelenkamp@deltares.nl>
2025-07-14 15:04:39 +0200

    improve formatting.

--- playwright-e2e.config.ts
+++ playwright-e2e.config.ts
@@ -18,12 +18,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [
-  ['list'],
-  ['junit', { outputFile: 'results/test-results.xml' }],
-  ['html', { outputFolder: 'playwright-report', open: 'never' }]
-  ] : 'html'
-  ,
+  // prettier-ignore
+  reporter: process.env.CI
+    ? [
+        ['list'],
+        ['html',  {outputFolder:'playwright-report', open: 'never'}],
+        ['junit', { outputFile: 'results/test-results.xml' }]
+      ]
+    : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
