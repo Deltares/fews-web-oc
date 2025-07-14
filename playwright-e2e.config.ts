b7cbb69fd6818b337319f032c37bbaa2dc772129
hvangeffen <hunter.vangeffen@vortech.nl>
2025-07-14 15:58:59 +0200

    Format playwright-e2e config

--- playwright-e2e.config.ts
+++ playwright-e2e.config.ts
@@ -18,12 +18,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // prettier-ignore
   reporter: process.env.CI
     ? [
         ['list'],
-        ['html',  {outputFolder:'playwright-report', open: 'never'}],
-        ['junit', { outputFile: 'results/test-results.xml' }]
+        ['html', { outputFolder: 'playwright-report', open: 'never' }],
+        ['junit', { outputFile: 'results/test-results.xml' }],
       ]
     : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
