ceed80bc9d2f2a218cd5fe3e19248bf5501f6e56
hvangeffen <hunter.vangeffen@vortech.nl>
2025-07-16 13:26:04 +0200

    Fix threhsold test for new topologyId config

--- tests/e2e/chart/threshold.spec.ts
+++ tests/e2e/chart/threshold.spec.ts
@@ -1,14 +1,12 @@
 import { test, expect } from '@playwright/test'
 
+const base = '/topology/early_warning/node'
+
 test.describe('Thresholds displayed in graph', () => {
   test('should display the thresholds correctly', async ({ page }) => {
     await page.goto(
-      '/topology/node/viewer_rivers_level_stations/viewer_rivers_level_stations_forecast/map/waterdepth_sfincs_inland/location/Umgeni_Mouth_level',
+      `${base}/viewer_rivers_level_stations/viewer_rivers_level_stations_forecast/map/waterdepth_sfincs_inland/location/Umgeni_Mouth_level`,
     )
-    // Open thresholds panel
-    await page.getByRole('button', { name: 'Badge' }).click()
-    // Click on the v-card containing the text
-    await page.locator('.v-card:has-text("Umgeni Mouth")').click()
     // Verify three thresholds are displayed in the graph
     const alertGroup = page.locator('g').filter({ hasText: /^Alert$/ })
     await expect(alertGroup).toBeVisible()
