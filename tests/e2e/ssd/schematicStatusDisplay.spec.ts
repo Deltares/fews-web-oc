1f2507ce64f8561ef5346068c6653fd3856f4627
cees-wolfs-vt <cees.wolfs@vortech.nl>
2025-07-25 09:56:01 +0200

    Rfeactor ssd tests with beforeeach

--- tests/e2e/ssd/schematicStatusDisplay.spec.ts
+++ tests/e2e/ssd/schematicStatusDisplay.spec.ts
@@ -2,17 +2,19 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Schematic Status Display Tests', () => {
   const base = 'topology/early_warning/node/'
+  const url =
+    base +
+    'viewer_coastal_flooding_warning/viewer_coastal_flooding_warning_warning1/ssd/coastal_flooding1'
 
-  test('SSD should load with filled in values', async ({ page }) => {
-    await page.goto(
-      base +
-        'viewer_coastal_flooding_warning/viewer_coastal_flooding_warning_warning1/ssd/coastal_flooding1',
-    )
-
+  test.beforeEach(async ({ page }) => {
+    await page.goto(url)
     const value1 = page.getByText('0.82')
     await expect(value1).toBeVisible()
     const value2 = page.getByText('0.67')
     await expect(value2).toBeVisible()
+  })
+
+  test('SSD should load with filled in values', async ({ page }) => {
     const value3 = page.getByText('0.72')
     await expect(value3).toBeVisible()
     const value4 = page.getByText('0.85')
@@ -22,10 +24,6 @@ test.describe('Schematic Status Display Tests', () => {
   test('SSD should be responsive and switch to other display', async ({
     page,
   }) => {
-    await page.goto(
-      base +
-        'viewer_coastal_flooding_warning/viewer_coastal_flooding_warning_warning1/ssd/coastal_flooding1',
-    )
     await page
       .getByRole('button', { name: 'Switch to Flood Warning #' })
       .click()
@@ -42,10 +40,6 @@ test.describe('Schematic Status Display Tests', () => {
   test('SSD should be responsive and switch to TimeSeriesDisplay', async ({
     page,
   }) => {
-    await page.goto(
-      base +
-        'viewer_coastal_flooding_warning/viewer_coastal_flooding_warning_warning1/ssd/coastal_flooding1',
-    )
     await page
       .getByRole('button', { name: 'Switch to TimeSeriesDisplay' })
       .click()
