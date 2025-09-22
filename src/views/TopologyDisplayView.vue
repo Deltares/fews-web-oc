8a4710983cc0bde41da02e7dcf63e0d1e4966014
Werner Kramer <werner.kramer@vortech.nl>
2025-09-22 16:55:58 +0200

    fix: update icon for TaskRunsControl and remove unused prepend icon in TopologyDisplayView

--- src/views/TopologyDisplayView.vue
+++ src/views/TopologyDisplayView.vue
@@ -65,7 +65,6 @@
         <!-- Task Run Overview option -->
         <v-list-item
           v-if="showTaskMenu && secondaryControl !== 'tasks'"
-          prepend-icon="mdi-format-list-checks"
           title="Task Overview"
           @click="
             () => {
