500a2c7441580ec3acc493232969bc3ac233bb56
hvangeffen <hunter.vangeffen@vortech.nl>
2025-09-30 10:27:46 +0200

    Add showBrush user setting

--- src/components/timeseries/TimeSeriesComponent.vue
+++ src/components/timeseries/TimeSeriesComponent.vue
@@ -18,6 +18,7 @@
             :forecastLegend="config.forecastLegend"
           >
             <TimeSeriesChartBrush
+              v-if="showBrush"
               v-model:domain="visibleDomain"
               :config="getSubplotWithDomain(subplot, fullBrushDomain)"
               :series="brushChartSeries"
@@ -119,6 +120,7 @@ import {
   getDomainWithConfigFallback,
   getSubplotWithDomain,
 } from '@/lib/display/utils'
+import { useUserSettingsStore } from '@/stores/userSettings'
 
 interface Props {
   config?: DisplayConfig
@@ -173,6 +175,7 @@ const MIN_HOURS_FOR_REFETCH = 12
 
 const { selectedDate } = useSelectedDate(() => props.currentTime)
 const store = useSystemTimeStore()
+const userSettings = useUserSettingsStore()
 const isEditing = ref(false)
 const confirmationDialog = ref(false)
 const { xs } = useDisplay()
@@ -189,6 +192,7 @@ const fullBrushDomain = ref<[Date, Date]>([
   new Date('2024-05-01T00:00:00Z'),
   new Date('2025-12-31T23:59:59Z'),
 ])
+const showBrush = computed(() => userSettings.get('charts.brush')?.value)
 const chartOptions = ref<UseTimeSeriesOptions>({
   startTime: store.startTime,
   endTime: store.endTime,
@@ -216,7 +220,7 @@ const { series: brushChartSeries } = useTimeSeries(
   baseUrl,
   () => props.config.requests,
   brushOptions,
-  () => tab.value === DisplayType.TimeSeriesChart,
+  () => tab.value === DisplayType.TimeSeriesChart && showBrush.value,
 )
 const {
   series: tableSeries,
