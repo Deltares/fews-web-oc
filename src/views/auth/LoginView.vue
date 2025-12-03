17c127cf25b6b893e7b5a5d35aaedf54744ca8d4
Werner Kramer <werner.kramer@deltares.nl>
2025-12-03 15:33:19 +0100

    feat: allow customization of login view with dynamic labels and icons

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,30 +1,62 @@
 <template>
   <v-app>
-    <v-main class="login-container">
-      <div>&nbsp;</div>
-      <h1 style="color: white">Delft-FEWS Web Operator Client</h1>
-      <div class="login-providers">
-        <deltares-login name="Deltares" />
+    <v-main class="weboc-login__main">
+      <div class="weboc-login__container">
+        <h1 class="weboc-login__title" ref="app-name">
+          Delft-FEWS Web Operator Client
+        </h1>
+        <div class="weboc-login-provider">
+          <v-btn
+            @click="login"
+            color="primary"
+            variant="elevated"
+            class="weboc-login-provider__button"
+            :prepend-icon="prependIcon"
+            :append-icon="appendIcon"
+          >
+            <span>{{ label }}</span>
+          </v-btn>
+        </div>
       </div>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import DeltaresLogin from './DeltaresLogin.vue'
+import { useTemplateRef, onMounted } from 'vue'
+import { configManager } from '@/services/application-config'
+import { useRoute } from 'vue-router'
+import { authenticationManager } from '@/services/authentication/AuthenticationManager.js'
+
+const route = useRoute()
+
+const appName = useTemplateRef('app-name')
+
+const label = configManager.getWithDefault(
+  'VITE_LOGIN_PROVIDER_LABEL',
+  'Sign in',
+)
+const appendIcon = configManager.getWithDefault(
+  'VITE_LOGIN_PROVIDER_APPEND_ICON',
+  '',
+)
+const prependIcon = configManager.getWithDefault(
+  'VITE_LOGIN_PROVIDER_PREPEND_ICON',
+  '',
+)
+
+onMounted(() => {
+  const name = configManager.getWithDefault(
+    'VITE_APP_NAME',
+    'Delft-FEWS Web OC',
+  )
+  appName.value!.textContent = name
+  document.title = name
+})
+
+function login(): void {
+  const redirect = route.query.redirect ?? '/'
+  authenticationManager.userManager.signinRedirect({ state: redirect })
+}
 </script>
 
-<style scoped>
-.login-container {
-  background-image: url('/images/D111-103_getijdenland_small.jpg');
-  background-position-x: center;
-  background-size: cover;
-  text-align: center;
-  height: 100%;
-  overflow-y: auto;
-}
-
-.login-providers {
-  margin: 50px;
-}
-</style>
