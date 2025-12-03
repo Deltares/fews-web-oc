49d6fa07054d167dfe820de75bebe5bcec910b1d
Werner Kramer <werner.kramer@deltares.nl>
2025-12-03 15:18:12 +0100

    refactor: update UI components in AggregationPanel and InformationPanel for improved usability

--- src/components/wms/panel/AggregationPanel.vue
+++ src/components/wms/panel/AggregationPanel.vue
@@ -1,17 +1,31 @@
 <template>
-  <v-list-item prepend-icon="mdi-sigma" class="pe-1">
-    <v-checkbox
-      v-model="doShowAggregated"
-      label="Show accumulated data"
-      density="compact"
-      hide-details
-    />
-    <v-select
-      v-model="selectedAggregationLabel"
-      label="Accumulation period"
-      :items="aggregationLabels"
-      hide-details
-    />
+  <v-list-item prepend-icon="mdi-sigma">
+    <v-list-item-title>Show accumulated data</v-list-item-title>
+    <template v-slot:append>
+      <v-switch
+        v-model="doShowAggregated"
+        color="primary"
+        density="compact"
+        hide-details
+      ></v-switch>
+    </template>
+    <v-list-item-content> </v-list-item-content>
+  </v-list-item>
+  <v-list-item v-if="doShowAggregated">
+    <template v-slot:prepend>
+      <v-icon></v-icon>
+    </template>
+    <v-list-item-action>
+      <v-select
+        v-model="selectedAggregationLabel"
+        label="Accumulation period"
+        :items="aggregationLabels"
+        density="compact"
+        variant="solo-filled"
+        flat
+        hide-details
+      />
+    </v-list-item-action>
   </v-list-item>
 </template>
 
