538d283b55cd0bc57d71b6244cd13980e70d6aae
Werner Kramer <werner.kramer@deltares.nl>
2025-12-03 15:33:19 +0100

    feat: allow customization of login view with dynamic labels and icons

--- src/services/application-config/ApplicationConfig.ts
+++ src/services/application-config/ApplicationConfig.ts
@@ -4,12 +4,16 @@ export enum RequestHeaderAuthorization {
 }
 
 export type ApplicationConfig = {
-  VITE_AUTH_SCOPE: string
+  VITE_APP_NAME: string
   VITE_AUTH_AUTHORITY: string
-  VITE_FEWS_ARCHIVE_WEBSERVICES_URL: string
-  VITE_AUTH_METADATA_URL: string
   VITE_AUTH_ID: string
-  VITE_REQUEST_HEADER_AUTHORIZATION: RequestHeaderAuthorization
+  VITE_AUTH_METADATA_URL: string
+  VITE_AUTH_SCOPE: string
+  VITE_FEWS_ARCHIVE_WEBSERVICES_URL: string
   VITE_FEWS_WEBSERVICES_URL: string
   LOCALE: string
+  VITE_LOGIN_PROVIDER_LABEL: string
+  VITE_LOGIN_PROVIDER_APPEND_ICON: string
+  VITE_LOGIN_PROVIDER_PREPEND_ICON: string
+  VITE_REQUEST_HEADER_AUTHORIZATION: RequestHeaderAuthorization
 }
