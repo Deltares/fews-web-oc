18edfbea55c56d3df2f5168c2cc9684cbea97d0a
Werner Kramer <werner.kramer@deltares.nl>
2025-12-11 11:46:18 +0100

    fix: update login button props reference in ApplicationConfig and LoginView

--- src/services/application-config/ApplicationConfig.ts
+++ src/services/application-config/ApplicationConfig.ts
@@ -14,7 +14,7 @@ export type ApplicationConfig = {
   VITE_FEWS_ARCHIVE_WEBSERVICES_URL: string
   VITE_FEWS_WEBSERVICES_URL: string
   LOCALE: string
-  VITE_LOGIN_PROVIDER_BUTTON_PROPS: string | VBtn['$props']
+  VITE_LOGIN_BUTTON_PROPS: string | VBtn['$props']
   VITE_LOGIN_STYLESHEET_URL: string
   VITE_REQUEST_HEADER_AUTHORIZATION: RequestHeaderAuthorization
 }
