d5dbbceeb83437bc9585b2ea36efd81105a6aa47
Werner Kramer <werner.kramer@deltares.nl>
2025-12-19 14:35:33 +0100

    Add i18n adapter to Vuetify configuration for improved localization support

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,8 +3,14 @@ import { toHumanReadableDate } from '@/lib/date'
 import '@/styles/main.scss'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
+import { i18n } from './i18n'
+import { useI18n } from 'vue-i18n'
 
 const vuetify = createVuetify({
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
+  },
   defaults: {
     VBtn: {
       variant: 'text',
