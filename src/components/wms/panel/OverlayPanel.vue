8d1aa6395be054a9e681d11e1de2a51e76264278
Werner Kramer <werner.kramer@deltares.nl>
2026-02-06 12:57:47 +0100

    feat: add vuedraggable for enhanced overlay selection in OverlayPanel

--- src/components/wms/panel/OverlayPanel.vue
+++ src/components/wms/panel/OverlayPanel.vue
@@ -1,35 +1,51 @@
 <template>
-  <v-list
-    v-model:selected="selectedOverlayIds"
-    select-strategy="leaf"
-    density="compact"
-  >
+  <v-list density="compact">
     <v-list-item-subtitle class="ps-3 pb-1">
       {{ t('wms.overlays') }}
     </v-list-item-subtitle>
-    <v-list-item
-      v-for="overlay in overlays"
-      :key="overlay.id"
-      :title="getTitle(overlay)"
-      :value="overlay.id"
-      :active="false"
-      min-height="32"
-      rounded
+    <draggable
+      :list="list"
+      :disabled="!enabled"
+      item-key="name"
+      class="list-group"
+      ghost-class="ghost"
     >
-      <template #prepend="{ isSelected, select }">
-        <v-list-item-action start>
-          <v-checkbox-btn
-            :model-value="isSelected"
-            @update:model-value="select"
-            density="compact"
-          />
-        </v-list-item-action>
+      <template #item="{ element }">
+        <v-list-item :class="{ 'not-draggable': !enabled }" density="compact">
+          <template #prepend>
+            <v-list-item-action start>
+              <v-icon
+                v-if="enabled"
+                icon="mdi-drag"
+                size="small"
+                class="drag-handle"
+              />
+            </v-list-item-action>
+            <v-list-item-action start>
+              <v-checkbox-btn
+                :modelValue="
+                  selectedOverlayIds.includes(element.overlayId ?? '')
+                "
+                @update:model-value="
+                  (value) => {
+                    console.log(value)
+                    selectOverlayId(element.overlayId ?? '', value as boolean)
+                  }
+                "
+                density="compact"
+              />
+            </v-list-item-action>
+          </template>
+          <v-list-item-title>{{ element.name }}</v-list-item-title>
+        </v-list-item>
       </template>
-    </v-list-item>
+    </draggable>
   </v-list>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
 import type { Overlay } from '@deltares/fews-pi-requests'
 import type { GetCapabilitiesResponse } from '@deltares/fews-wms-requests'
 
@@ -54,4 +70,43 @@ function getTitle(overlay: Overlay): string {
 const selectedOverlayIds = defineModel<string[]>('selectedOverlayIds', {
   required: true,
 })
+
+function selectOverlayId(overlayId: string, value: boolean) {
+  const index = selectedOverlayIds.value.indexOf(overlayId)
+  if (value) {
+    if (index === -1) {
+      selectedOverlayIds.value.push(overlayId)
+    }
+  } else {
+    if (index !== -1) {
+      selectedOverlayIds.value.splice(index, 1)
+    }
+  }
+}
+
+interface ListItem {
+  id: number
+  overlayId?: string
+  name: string
+}
+
+const enabled = ref(true)
+const list = ref<ListItem[]>(
+  props.overlays.map((o, index) => ({
+    id: index,
+    overlayId: o.id,
+    name: getTitle(o),
+  })),
+)
 </script>
+
+<style scoped>
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.not-draggable {
+  cursor: no-drop;
+}
+</style>
