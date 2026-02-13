fd46cbc38461a0c5286a7b04dc7836e7fb2793e2
Jules Fleuren <jules.fleuren@vortech.nl>
2026-02-13 09:06:29 +0100

    Add control for disabling permissions

--- src/lib/requests/transformRequest.ts
+++ src/lib/requests/transformRequest.ts
@@ -1,12 +1,22 @@
 import { authenticationManager } from '@/services/authentication/AuthenticationManager.ts'
+import { getPermissionExcludesHeader } from '@/services/usePermissionExcludes'
 
 export function createTransformRequestFn(controller?: AbortController) {
   return async (request: Request): Promise<Request> => {
-    return await authenticationManager.transformRequestAuth(
-      request,
-      controller?.signal,
-    )
+    const additionalHeaders = await getRequestHeaders()
+    const requestInit = {
+      headers: mergeHeaders(request.headers, additionalHeaders),
+      signal: controller?.signal,
+    }
+    const newRequest = new Request(request, requestInit)
+    return Promise.resolve(newRequest)
   }
+}
+
+export async function getRequestHeaders(): Promise<Headers> {
+  const permExcludeHeaders = getPermissionExcludesHeader()
+  const authHeaders = await authenticationManager.getAuthorizationHeaders()
+  return mergeHeaders(permExcludeHeaders, authHeaders)
 }
 
 export function mergeHeaders(headers1: Headers, headers2: Headers): Headers {
