bad30240ef31cae3b667c7c580978c5d741abe8e
hvangeffen <hunter.vangeffen@vortech.nl>
2026-04-10 10:11:46 +0200

    Give data analysis higher tab priority over data download

--- src/lib/topology/component.ts
+++ src/lib/topology/component.ts
@@ -12,6 +12,7 @@ export const ComponentType = {
   dashboard: 'dashboard',
   map: 'map',
   charts: 'charts',
+  'data-analysis-display': 'data-analysis-display',
   'data-download-display': 'data-download-display',
   report: 'report',
   'schematic-status-display': 'schematic-status-display',
@@ -20,7 +21,6 @@ export const ComponentType = {
   'dynamic-report-display': 'dynamic-report-display',
   'runtask-display': 'runtask-display',
   'log-display': 'log-display',
-  'data-analysis-display': 'data-analysis-display',
   'documents-display': 'documents-display',
 } satisfies Record<ComponentType, ComponentType>
 
@@ -28,6 +28,7 @@ export const componentTypeToIconMap = {
   dashboard: 'mdi-view-dashboard',
   map: 'mdi-map',
   charts: 'mdi-chart-multiple',
+  'data-analysis-display': 'mdi-database-search',
   'data-download-display': 'mdi-download',
   report: 'mdi-file-document',
   'schematic-status-display': 'mdi-application',
@@ -36,7 +37,6 @@ export const componentTypeToIconMap = {
   'html-display': 'mdi-web',
   'runtask-display': 'mdi-cog',
   'log-display': 'mdi-file-document',
-  'data-analysis-display': 'mdi-database-search',
   'documents-display': 'mdi-file-document-multiple',
 } satisfies Record<ComponentType, string>
 
@@ -44,6 +44,7 @@ export const componentTypeToTitleMap = {
   dashboard: 'Dashboard',
   map: 'Map',
   charts: 'Charts',
+  'data-analysis-display': 'Data Analysis',
   'data-download-display': 'Download',
   report: 'Reports',
   'schematic-status-display': 'Schematic',
@@ -52,7 +53,6 @@ export const componentTypeToTitleMap = {
   'dynamic-report-display': 'Dynamic Report',
   'runtask-display': 'Run Task',
   'log-display': 'Log',
-  'data-analysis-display': 'Data Analysis',
   'documents-display': 'Documents Browser',
 } satisfies Record<ComponentType, string>
 
@@ -60,6 +60,7 @@ export const componentTypeToRouteNameMap = {
   dashboard: 'TopologyDashboard',
   map: 'TopologySpatialDisplay',
   charts: 'TopologyTimeSeries',
+  'data-analysis-display': 'TopologyDataAnalysisDisplay',
   'data-download-display': 'TopologyDataDownload',
   report: 'TopologyReports',
   'schematic-status-display': 'TopologySchematicStatusDisplay',
@@ -68,7 +69,6 @@ export const componentTypeToRouteNameMap = {
   'dynamic-report-display': 'TopologyDynamicReports',
   'runtask-display': 'TopologyWhatIfDisplay',
   'log-display': 'TopologyLogDisplay',
-  'data-analysis-display': 'TopologyDataAnalysisDisplay',
   'documents-display': 'TopologyDocumentDisplay',
 } satisfies Record<ComponentType, string>
 
@@ -76,6 +76,7 @@ export const componentTypeToIdMap = {
   dashboard: 'dashboard',
   map: 'spatial',
   charts: 'timeseries',
+  'data-analysis-display': 'analysis',
   'data-download-display': 'download',
   report: 'reports',
   'schematic-status-display': 'ssd',
@@ -84,6 +85,5 @@ export const componentTypeToIdMap = {
   'runtask-display': 'runtask',
   'dynamic-report-display': 'dynamicreport',
   'log-display': 'log',
-  'data-analysis-display': 'analysis',
   'documents-display': 'documents',
 } satisfies Record<ComponentType, string>
