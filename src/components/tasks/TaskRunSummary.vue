e4e9a1de76df350ac0db888f8a3105930d6ca193
Cees Voesenek <cees.voesenek@vortech.nl>
2026-04-17 14:52:39 +0200

    Make icons in task overview and non-current data panels the same size

--- src/components/tasks/TaskRunSummary.vue
+++ src/components/tasks/TaskRunSummary.vue
@@ -17,7 +17,7 @@
                 :text="t('workflow.startedByMe')"
               >
                 <template #activator="{ props }">
-                  <v-icon v-bind="props" icon="mdi-account" size="small" />
+                  <v-icon v-bind="props" icon="mdi-account" />
                 </template>
               </v-tooltip>
               <v-tooltip
@@ -30,7 +30,6 @@
                   <v-icon
                     v-bind="props"
                     :icon="isFollowed ? 'mdi-bell' : 'mdi-bell-off'"
-                    size="small"
                     @click.stop="toggleFollow"
                   />
                 </template>
