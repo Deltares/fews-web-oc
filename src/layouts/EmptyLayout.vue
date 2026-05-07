146243091b50cb16318373182fe6567cdc33669f
hvangeffen <hunter.vangeffen@vortech.nl>
2026-05-07 11:51:32 +0200

    Remove unused currentItem from EmptyLayout

--- src/layouts/EmptyLayout.vue
+++ src/layouts/EmptyLayout.vue
@@ -12,22 +12,6 @@
 
 <script setup lang="ts">
 import { useCustomStyleSheet } from '@/services/useCustomStyleSheet'
-import { onBeforeMount, ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-
-const currentItem = ref('')
-const route = useRoute()
-
-onBeforeMount(async () => {
-  currentItem.value = route.name?.toString() ?? ''
-})
-
-watch(
-  () => route.name,
-  async (name) => {
-    currentItem.value = name?.toString() ?? ''
-  },
-)
 
 useCustomStyleSheet()
 </script>
