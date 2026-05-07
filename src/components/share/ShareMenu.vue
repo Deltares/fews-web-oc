e3f6698214c9a9242e6393edae99276031a9b6c7
hvangeffen <hunter.vangeffen@vortech.nl>
2026-05-07 12:54:43 +0200

    Remove component settings tab

--- src/components/share/ShareMenu.vue
+++ src/components/share/ShareMenu.vue
@@ -2,57 +2,43 @@
   <div class="pa-4 h-100 overflow-auto">
     <CopyUrlField :url="embedUrl" />
 
-    <v-sheet border rounded class="my-4">
-      <v-tabs v-model="tab" grow>
-        <v-tab value="user-settings" class="text-none">User settings</v-tab>
-        <v-tab value="component-settings" class="text-none">
-          Component settings
-        </v-tab>
-      </v-tabs>
-
+    <v-card flat border class="my-4">
+      <v-toolbar density="compact">
+        <div class="ms-4">User settings</div>
+      </v-toolbar>
       <v-divider />
 
-      <v-tabs-window v-model="tab">
-        <v-tabs-window-item value="user-settings">
-          <v-list density="compact" class="py-0">
-            <v-list-group v-for="group in store.groups" :key="group.id">
-              <template #activator="{ props }">
-                <v-list-item
-                  v-bind="props"
-                  :title="group.title"
-                  :prepend-icon="group.icon"
-                />
-              </template>
+      <v-list density="compact" class="py-0">
+        <v-list-group v-for="group in store.groups" :key="group.id">
+          <template #activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              :title="group.title"
+              :prepend-icon="group.icon"
+            />
+          </template>
 
-              <template
-                v-for="setting in getSettingsByGroup(group.id)"
-                :key="setting.id"
-              >
-                <UserSettingsOneOfMultiple
-                  v-if="setting.type === 'oneOfMultiple'"
-                  v-model="setting.value"
-                  :setting="setting"
-                  inline
-                />
+          <template
+            v-for="setting in getSettingsByGroup(group.id)"
+            :key="setting.id"
+          >
+            <UserSettingsOneOfMultiple
+              v-if="setting.type === 'oneOfMultiple'"
+              v-model="setting.value"
+              :setting="setting"
+              inline
+            />
 
-                <UserSettingsBoolean
-                  v-else-if="setting.type === 'boolean'"
-                  v-model="setting.value"
-                  :setting="setting"
-                  inline
-                />
-              </template>
-            </v-list-group>
-          </v-list>
-        </v-tabs-window-item>
-
-        <v-tabs-window-item value="component-settings">
-          <v-card-text class="text-center">
-            <p class="mb-2">Component settings are not supported yet.</p>
-          </v-card-text>
-        </v-tabs-window-item>
-      </v-tabs-window>
-    </v-sheet>
+            <UserSettingsBoolean
+              v-else-if="setting.type === 'boolean'"
+              v-model="setting.value"
+              :setting="setting"
+              inline
+            />
+          </template>
+        </v-list-group>
+      </v-list>
+    </v-card>
   </div>
 </template>
 
@@ -67,8 +53,6 @@ import { computed, ref } from 'vue'
 
 const route = useRoute()
 const store = useUserSettingsStore()
-
-const tab = ref('user-settings')
 
 const settings = ref(store.items.map((item) => ({ ...item })))
 
@@ -98,13 +82,6 @@ const embedUrl = computed(() => {
       params.set(setting.id, setting.value)
     }
   })
-
-  // Example of getting these params using vue router
-  // const route = useRoute()
-  // const mySettingValue = route.query['my-setting-id']
-  // You can also parse boolean values if needed
-  // const mySettingBoolean = mySettingValue === 'true'
-  // This way you can apply the settings when the component is created based on the URL params
 
   return url.toString()
 })
