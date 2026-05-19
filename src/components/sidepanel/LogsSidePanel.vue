571c2aa7cb57aad353664d9826b95d6ec85aa8e6
Werner Kramer <werner.kramer@deltares.nl>
2026-05-19 09:21:54 +0200

    Add LogDisplay component to side panel and update localization

---
+++ src/components/sidepanel/LogsSidePanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <SidePanelContent :title="t('sidePanel.logDisplay')" @close="emit('close')">
+  </SidePanelContent>
+</template>
+
+<script setup lang="ts">
+import SidePanelContent from './SidePanelContent.vue'
+
+import { type TopologyNode } from '@deltares/fews-pi-requests'
+import { useI18n } from 'vue-i18n'
+
+interface Props {
+  topologyNode?: TopologyNode
+}
+
+const props = defineProps<Props>()
+
+const { t } = useI18n()
+
+interface Emits {
+  close: []
+}
+const emit = defineEmits<Emits>()
+</script>
