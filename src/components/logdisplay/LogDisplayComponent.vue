cbb1d2a64dd61bdd52165b39f61aec9d0d4f1f2c
Werner Kramer <werner.kramer@deltares.nl>
2026-06-08 10:57:31 +0200

    fix: adjust padding for expansion panel text wrapper in LogDisplayComponent

--- src/components/logdisplay/LogDisplayComponent.vue
+++ src/components/logdisplay/LogDisplayComponent.vue
@@ -540,7 +540,7 @@ async function refreshLogs() {
 }
 
 :deep(.v-expansion-panel-text__wrapper) {
-  padding-right: 0;
+  padding: 0px 0px 5px 0px;
 }
 
 .expand-icon-container {
@@ -567,11 +567,6 @@ async function refreshLogs() {
 
 :deep(.v-expansion-panel-title__overlay) {
   background-color: transparent;
-}
-
-:deep(.v-expansion-panel-text__wrapper) {
-  padding: 0;
-  padding-bottom: 5px;
 }
 
 .spacer {
