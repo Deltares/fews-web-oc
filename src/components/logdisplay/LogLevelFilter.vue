9f14c4841967e227c8b5c9631e426f9204d9adf8
Werner Kramer <werner.kramer@deltares.nl>
2026-06-08 16:47:22 +0200

    Add LogLevelFilter to LogSidePanel and initialize selectedLevels with logLevels

---
+++ src/components/logdisplay/LogLevelFilter.vue
@@ -0,0 +1,26 @@
+<template>
+  <BaseTasksFilterControl
+    v-model="selected"
+    :items="logLevelsItems"
+    label="Log Levels"
+    sortItems
+  />
+</template>
+<script setup lang="ts">
+import BaseTasksFilterControl from '@/components/tasks/BaseTaskFilterControl.vue'
+import { computed } from 'vue'
+import { levelToIcon, logLevels } from '@/lib/log'
+
+const selected = defineModel<string[]>({ required: true })
+
+const logLevelsItems = computed(() =>
+  logLevels.map((logLevel) => {
+    return {
+      id: logLevel,
+      icon: levelToIcon(logLevel),
+      title: logLevel,
+      value: logLevel,
+    }
+  }),
+)
+</script>
