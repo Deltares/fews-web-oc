ec02e0d10af0dd8d2e795cded53f0196f5492233
hvangeffen <hunter.vangeffen@vortech.nl>
2026-06-10 10:20:51 +0200

    Refactor remote manifest loading to use manifest JSON for Module Federation initialization

--- src/lib/startup/module-federation.ts
+++ src/lib/startup/module-federation.ts
@@ -1,22 +1,14 @@
 import { init } from '@module-federation/enhanced/runtime'
 import { configManager } from '../../services/application-config'
 
-export function setupModuleFederation() {
-  const remoteManifest = configManager.get('VITE_FEWS_WEBOC_MF_MANIFEST_URL')
-  if (!remoteManifest) return
+export async function setupModuleFederation() {
+  const manifestUrl = configManager.get('VITE_FEWS_WEBOC_MF_MANIFEST_URL')
+  if (!manifestUrl) return
 
-  console.log(`Loading remote manifest from: ${remoteManifest}`)
+  console.log(`Loading manifest from: ${manifestUrl}`)
 
-  const initOptions = {
-    name: 'weboc-micro-frontend',
-    remotes: [
-      {
-        // mf-manifest.json is a file type generated in the new version of Module Federation build tools, providing richer functionality compared to remoteEntry
-        // Preloading depends on the use of the mf-manifest.json file type
-        name: 'mdba-micro-frontend',
-        entry: remoteManifest,
-      },
-    ],
-  }
-  init(initOptions)
+  const response = await fetch(manifestUrl)
+  const manifestJson = await response.json()
+
+  init(manifestJson)
 }
