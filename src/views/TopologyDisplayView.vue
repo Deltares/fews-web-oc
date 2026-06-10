dd081f893183c45c9b6870f58a1e6b4de5c8b5c7
hvangeffen <hunter.vangeffen@vortech.nl>
2026-06-10 10:15:33 +0200

    Use 'micro-frontend' terminology instead of 'plugin'

--- src/views/TopologyDisplayView.vue
+++ src/views/TopologyDisplayView.vue
@@ -283,7 +283,7 @@ function onNavigate(to: NavigateRoute) {
       })
       break
     }
-    case 'PluginDisplay':
+    case 'MicroFrontendDisplay':
       router.push({
         name,
         params: {
@@ -292,7 +292,7 @@ function onNavigate(to: NavigateRoute) {
         query: route.query,
       })
       break
-    case 'PluginTimeSeriesDisplay':
+    case 'MicroFrontendTimeSeriesDisplay':
       router.push({
         name,
         params: {
