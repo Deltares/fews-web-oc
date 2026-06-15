1f30d0f68b67cb54dab2397f39818aa6b0260671
hvangeffen <hunter.vangeffen@vortech.nl>
2026-06-15 13:23:41 +0200

    Fix and cleanup MF ts errors

--- src/plugins/moduleFederation.ts
+++ src/plugins/moduleFederation.ts
@@ -1,16 +1,12 @@
 import { MF_REGISTRY_KEY } from '@/composables/useMicroFrontEnd'
 import { createTransformRequestFn } from '@/lib/requests/transformRequest'
-import {
-  PiWebserviceProvider,
-  WebOCMicroFrontEnds,
-  WebOCMicroFrontEndsResponse,
-} from '@deltares/fews-pi-requests'
-import { getInstance } from '@module-federation/enhanced/runtime'
+import { PiWebserviceProvider } from '@deltares/fews-pi-requests'
+import { registerRemotes } from '@module-federation/enhanced/runtime'
 import { App } from 'vue'
 
 export default {
   async install(app: App, options: { manifestUrl: string; baseUrl: string }) {
-    if (!options || !options.manifestUrl) {
+    if (!options?.manifestUrl) {
       console.error(
         'Module Federation Plugin: Missing manifest URL configuration.',
       )
@@ -20,21 +16,18 @@ export default {
     try {
       const response = await fetch(options.manifestUrl)
       const manifest = await response.json()
-      const instance = getInstance()
-      instance.registerRemotes(manifest.remotes)
-      // const instance = getInstance()
-      console.log('Module Federation Plugin: Instance created.', instance)
+
+      registerRemotes(manifest.remotes)
+
       const piProvider = new PiWebserviceProvider(options.baseUrl, {
         transformRequestFn: createTransformRequestFn(),
       })
 
       const config = await piProvider.getMicroFrontEnds({})
-      if (isMicroFrontendConfig(config)) {
-        app.provide(MF_REGISTRY_KEY, {
-          config,
-          options,
-        })
-      }
+      app.provide(MF_REGISTRY_KEY, {
+        config,
+        options,
+      })
       console.log('Module Federation Plugin: Instance created and installed.')
     } catch (error) {
       console.error(
@@ -43,14 +36,4 @@ export default {
       )
     }
   },
-}
-
-function isMicroFrontendConfig(config: WebOCMicroFrontEndsResponse): config is {
-  microFrontEnds: WebOCMicroFrontEnds[]
-} {
-  return (
-    config &&
-    Array.isArray(config.microFrontEnds) &&
-    config.microFrontEnds.every((mf) => typeof mf.id === 'string')
-  )
 }
