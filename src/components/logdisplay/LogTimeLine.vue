7732cb954b2d343e5f352eef84baaee09cc90dd6
Werner Kramer <werner.kramer@deltares.nl>
2026-07-02 13:28:06 +0200

    Enhance LogTimeLine component with dropdown menu for log actions and improved layout

--- src/components/logdisplay/LogTimeLine.vue
+++ src/components/logdisplay/LogTimeLine.vue
@@ -15,9 +15,43 @@
       </template>
       <div class="d-flex flex-column log_item__content">
         <div class="text-body-medium">{{ log.text }}</div>
-        <span class="text-label-large text-medium-emphasis">{{
-          toHumanReadableDateTime(log.entryTime)
-        }}</span>
+        <div class="d-flex align-center ga-1">
+          <span class="text-label-large text-medium-emphasis">{{
+            toHumanReadableDateTime(log.entryTime)
+          }}</span>
+          <v-menu
+            v-if="log.topologyNodeId || systemDisseminations.length"
+            location="bottom right"
+            max-width="300"
+            :close-on-content-click="false"
+          >
+            <template #activator="{ props: activatorProps }">
+              <v-btn
+                v-bind="activatorProps"
+                icon
+                density="compact"
+                size="small"
+              >
+                <v-icon size="x-small">mdi-dots-horizontal</v-icon>
+              </v-btn>
+            </template>
+            <v-list density="compact">
+              <v-list-item
+                v-if="log.topologyNodeId"
+                :to="logToRoute(log)"
+                prepend-icon="mdi-link-variant"
+              />
+              <LogDisseminations
+                :log="log"
+                :disseminations="systemDisseminations"
+                :disseminationStatus="props.disseminationStatus"
+                @disseminate-log="
+                  (log, dis) => emit('disseminateLog', log, dis)
+                "
+              />
+            </v-list>
+          </v-menu>
+        </div>
       </div>
     </v-timeline-item>
   </v-timeline>
@@ -31,12 +65,15 @@ import type {
 import {
   levelToColor,
   levelToIcon,
+  logToRoute,
   type LogActionEmit,
   type LogDisseminationStatus,
   type LogMessage,
 } from '@/lib/log'
 import type { TaskRun } from '@/lib/taskruns'
 import { toHumanReadableDateTime } from '@/lib/date'
+import { computed } from 'vue'
+import LogDisseminations from '@/components/logdisplay/LogDisseminations.vue'
 
 interface Props {
   userName: string
@@ -50,4 +87,8 @@ interface Props {
 const props = defineProps<Props>()
 
 const emit = defineEmits<LogActionEmit>()
+
+const systemDisseminations = computed(() =>
+  props.disseminations.filter((d) => d.systemLog),
+)
 </script>
