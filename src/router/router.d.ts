2be72ded72584434e0b63926d5081a2d012f387c
Werner Kramer <werner.kramer@deltares.nl>
2026-07-20 14:45:42 +0200

    feat: add RouteMeta interface to extend vue-router functionality

---
+++ src/router/router.d.ts
@@ -0,0 +1,13 @@
+import 'vue-router'
+
+// To ensure it is treated as a module, add at least one `export` statement
+export {}
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string
+    layout?: 'EmptyLayout' | 'DefaultLayout'
+    sidebar?: boolean
+    authorize?: string[]
+  }
+}
