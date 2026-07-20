0562199875d8d82938ff35de5c63fcd09055205d
Werner Kramer <werner.kramer@deltares.nl>
2026-07-20 16:15:07 +0200

    refactor: remove unnecessary export statement in router.d.ts

--- src/router/router.d.ts
+++ src/router/router.d.ts
@@ -1,8 +1,5 @@
 import 'vue-router'
 
-// To ensure it is treated as a module, add at least one `export` statement
-export {}
-
 declare module 'vue-router' {
   interface RouteMeta {
     title?: string
