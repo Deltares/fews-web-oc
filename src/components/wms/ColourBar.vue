e570adf1f6993e4edfe522e62a942c4ed0a7a91d
hvangeffen <hunter.vangeffen@vortech.nl>
2026-09-03 10:10:35 +0200

    Remove unused legend input

--- src/components/wms/ColourBar.vue
+++ src/components/wms/ColourBar.vue
@@ -5,18 +5,6 @@
     class="legend_container"
   >
     <svg ref="colourbarElement" class="map__colour-bar"></svg>
-    <LegendInput
-      parentId="min-legend"
-      v-model:value="range.min"
-      :maxValue="range.max"
-      v-model:isEditing="isEditingMin"
-    />
-    <LegendInput
-      parentId="max-legend"
-      v-model:value="range.max"
-      :minValue="range.min"
-      v-model:isEditing="isEditingMax"
-    />
   </div>
 </template>
 
@@ -29,12 +17,6 @@ import {
   ColourMap,
   AxisPosition,
 } from '@deltares/fews-web-oc-charts'
-import LegendInput from '@/components/wms/LegendInput.vue'
-
-type Range = {
-  min: number
-  max: number
-}
 
 interface Props {
   colourMap?: ColourMap
@@ -46,14 +28,10 @@ const props = withDefaults(defineProps<Props>(), {
   useGradients: true,
 })
 
-const range = defineModel<Range>('range', { required: true })
-
 const colourbarElement = ref<SVGElement>()
 
 const isVisible = ref<boolean>(true)
 
-const isEditingMin = ref<boolean>(false)
-const isEditingMax = ref<boolean>(false)
 let group: Selection<SVGGElement, unknown, null, unknown>
 
 onMounted(() => {
@@ -85,14 +63,6 @@ function updateColourBar() {
   }
   new ColourBar(group as any, props.colourMap, 250, 10, options)
   isVisible.value = true
-
-  const firstChild = select('#colourbar > g > g.axis').selectChild()
-  const lastChild = select('#colourbar > g > g.axis').selectChild(':last-child')
-
-  firstChild.on('click', () => (isEditingMin.value = true))
-  lastChild.on('click', () => (isEditingMax.value = true))
-  firstChild.attr('id', 'min-legend')
-  lastChild.attr('id', 'max-legend')
 }
 </script>
 
